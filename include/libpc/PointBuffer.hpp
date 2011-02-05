741a590fa297e3bf5cdee2dfed61c9974d66445f
Michael P. Gerlek <mpg@flaxen.com>
2011-02-04 18:11:50 -0800

    updated copyright

--- include/libpc/PointBuffer.hpp
+++ include/libpc/PointBuffer.hpp
@@ -13,10 +13,10 @@
 *       notice, this list of conditions and the following disclaimer in 
 *       the documentation and/or other materials provided 
 *       with the distribution.
-*     * Neither the name of the Martin Isenburg or Iowa Department 
-*       of Natural Resources nor the names of its contributors may be 
-*       used to endorse or promote products derived from this software 
-*       without specific prior written permission.
+*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the 
+*       names of its contributors may be used to endorse or promote 
+*       products derived from this software without specific prior 
+*       written permission.
 * 
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
 * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
