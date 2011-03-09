add2c2547921f61e0241d699e4912dfc20653c3f
Michael P. Gerlek <mpg@flaxen.com>
2011-03-08 16:04:11 -0800

    added cheesy decimation filter

---
+++ src/DecimationFilter.cpp
@@ -0,0 +1,79 @@
+/******************************************************************************
+* Copyright (c) 2011, Michael P. Gerlek (mpg@flaxen.com)
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following
+* conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in
+*       the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
+*       names of its contributors may be used to endorse or promote
+*       products derived from this software without specific prior
+*       written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+* OF SUCH DAMAGE.
+****************************************************************************/
+
+#include "libpc/DecimationFilter.hpp"
+
+namespace libpc
+{
+
+
+DecimationFilter::DecimationFilter(Stage& prevStage, int step)
+    : Filter(prevStage)
+    , m_step(step)
+{
+    Header& header = getHeader();
+    header.setNumPoints( header.getNumPoints() / step );
+
+    return;
+}
+
+
+const std::string& DecimationFilter::getName() const
+{
+    static std::string name("Decimation Filter");
+    return name;
+}
+
+
+boost::uint32_t DecimationFilter::readBuffer(PointData& dstData)
+{
+    // naive implementation: read a buffer N times larger, then pull out what we need
+    PointData srcData(dstData.getSchemaLayout(), dstData.getNumPoints() * m_step);
+    boost::uint32_t numSrcPointsRead = m_prevStage.read(srcData);
+
+    boost::uint32_t numPoints = dstData.getNumPoints();
+    
+    boost::uint32_t srcIndex = 0;
+    boost::uint32_t dstIndex = 0;
+    for (dstIndex=0; dstIndex<numPoints; dstIndex++)
+    {
+        dstData.copyPointFast(dstIndex, srcIndex, srcData);
+        srcIndex += m_step;
+        if (srcIndex > numSrcPointsRead) break;
+    }
+
+    return dstIndex;
+}
+
+}
---
+++ test/unit/DecimationFilterTest.cpp
@@ -0,0 +1,77 @@
+/******************************************************************************
+* Copyright (c) 2011, Michael P. Gerlek (mpg@flaxen.com)
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following
+* conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in
+*       the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
+*       names of its contributors may be used to endorse or promote
+*       products derived from this software without specific prior
+*       written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+* OF SUCH DAMAGE.
+****************************************************************************/
+
+#include <boost/test/unit_test.hpp>
+#include <boost/cstdint.hpp>
+
+#include "libpc/FauxReader.hpp"
+#include "libpc/FauxWriter.hpp"
+#include "libpc/DecimationFilter.hpp"
+
+using namespace libpc;
+
+BOOST_AUTO_TEST_SUITE(DecimationFilterTest)
+
+BOOST_AUTO_TEST_CASE(test)
+{
+    Bounds<double> srcBounds(0.0, 0.0, 0.0, 100.0, 100.0, 100.0);
+
+    FauxReader reader(srcBounds, 1000, FauxReader::Random);
+
+    DecimationFilter filter(reader, 10);
+    BOOST_CHECK(filter.getName() == "Decimation Filter");
+
+    const Schema& schema = filter.getHeader().getSchema();
+    SchemaLayout layout(schema);
+
+    PointData data(layout, 3);
+    
+    boost::uint32_t numRead = filter.read(data);
+
+    BOOST_CHECK(numRead == 3);
+
+    int offsetT = schema.getDimensionIndex(Dimension::Field_GpsTime);
+
+    boost::uint64_t t0 = data.getField<boost::uint64_t>(0, offsetT);
+    boost::uint64_t t1 = data.getField<boost::uint64_t>(1, offsetT);
+    boost::uint64_t t2 = data.getField<boost::uint64_t>(2, offsetT);
+
+    BOOST_CHECK(t0 == 0);
+    BOOST_CHECK(t1 == 10);
+    BOOST_CHECK(t2 == 20);
+
+    return;
+}
+
+BOOST_AUTO_TEST_SUITE_END()
