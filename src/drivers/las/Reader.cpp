bf838ffdfe54350f0901eee3dc734836af1662eb
Michael P. Gerlek <mpg@flaxen.com>
2011-03-17 14:01:41 -0400

    lint

--- src/drivers/las/Reader.cpp
+++ src/drivers/las/Reader.cpp
@@ -256,7 +256,6 @@ boost::uint32_t LasReader::readBuffer(PointData& pointData, const Bounds<double>
 Iterator* LasReader::createIterator(const Bounds<double>&)
 {
     throw not_yet_implemented("iterator");
-    return NULL;
 }
 
 
