9712acabf5e33c501da8aea9b73597b30ac80c32
Howard Butler <hobu.inc@gmail.com>
2011-07-03 14:17:21 -0500

    clean up initialization order

--- src/Writer.cpp
+++ src/Writer.cpp
@@ -48,11 +48,12 @@ const boost::uint32_t Writer::s_defaultChunkSize = 1024 * 32;
 
 
 Writer::Writer(Stage& prevStage, const Options& options)
-    : m_options(options)
-    , m_actualNumPointsWritten(0)
+    : m_actualNumPointsWritten(0)
     , m_targetNumPointsToWrite(0)
+    , m_options(options)
     , m_prevStage(prevStage)
     , m_chunkSize(s_defaultChunkSize)
+
 {
     return;
 }
