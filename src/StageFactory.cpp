6d34d09f6d5c19ad5102408fa21efb6d5783496c
Michael P. Gerlek <mpg@flaxen.com>
2011-08-16 10:11:20 -0700

    cleanup; added XML driver support

--- src/StageFactory.cpp
+++ src/StageFactory.cpp
@@ -41,24 +41,21 @@
 
 #include <pdal/drivers/faux/Reader.hpp>
 #include <pdal/drivers/las/Reader.hpp>
-
 #ifdef PDAL_HAVE_LIBLAS
 #include <pdal/drivers/liblas/Reader.hpp>
 #endif
-
 #ifdef PDAL_HAVE_ORACLE
 #include <pdal/drivers/oci/Reader.hpp>
 #endif
+#include <pdal/drivers/pipeline/Reader.hpp>
 #include <pdal/drivers/qfit/Reader.hpp>
 #include <pdal/drivers/terrasolid/Reader.hpp>
 
 #include <pdal/drivers/faux/Writer.hpp>
 #include <pdal/drivers/las/Writer.hpp>
-
 #ifdef PDAL_HAVE_LIBLAS
 #include <pdal/drivers/liblas/Writer.hpp>
 #endif
-
 #ifdef PDAL_HAVE_ORACLE
 #include <pdal/drivers/oci/Writer.hpp>
 #endif
@@ -80,54 +77,77 @@
 
 namespace pdal
 {
-
+    //
+    // macros for creating the various stage types
+    //
 #define MAKE_READER_CREATOR(T, FullT) \
-    Reader* create_##T(const Options& options) { return new FullT(options); }
-
+    Reader* create_##T(const Options& options) \
+        { return new FullT(options); }
 #define MAKE_FILTER_CREATOR(T, FullT) \
-    Filter* create_##T(Stage& prevStage, const Options& options) { return new FullT(prevStage, options); }
-
+    Filter* create_##T(Stage& prevStage, const Options& options) \
+        { return new FullT(prevStage, options); }
 #define MAKE_MULTIFILTER_CREATOR(T, FullT) \
-    MultiFilter* create_##T(const std::vector<Stage*>& prevStages, const Options& options) { return new FullT(prevStages, options); }
-
+    MultiFilter* create_##T(const std::vector<Stage*>& prevStages, const Options& options) \
+        { return new FullT(prevStages, options); }
 #define MAKE_WRITER_CREATOR(T, FullT) \
-    Writer* create_##T(Stage& prevStage, const Options& options) { return new FullT(prevStage, options); }
+    Writer* create_##T(Stage& prevStage, const Options& options) \
+        { return new FullT(prevStage, options); }
 
-MAKE_READER_CREATOR(FauxReader, pdal::drivers::faux::Reader)
-MAKE_READER_CREATOR(LasReader, pdal::drivers::las::LasReader)
+    //
+    // macros to register the stage creators
+    //
+#define REGISTER_WRITER(T, FullT) \
+    registerWriter(FullT::s_getName(), create_##T)
+#define REGISTER_READER(T, FullT) \
+    registerReader(FullT::s_getName(), create_##T)
+#define REGISTER_FILTER(T, FullT) \
+    registerFilter(FullT::s_getName(), create_##T)
+#define REGISTER_MULTIFILTER(T, FullT) \
+    registerMultiFilter(FullT::s_getName(), create_##T)
 
+    //
+    // define the functions to create the readers
+    //
+    MAKE_READER_CREATOR(FauxReader, pdal::drivers::faux::Reader)
+    MAKE_READER_CREATOR(LasReader, pdal::drivers::las::LasReader)
 #ifdef PDAL_HAVE_LIBLAS
-MAKE_READER_CREATOR(LiblasReader, pdal::drivers::liblas::LiblasReader)
+    MAKE_READER_CREATOR(LiblasReader, pdal::drivers::liblas::LiblasReader)
 #endif
-
 #ifdef PDAL_HAVE_ORACLE
-MAKE_READER_CREATOR(OciReader, pdal::drivers::oci::Reader)
+    MAKE_READER_CREATOR(OciReader, pdal::drivers::oci::Reader)
 #endif
+    MAKE_READER_CREATOR(Reader, pdal::drivers::pipeline::Reader)
+    MAKE_READER_CREATOR(QfitReader, pdal::drivers::qfit::Reader)
+    MAKE_READER_CREATOR(TerrasolidReader, pdal::drivers::terrasolid::Reader)
 
-MAKE_READER_CREATOR(QfitReader, pdal::drivers::qfit::Reader)
-MAKE_READER_CREATOR(TerrasolidReader, pdal::drivers::terrasolid::Reader)
+    //
+    // define the functions to create the filters
+    //
+    MAKE_FILTER_CREATOR(ByteSwapFilter, pdal::filters::ByteSwapFilter)
+    MAKE_FILTER_CREATOR(CacheFilter, pdal::filters::CacheFilter)
+    MAKE_FILTER_CREATOR(Chipper, pdal::filters::Chipper)
+    MAKE_FILTER_CREATOR(ColorFilter, pdal::filters::ColorFilter)
+    MAKE_FILTER_CREATOR(CropFilter, pdal::filters::CropFilter)
+    MAKE_FILTER_CREATOR(DecimationFilter, pdal::filters::DecimationFilter)
+    MAKE_FILTER_CREATOR(ReprojectionFilter, pdal::filters::ReprojectionFilter)
+    MAKE_FILTER_CREATOR(ScalingFilter, pdal::filters::ScalingFilter)
+    MAKE_FILTER_CREATOR(DescalingFilter, pdal::filters::DescalingFilter)
 
-MAKE_FILTER_CREATOR(ByteSwapFilter, pdal::filters::ByteSwapFilter)
-MAKE_FILTER_CREATOR(CacheFilter, pdal::filters::CacheFilter)
-MAKE_FILTER_CREATOR(Chipper, pdal::filters::Chipper)
-MAKE_FILTER_CREATOR(ColorFilter, pdal::filters::ColorFilter)
-MAKE_FILTER_CREATOR(CropFilter, pdal::filters::CropFilter)
-MAKE_FILTER_CREATOR(DecimationFilter, pdal::filters::DecimationFilter)
-MAKE_FILTER_CREATOR(ReprojectionFilter, pdal::filters::ReprojectionFilter)
-MAKE_FILTER_CREATOR(ScalingFilter, pdal::filters::ScalingFilter)
-MAKE_FILTER_CREATOR(DescalingFilter, pdal::filters::DescalingFilter)
+    //
+    // define the functions to create the multifilters
+    //
+    MAKE_MULTIFILTER_CREATOR(MosaicFilter, pdal::filters::MosaicFilter)
 
-MAKE_MULTIFILTER_CREATOR(MosaicFilter, pdal::filters::MosaicFilter)
-
-MAKE_WRITER_CREATOR(FauxWriter, pdal::drivers::faux::Writer)
-MAKE_WRITER_CREATOR(LasWriter, pdal::drivers::las::LasWriter)
-
+    //
+    // define the functions to create the writers
+    //
+    MAKE_WRITER_CREATOR(FauxWriter, pdal::drivers::faux::Writer)
+    MAKE_WRITER_CREATOR(LasWriter, pdal::drivers::las::LasWriter)
 #ifdef PDAL_HAVE_LIBLAS
-MAKE_WRITER_CREATOR(LiblasWriter, pdal::drivers::liblas::LiblasWriter)
+    MAKE_WRITER_CREATOR(LiblasWriter, pdal::drivers::liblas::LiblasWriter)
 #endif
-
 #ifdef PDAL_HAVE_ORACLE
-MAKE_WRITER_CREATOR(OciWriter, pdal::drivers::oci::Writer)
+    MAKE_WRITER_CREATOR(OciWriter, pdal::drivers::oci::Writer)
 #endif
 
 
@@ -261,9 +281,6 @@ void StageFactory::registerWriter(const std::string& type, WriterCreator* f)
 
 void StageFactory::registerKnownReaders()
 {
-#define REGISTER_READER(T, FullT) \
-    registerReader(FullT::s_getName(), create_##T)
-
     REGISTER_READER(FauxReader, pdal::drivers::faux::Reader);
     REGISTER_READER(LasReader, pdal::drivers::las::LasReader);
 #ifdef PDAL_HAVE_LIBLAS
@@ -272,6 +289,7 @@ void StageFactory::registerKnownReaders()
 #ifdef PDAL_HAVE_ORACLE
     REGISTER_READER(OciReader, pdal::drivers::oci::Reader);
 #endif
+    REGISTER_READER(Reader, pdal::drivers::pipeline::Reader);
     REGISTER_READER(QfitReader, pdal::drivers::qfit::Reader);
     REGISTER_READER(TerrasolidReader, pdal::drivers::terrasolid::Reader);
 }
@@ -279,9 +297,6 @@ void StageFactory::registerKnownReaders()
 
 void StageFactory::registerKnownFilters()
 {
-#define REGISTER_FILTER(T, FullT) \
-    registerFilter(FullT::s_getName(), create_##T)
-
     REGISTER_FILTER(ByteSwapFilter, pdal::filters::ByteSwapFilter);
     REGISTER_FILTER(CacheFilter, pdal::filters::CacheFilter);
     REGISTER_FILTER(Chipper, pdal::filters::Chipper);
@@ -295,19 +310,13 @@ void StageFactory::registerKnownFilters()
 
 
 void StageFactory::registerKnownMultiFilters()
-{
-#define REGISTER_MULTIFILTER(T, FullT) \
-    registerMultiFilter(FullT::s_getName(), create_##T)
-    
+{   
     REGISTER_MULTIFILTER(MosaicFilter, pdal::filters::MosaicFilter);
 }
 
 
 void StageFactory::registerKnownWriters()
 {
-#define REGISTER_WRITER(T, FullT) \
-    registerWriter(FullT::s_getName(), create_##T)
-
     REGISTER_WRITER(FauxWriter, pdal::drivers::faux::Writer);
     REGISTER_WRITER(LasWriter, pdal::drivers::las::LasWriter);
 #ifdef PDAL_HAVE_LIBLAS
