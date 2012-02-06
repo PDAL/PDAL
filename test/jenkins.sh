1a0cbc1ab85fcb33d6edaa5bb02fb1a54e8bbeb2
Pete Gadomski <pete.gadomski@gmail.com>
2012-02-06 12:22:58 -0800

    Add ORACLE_HOME to jenkins test script

--- test/jenkins.sh
+++ test/jenkins.sh
@@ -2,6 +2,7 @@
 # Run the tests in a clean PDAL repository
 # "clean" == git clean -fdx
 
+export ORACLE_HOME="/u01/app/oracle/product/11.2.0/dbhome_1"
 cmake -G "Unix Makefiles" -C "test/BuildSetup.jenkins" .
 make
 ./bin/pdal_test test/data
