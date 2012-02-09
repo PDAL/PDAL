5eb80465efc487e46950e0cbdd271e7afd97168a
Pete Gadomski <pete.gadomski@gmail.com>
2012-02-08 20:53:19 -0800

    Add a 'PDAL-embed-boost' option to test/jenkins.sh
    This option will configure PDAL to use the embedded boost before testing.

--- test/jenkins.sh
+++ test/jenkins.sh
@@ -2,8 +2,12 @@
 # Run the tests in a clean PDAL repository
 # "clean" == git clean -fdx
 
+CMAKE_OPTS="-G \"Unix Makefiles\" -C test/BuildSetup.jenkins ."
+if [[ "$1" == "PDAL-embed-boost" ]]; then
+    CMAKE_OPTS="$CMAKE_OPTS -DPDAL_EMBED_BOOST:STRING=ON"
+fi
 export ORACLE_HOME="/u01/app/oracle/product/11.2.0/dbhome_1"
 export PATH="/var/lib/jenkins/local/bin:$PATH"
-cmake -G "Unix Makefiles" -C "test/BuildSetup.jenkins" .
+eval cmake $CMAKE_OPTS
 make
 ./bin/pdal_test test/data
