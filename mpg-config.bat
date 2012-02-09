73d5ecea0e796d103fd1ab0d8abba65416091d7b
Michael Rosen <michael.rosen@gmail.com>
2012-02-08 16:20:51 -0800

    new changes prior to rename

--- mpg-config.bat
+++ mpg-config.bat
@@ -1,108 +1,94 @@
 @echo off
+:: This configure script expects to be run from the PDAL root directory.
 
-:: Pick your architecture, 32 or 64 bits 
-rem set COMPILER="NMake Makefiles"
-rem set COMPILER="Visual Studio 10 Win64"
-set COMPILER="Visual Studio 10"
-
+:: Pick your CMake GENERATOR.  (NMake will pick up architecture (x32, x64) from your environment)
+rem set GENERATOR="NMake Makefiles"
+rem set GENERATOR="Visual Studio 10 Win64"
+set GENERATOR="Visual Studio 10"
 
 :: Pick your build type
-rem set BUILD_TYPE=Release
-set BUILD_TYPE=Debug
-
-
-:: Set some useful path variables
-::    "utils" is where you might have libraries installed, like Boost
-::    "dev" is where you build things yourself, like PDAL and LASzip
-set UTILS_DIR=c:\utils
-set DEV_DIR=c:\dev
-
-
-:: Where is your OSGeo4W installed?
-set OSGEO4W_DIR=C:\OSGeo4W
-
-
-:: Where is boost installed?
-set BOOST_DIR=%UTILS_DIR%\boost_1_47
-
-
-:: Where are the GLUT libs installed?
-::  (only needed if you want to build pcview)
-set FREEGLUT_DIR=%DEV_DIR%\freeglut-2.6.0-3.mp
-set GLUT_DIR=%DEV_DIR%\freeglut-2.6.0-3.mp
-
-:: Where is LASzip?  (can be either from OSGeo4W or your own build tree)
-rem  set LASZIP_LIBRARY=%OSGEO4W_DIR%\LASzip\laszip.lib
-rem  set LASZIP_INCLUDE_DIR=%OSGEO4W_DIR%\LASzip\include
-set LASZIP_LIBRARY=%DEV_DIR%\LASzip\bin\Debug\Debug\laszip.lib
-set LASZIP_INCLUDE_DIR=%DEV_DIR%\LASzip\include
-
-
-:: Where is LIBLAS?  (can be either from OSGeo4W or your own build tree)
-rem  set LIBLAS_LIBRARY=%OSGEO4W_DIR%\lib\liblas.lib
-rem  set LIBLAS_INCLUDE_DIR=%OSGEO4W_DIR%\include
-rem set LIBLAS_LIBRARY=%DEV_DIR%\liblas\bin\Debug\Debug\liblas.lib
-rem set LIBLAS_INCLUDE_DIR=%DEV_DIR%\liblas\include
-
+set BUILD_TYPE=Release
+rem set BUILD_TYPE=Debug
 
 :: Where is your PDAL build tree?
-set PDAL_DIR=%DEV_DIR%\pdal
+set PDAL_DIR=.
+rem set PDAL_DIR=c:\dev\pdal
 
+:: Where is your OSGeo4W installed (recommended basic way to satisfy dependent libs)
+set OSGEO4W_DIR=C:\OSGeo4W
 
-:: Where are the OCI libraries installed?
+:: Advanced users may want to use this as part of the dependency paths below
+set DEV_DIR=c:\dev
+
+:: Where is boost installed?
+rem set BOOST_DIR=%PDAL_DIR%\boost
+set BOOST_DIR=c:\utils\boost_1_48_0
+
+:: GDAL
+set GDAL_ENABLED=ON
+set GDAL_INCLUDE_DIR=%OSGEO4W_DIR%\apps\gdal-dev\include
+set GDAL_LIBRARY=%OSGEO4W_DIR%\apps\gdal-dev\lib\gdal_i.lib
+
+:: LIBTIFF
+set TIFF_ENABLED=ON
+set TIFF_INCLUDE_DIR=%OSGEO4W_DIR%\include 
+set TIFF_LIBRARY=%OSGEO4W_DIR%\lib\libtiff_i.lib 
+
+:: GeoTIFF
+set GEOTIFF_ENABLED=ON
+set GEOTIFF_INCLUDE_DIR=%OSGEO4W_DIR%\include 
+set GEOTIFF_LIBRARY=%OSGEO4W_DIR%\lib\geotiff_i.lib 
+
+:: LASZIP
+set LASZIP_ENABLED=ON
+set LASZIP_LIBRARY=%DEV_DIR%\laszip\bin\Debug\laszip.lib
+set LASZIP_INCLUDE_DIR=%DEV_DIR%\laszip\include
+
+:: Oracle
+set ORACLE_ENABLED=ON
 set ORACLE_HOME=%OSGEO4W_DIR%
+set ORACLE_INCLUDE_DIR=%ORACLE_HOME%\include
+set ORACLE_LIBRARY=%ORACLE_HOME%\lib\oci.lib
 
+:: LibXML2
+set LIBXML2_ENABLED=ON
+set LIBXML2_INCLUDE_DIR=%OSGEO4W_DIR%\include
+set LIBXML2_LIBRARY=%OSGEO4W_DIR%\lib\libxml2.lib
+
+:: IConv
+set ICONV_ENABLED=ON
+set ICONV_INCLUDE_DIR=%OSGEO4W_DIR%\include
+set ICONV_LIBRARY=%OSGEO4W_DIR%\lib\iconv.lib
 
 :: Set this if you are building SWIG bindings for C#.  Visual Studio
 :: needs to use this env var to find where boost lives.
+set PDAL_SWIG_ENABLED=ON
 set PDAL_SWIG_BOOST_HOME=%BOOST_DIR%
 
-
-:: If you set the above stuff correctly, then you should only need to
-:: modify the "WITH" (on/off) settings in the following CMAKE invocation.
-::
-:: For most people, you should have these turned ON:
-::    GDAL
-::    GEOTIFF
-::    ORACLE
-::    LASZIP
-::    LIBXML2
-:: and these turned OFF:
-::    LIBLAS
-::    FREEGLUT
-::    SWIG_CSHARP
-cmake -G %COMPILER% ^
+if EXIST CMakeCache.txt del CMakeCache.txt
+cmake -G %GENERATOR% ^
     -DBOOST_INCLUDEDIR=%BOOST_DIR% ^
-    -DWITH_GDAL=ON ^
-    -DWITH_GEOTIFF=ON ^
-    -DWITH_ORACLE=ON ^
-    -DWITH_LASZIP=ON ^
-    -DWITH_LIBLAS=OFF ^
-    -DWITH_FREEGLUT=ON ^
-    -DWITH_LIBXML2=ON ^
-    -DWITH_SWIG_CSHARP=ON ^
-	-DWITH_PLANG=ON ^
-    -DFREEGLUT_LIBRARY=%FREEGLUT_DIR%\lib\freeglut.lib ^
-    -DFREEGLUT_INCLUDE_DIR=%FREEGLUT_DIR%\include ^
-    -DGLUT_LIBRARY=%FREEGLUT_DIR%\lib ^
-    -DGLUT_INCLUDE_DIR=%FREEGLUT_DIR%\include ^
-    -DTIFF_INCLUDE_DIR=%OSGEO4W_DIR%\include ^
-    -DTIFF_LIBRARY=%OSGEO4W_DIR%\lib\libtiff_i.lib ^
-    -DGEOTIFF_INCLUDE_DIR=%OSGEO4W_DIR%\include ^
-    -DGEOTIFF_LIBRARY=%OSGEO4W_DIR%\lib\geotiff_i.lib ^
-    -DGDAL_INCLUDE_DIR=%OSGEO4W_DIR%\include ^
-    -DGDAL_LIBRARY=%OSGEO4W_DIR%\lib\gdal_i.lib ^
-    -DORACLE_INCLUDE_DIR=%OSGEO4W_DIR%\include ^
-    -DORACLE_OCI_LIBRARY=%OSGEO4W_DIR%\lib\oci.lib ^
+    -DWITH_GDAL=%GDAL_ENABLED% ^
+    -DWITH_GEOTIFF=%GEOTIFF_ENABLED% ^
+    -DWITH_ORACLE=%ORACLE_ENABLED% ^
+    -DWITH_LASZIP=%LASZIP_ENABLED% ^
+    -DWITH_LIBXML2=%LIBXML2_ENABLED% ^
+    -DWITH_SWIG_CSHARP=%PDAL_SWIG_ENABLED% ^
+    -DWITH_PLANG=ON ^
+    -DTIFF_INCLUDE_DIR=%TIFF_INCLUDE_DIR% ^
+    -DTIFF_LIBRARY=%TIFF_LIBRARY% ^
+    -DGEOTIFF_INCLUDE_DIR=%GEOTIFF_INCLUDE_DIR% ^
+    -DGEOTIFF_LIBRARY=%GEOTIFF_LIBRARY% ^
+    -DGDAL_INCLUDE_DIR=%GDAL_INCLUDE_DIR% ^
+    -DGDAL_LIBRARY=%GDAL_LIBRARY% ^
+    -DORACLE_INCLUDE_DIR=%ORACLE_INCLUDE_DIR% ^
+    -DORACLE_OCI_LIBRARY=%ORACLE_LIBRARY% ^
     -DLASZIP_INCLUDE_DIR=%LASZIP_INCLUDE_DIR% ^
     -DLASZIP_LIBRARY=%LASZIP_LIBRARY% ^
-    -DLIBXML2_LIBRARIES=%OSGEO4W_DIR%\lib\libxml2.lib ^
-    -DLIBXML2_INCLUDE_DIR=%OSGEO4W_DIR%\include ^
-	-DICONV_LIBRARY=%OSGEO4W_DIR%\lib\iconv.lib ^
-	-DICONV_INCLUDE_DIR=%OSGEO4W_DIR%\include ^
+    -DLIBXML2_LIBRARIES=%LIBXML2_LIBRARY% ^
+    -DLIBXML2_INCLUDE_DIR=%LIBXML2_INCLUDE_DIR% ^
+    -DICONV_LIBRARY=%ICONV_LIBRARY% ^
+    -DICONV_INCLUDE_DIR=%ICONV_INCLUDE_DIR% ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_VERBOSE_MAKEFILE=OFF ^
     %PDAL_DIR%
-
-rem     -DLIBLAS_INCLUDE_DIR=%LIBLAS_INCLUDE_DIR% ^
-rem     -DLIBLAS_LIBRARY=%LIBLAS_LIBRARY% ^
