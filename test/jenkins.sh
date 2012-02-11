a8b37f680b96bc4c66c87b5be12830b9a549651a
Howard Butler <hobu.inc@gmail.com>
2012-02-11 14:00:35 -0600

    tweak embed build option some more

--- test/jenkins.sh
+++ test/jenkins.sh
@@ -2,9 +2,11 @@
 # Run the tests in a clean PDAL repository
 # "clean" == git clean -fdx
 
-CMAKE_OPTS="-G \"Unix Makefiles\" -C test/BuildSetup.jenkins ."
+CMAKE_OPTS="-G \"Unix Makefiles\""
 if [ "$1" == "PDAL-embed-boost" ]; then
-    CMAKE_OPTS="$CMAKE_OPTS -DPDAL_EMBED_BOOST:BOOL=ON"
+    CMAKE_OPTS="$CMAKE_OPTS -C test/BuildSetup-embed.jenkins ."
+else
+    CMAKE_OPTS="$CMAKE_OPTS -C test/BuildSetup.jenkins ."
 fi
 export ORACLE_HOME="/u01/app/oracle/product/11.2.0/dbhome_1"
 export PATH="/var/lib/jenkins/local/bin:$PATH"
