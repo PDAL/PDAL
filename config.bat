7695bb105904a3432923724cb07b2f13f65ebf75
Michael P. Gerlek <mpg@flaxen.com>
2012-03-02 11:59:21 -0800

    fix various issues with embeddedboost on windows

--- config.bat
+++ config.bat
@@ -1,28 +1,28 @@
 @echo off
+
+:: This configure script is designed for the default Windows world, which means
+:: you have OSGeo4W installed, including Oracle and GDAL and LASzip.
+
 :: This configure script expects to be run from the PDAL root directory.
 
 :: Pick your CMake GENERATOR.  (NMake will pick up architecture (x32, x64) from your environment)
 rem set GENERATOR="NMake Makefiles"
-rem set GENERATOR="Visual Studio 10 Win64"
+set GENERATOR="Visual Studio 10 Win64"
 set GENERATOR="Visual Studio 10"
 
 :: Pick your build type
 set BUILD_TYPE=Release
-rem set BUILD_TYPE=Debug
+set BUILD_TYPE=Debug
 
 :: Where is your PDAL build tree?
 set PDAL_DIR=.
-rem set PDAL_DIR=c:\dev\pdal
 
 :: Where is your OSGeo4W installed (recommended basic way to satisfy dependent libs)
 set OSGEO4W_DIR=C:\OSGeo4W
 
-:: Advanced users may want to use this as part of the dependency paths below
-set DEV_DIR=c:\dev
-
 :: Where is boost installed?
-rem set BOOST_DIR=%PDAL_DIR%\boost
-set BOOST_DIR=c:\utils\boost_1_48_0
+rem set BOOST_DIR=c:\utils\boost_1_48_0
+set BOOST_DIR=%PDAL_DIR%\boost
 
 :: GDAL
 set GDAL_ENABLED=ON
@@ -41,8 +41,8 @@ set GEOTIFF_LIBRARY=%OSGEO4W_DIR%\lib\geotiff_i.lib
 
 :: LASZIP
 set LASZIP_ENABLED=ON
-set LASZIP_LIBRARY=%DEV_DIR%\laszip\bin\Debug\laszip.lib
-set LASZIP_INCLUDE_DIR=%DEV_DIR%\laszip\include
+set LASZIP_INCLUDE_DIR=%OSGEO4W_DIR%\include
+set LASZIP_LIBRARY=%OSGEO4W_DIR%\lib\laszip.lib
 
 :: Oracle
 set ORACLE_ENABLED=ON
@@ -60,21 +60,20 @@ set ICONV_ENABLED=ON
 set ICONV_INCLUDE_DIR=%OSGEO4W_DIR%\include
 set ICONV_LIBRARY=%OSGEO4W_DIR%\lib\iconv.lib
 
-:: Set this if you are building SWIG bindings for C#.  Visual Studio
+:: Set this if you are building SWIG bindings for C#. Visual Studio
 :: needs to use this env var to find where boost lives.
 set PDAL_SWIG_ENABLED=ON
 set PDAL_SWIG_BOOST_HOME=%BOOST_DIR%
 
-if EXIST CMakeCache.txt del CMakeCache.txt
+rem if EXIST CMakeCache.txt del CMakeCache.txt
 cmake -G %GENERATOR% ^
-    -DBOOST_INCLUDEDIR=%BOOST_DIR% ^
     -DWITH_GDAL=%GDAL_ENABLED% ^
     -DWITH_GEOTIFF=%GEOTIFF_ENABLED% ^
     -DWITH_ORACLE=%ORACLE_ENABLED% ^
     -DWITH_LASZIP=%LASZIP_ENABLED% ^
     -DWITH_LIBXML2=%LIBXML2_ENABLED% ^
     -DWITH_SWIG_CSHARP=%PDAL_SWIG_ENABLED% ^
-    -DWITH_PLANG=ON ^
+    -DWITH_ICONV=%ICONV_ENABLED% ^
     -DTIFF_INCLUDE_DIR=%TIFF_INCLUDE_DIR% ^
     -DTIFF_LIBRARY=%TIFF_LIBRARY% ^
     -DGEOTIFF_INCLUDE_DIR=%GEOTIFF_INCLUDE_DIR% ^
@@ -92,3 +91,7 @@ cmake -G %GENERATOR% ^
     -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
     -DCMAKE_VERBOSE_MAKEFILE=OFF ^
     %PDAL_DIR%
+
+
+rem    -DWITH_PLANG=ON ^
+rem    -DBOOST_INCLUDEDIR=%BOOST_DIR% ^
