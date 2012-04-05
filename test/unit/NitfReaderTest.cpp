0b6f03c57d8b5e3104592b1f0fdc898f2a3415a1
Howard Butler <hobu.inc@gmail.com>
2012-04-05 10:40:31 -0500

    fix up broken test

--- test/unit/NitfReaderTest.cpp
+++ test/unit/NitfReaderTest.cpp
@@ -73,9 +73,9 @@ BOOST_AUTO_TEST_CASE(test_one)
         /////////////////////////////////////////////////BOOST_CHECK_EQUAL(metadatums.size(), 80u);
         
         
-        pdal::metadata::Entry const& m = metadata.getMetadata("drivers.nitf.reader.FH.FDT");
+        pdal::metadata::Entry const& m = metadata.getMetadata("FH.FDT");
 
-        BOOST_CHECK_EQUAL(m.getName(), "drivers.nitf.reader.FH.FDT");
+        BOOST_CHECK_EQUAL(m.getName(), "FH.FDT");
 
         const std::string s(m.getValue<std::string>());
         BOOST_CHECK_EQUAL(s, "20120323002946");
