8b7b7b9fb4fccf216b2723c7f35269a16dd8655b
Howard Butler <hobu.inc@gmail.com>
2012-04-19 14:18:28 -0500

    add metadata entries for stats filter options

--- src/filters/Stats.cpp
+++ src/filters/Stats.cpp
@@ -87,6 +87,16 @@ boost::property_tree::ptree Summary::toPTree() const
 Stats::Stats(Stage& prevStage, const Options& options)
     : pdal::Filter(prevStage, options)
 {
+    Metadata& metadata = getMetadataRef();
+    metadata.addEntry<boost::uint32_t>( "sample_size", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("sample_size", 1000));
+    metadata.addEntry<boost::uint32_t>( "seed", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("seed", 0));
+    metadata.addEntry<boost::uint32_t>( "num_bins", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("num_bins", 20));
+    metadata.addEntry<boost::uint32_t>( "stats_cache_size", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("num_bins", 20));
+    
     return;
 }
 
@@ -94,6 +104,16 @@ Stats::Stats(Stage& prevStage, const Options& options)
 Stats::Stats(Stage& prevStage)
     : Filter(prevStage, Options::none())
 {
+    Metadata& metadata = getMetadataRef();
+    metadata.addEntry<boost::uint32_t>( "sample_size", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("sample_size", 1000));
+    metadata.addEntry<boost::uint32_t>( "seed", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("seed", 0));
+    metadata.addEntry<boost::uint32_t>( "num_bins", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("num_bins", 20));
+    metadata.addEntry<boost::uint32_t>( "stats_cache_size", 
+                        getOptions().getValueOrDefault<boost::uint32_t>("num_bins", 20));
+    
     return;
 }
 
