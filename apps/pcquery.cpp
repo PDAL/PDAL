f9a422f24c0e46170e3c93c4a7a1a15961daec98
Howard Butler <hobu.inc@gmail.com>
2012-07-19 10:41:39 -0500

    add pcquery scaffolding

---
+++ apps/pcquery.cpp
@@ -0,0 +1,207 @@
+/******************************************************************************
+* Copyright (c) 2012, Howard Butler (hobu.inc@gmail.com)
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following
+* conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in
+*       the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
+*       names of its contributors may be used to endorse or promote
+*       products derived from this software without specific prior
+*       written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+* OF SUCH DAMAGE.
+****************************************************************************/
+
+
+#include <iostream>
+
+#include <boost/scoped_ptr.hpp>
+
+#include <pdal/Stage.hpp>
+#include <pdal/StageIterator.hpp>
+#include <pdal/FileUtils.hpp>
+#include <pdal/PointBuffer.hpp>
+#include <pdal/filters/Index.hpp>
+
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include "AppSupport.hpp"
+#include "Application.hpp"
+
+#ifdef PDAL_HAVE_GEOS
+#include <geos_c.h>
+
+
+namespace pcquery
+{
+    static void _GEOSErrorHandler(const char *fmt, ...)
+    {
+        va_list args;
+
+        va_start(args, fmt);
+        char buf[1024];  
+
+        vsnprintf( buf, sizeof( buf), fmt, args);
+        std::cerr << "GEOS Error: " << buf << std::endl;
+
+        va_end(args);
+    }
+
+    static void _GEOSWarningHandler(const char *fmt, ...)
+    {
+        va_list args;
+
+        char buf[1024];  
+        vsnprintf( buf, sizeof( buf), fmt, args);
+        std::cout << "GEOS warning: " << buf << std::endl;
+
+        va_end(args);
+    }
+
+} // geos
+
+#endif
+
+using namespace pdal;
+
+
+class PcQuery : public Application
+{
+public:
+    PcQuery(int argc, char* argv[]);
+    int execute(); // overrride
+
+private:
+    void addSwitches(); // overrride
+    void validateSwitches(); // overrride
+
+    std::string m_inputFile;
+    std::string m_wkt;
+
+#ifdef PDAL_HAVE_GEOS
+	GEOSContextHandle_t m_geosEnvironment;
+#endif
+	
+    pdal::Options m_options;
+};
+
+
+PcQuery::PcQuery(int argc, char* argv[])
+    : Application(argc, argv, "pcquery")
+    , m_inputFile("")
+{
+    return;
+}
+
+
+void PcQuery::validateSwitches()
+{
+    
+    if (m_wkt.size())
+    {
+        // read the WKT using GEOS
+        m_geosEnvironment = initGEOS_r(pcquery::_GEOSWarningHandler, pcquery::_GEOSErrorHandler);
+        GEOSWKTReader* reader = GEOSWKTReader_create_r(m_geosEnvironment);
+        GEOSGeometry* geom = GEOSWKTReader_read_r(m_geosEnvironment, reader, m_wkt.c_str());
+        if (!geom)
+            throw app_runtime_error("unable to ingest given WKT string");
+            
+    }
+    return;
+}
+
+
+void PcQuery::addSwitches()
+{
+    namespace po = boost::program_options;
+
+    po::options_description* file_options = new po::options_description("file options");
+    
+
+    file_options->add_options()
+        ("input,i", po::value<std::string>(&m_inputFile)->default_value(""), "input file name")
+        ("point", po::value< std::vector<float> >()->multitoken(), "A 2d or 3d point to use for querying")
+        ("wkt", po::value<std::string>(&m_wkt)->default_value(""), "WKT object to use for querying")
+        ;
+
+    addSwitchSet(file_options);
+
+    po::options_description* processing_options = new po::options_description("processing options");
+    
+    processing_options->add_options()
+
+        ;
+    
+    addSwitchSet(processing_options);
+
+    addPositionalSwitch("input", 1);
+
+    return;
+}
+
+
+
+int PcQuery::execute()
+{
+
+
+    Options readerOptions;
+    {
+        if (m_usestdin)
+            m_inputFile = "STDIN";
+        readerOptions.add<std::string>("filename", m_inputFile);
+        readerOptions.add<bool>("debug", isDebug());
+        readerOptions.add<boost::uint32_t>("verbose", getVerboseLevel());
+    }
+
+    Stage* reader = AppSupport::makeReader(readerOptions);
+
+    
+
+    pdal::Options options = m_options + readerOptions;
+    
+    pdal::filters::Index* filter = new pdal::filters::Index(*reader, options);
+
+    filter->initialize();
+
+
+    
+    
+    std::cout << std::endl;
+    
+    delete filter;
+    delete reader;
+
+
+    
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    PcQuery app(argc, argv);
+    return app.run();
+}
+
