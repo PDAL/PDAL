8fa51bbc629bbf8977b3bbfa79cbd489043ea994
Michael P. Gerlek <mpg@flaxen.com>
2012-07-28 15:23:11 -0700

    vs2010 lint

--- test/unit/drivers/nitf/NitfReaderTest.cpp
+++ test/unit/drivers/nitf/NitfReaderTest.cpp
@@ -159,7 +159,7 @@ BOOST_AUTO_TEST_CASE(test_chipper)
     
     StageSequentialIterator* iter = reader.createSequentialIterator(data);
     const boost::uint32_t num_read = iter->read(data);
-    
+    BOOST_CHECK_EQUAL(num_read, 13u);    
     
     boost::uint32_t num_blocks = chipper->GetBlockCount();
     BOOST_CHECK_EQUAL(num_blocks, 8u);
