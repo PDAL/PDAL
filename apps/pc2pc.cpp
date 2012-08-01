88aac316e95e83ca1f8d05a49854429a7098447e
Howard Butler <hobu.inc@gmail.com>
2012-08-01 13:52:09 -0500

    add --t_srs, --a_srs, --bounds, and --polygon options to pc2pc #40

--- apps/pc2pc.cpp
+++ apps/pc2pc.cpp
@@ -43,8 +43,11 @@
 
 #include <pdal/filters/Cache.hpp>
 #include <pdal/filters/Chipper.hpp>
-#include <pdal/filters/Reprojection.hpp>
+#include <pdal/Filters/Crop.hpp>
+#include <pdal/filters/InPlaceReprojection.hpp>
 #include <pdal/filters/Scaling.hpp>
+#include <pdal/SpatialReference.hpp>
+#include <pdal/Bounds.hpp>
 
 #include <boost/property_tree/xml_parser.hpp>
 
@@ -67,10 +70,13 @@ private:
 
     std::string m_inputFile;
     std::string m_outputFile;
-    std::string m_srs;
     bool m_bCompress;
     boost::uint64_t m_numPointsToWrite; 
     boost::uint64_t m_numSkipPoints;
+    pdal::SpatialReference m_input_srs;
+    pdal::SpatialReference m_output_srs;
+    pdal::Bounds<double> m_bounds;
+    std::string m_wkt;
 };
 
 
@@ -78,10 +84,12 @@ Pc2Pc::Pc2Pc(int argc, char* argv[])
     : Application(argc, argv, "pc2pc")
     , m_inputFile("")
     , m_outputFile("")
-    , m_srs("")
     , m_bCompress(false)
     , m_numPointsToWrite(0)
     , m_numSkipPoints(0)
+    , m_input_srs(pdal::SpatialReference())
+    , m_output_srs(pdal::SpatialReference())
+    , m_wkt("")
 {
     return;
 }
@@ -112,10 +120,13 @@ void Pc2Pc::addSwitches()
     file_options->add_options()
         ("input,i", po::value<std::string>(&m_inputFile)->default_value(""), "input file name")
         ("output,o", po::value<std::string>(&m_outputFile)->default_value(""), "output file name")
-        ("a_srs", po::value<std::string>(&m_srs)->default_value(""), "Assign output coordinate system (if supported by output format)")
+        ("a_srs", po::value<pdal::SpatialReference>(&m_input_srs), "Assign input coordinate system (if supported by output format)")
+        ("t_srs", po::value<pdal::SpatialReference>(&m_output_srs), "Transform to output coordinate system (if supported by output format)")
         ("compress,z", po::value<bool>(&m_bCompress)->zero_tokens()->implicit_value(true), "Compress output data (if supported by output format)")
         ("count", po::value<boost::uint64_t>(&m_numPointsToWrite)->default_value(0), "How many points should we write?")
         ("skip", po::value<boost::uint64_t>(&m_numSkipPoints)->default_value(0), "How many points should we skip?")
+        ("bounds", po::value<pdal::Bounds<double> >(&m_bounds), "Extent (in XYZ to clip output to)")
+        ("polygon", po::value<std::string >(&m_wkt), "POLYGON WKT to use for precise crop of data (2d or 3d)")
         ;
 
     addSwitchSet(file_options);
@@ -132,6 +143,11 @@ int Pc2Pc::execute()
         readerOptions.add<std::string>("filename", m_inputFile);
         readerOptions.add<bool>("debug", isDebug());
         readerOptions.add<boost::uint32_t>("verbose", getVerboseLevel());
+
+        if (!m_input_srs.empty())
+        {
+            readerOptions.add<std::string>("spatialreference", m_input_srs.getWKT());
+        }
     }
 
     Options writerOptions;
@@ -140,9 +156,9 @@ int Pc2Pc::execute()
         writerOptions.add<bool>("debug", isDebug());
         writerOptions.add<boost::uint32_t>("verbose", getVerboseLevel());
 
-        if (m_srs != "")
+        if (!m_input_srs.empty())
         {
-            writerOptions.add<std::string>("a_srs", m_srs);
+            writerOptions.add<std::string>("spatialreference", m_input_srs.getWKT());
         }
 
         if (m_bCompress)
@@ -156,13 +172,77 @@ int Pc2Pc::execute()
         }
     }
 
-    Stage* stage = AppSupport::makeReader(readerOptions);
+    Stage* reader_stage = AppSupport::makeReader(readerOptions);
+    
+    Stage* final_stage(0);
+    if (!m_bounds.empty() || !m_wkt.empty() || !m_output_srs.empty())
+    {
+        Stage* next_stage = reader_stage;
+        
+        Stage* crop_stage(0);
+        Stage* reprojection_stage(0);
 
-    Writer* writer = AppSupport::makeWriter(writerOptions, *stage);
+        if (!m_output_srs.empty())
+        {
+            readerOptions.add<std::string >("out_srs", m_output_srs.getWKT());
+            if (m_output_srs.isGeographic())
+            {
+                readerOptions.add<double >("scale_x", 0.0000001);
+                readerOptions.add<double >("scale_y", 0.0000001);                
+            } else
+            {
+                readerOptions.add<double >("scale_x", 0.01);
+                readerOptions.add<double >("scale_y", 0.01);
+            }
+            reprojection_stage = new pdal::filters::InPlaceReprojection(*next_stage, readerOptions);
+            next_stage = reprojection_stage;
+        }
+        
+        if (!m_bounds.empty() && m_wkt.empty())
+        {
+            readerOptions.add<pdal::Bounds<double> >("bounds", m_bounds);
+            crop_stage = new pdal::filters::Crop(*next_stage, readerOptions);
+            next_stage = crop_stage;
+        } 
+        else if (m_bounds.empty() && !m_wkt.empty())
+        {
+            std::istream* wkt_stream;
+            try
+            {
+                wkt_stream = FileUtils::openFile(m_wkt);
+                std::stringstream buffer;
+                buffer << wkt_stream->rdbuf();
+
+                m_wkt = buffer.str();
+                
+            } catch (pdal::pdal_error const&)
+            {
+                // If we couldn't open the file given in m_wkt because it 
+                // was likely actually wkt, leave it alone
+            }
+            readerOptions.add<std::string >("polygon", m_wkt);
+            crop_stage = new pdal::filters::Crop(*next_stage, readerOptions);
+            next_stage = crop_stage;
+        }
+
+
+        
+        final_stage = next_stage;
+    }
+    
+    if (final_stage == 0) 
+        final_stage = reader_stage;
+    
+    Writer* writer = AppSupport::makeWriter(writerOptions, *final_stage);
+
+    if (!m_output_srs.empty())
+    {
+        writer->setSpatialReference(m_output_srs);
+    }
 
     writer->initialize();
 
-    const boost::uint64_t numPointsToRead = stage->getNumPoints();
+    const boost::uint64_t numPointsToRead = final_stage->getNumPoints();
     boost::scoped_ptr<pdal::UserCallback> callback((numPointsToRead == 0) ? 
         (pdal::UserCallback*)(new HeartbeatCallback) :
         (pdal::UserCallback*)(new PercentageCallback));
@@ -173,7 +253,7 @@ int Pc2Pc::execute()
     std::cout << "Wrote " << numPointsRead << " points\n";
 
     delete writer;
-    delete stage;
+    delete final_stage;
 
     return 0;
 }
