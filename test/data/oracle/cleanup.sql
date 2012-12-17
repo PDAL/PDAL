c9a8e0919117bf7469e94914223b79f2ed28f0ea
Howard Butler <hobu.inc@gmail.com>
2012-12-17 17:05:14 -0600

    tweak configuration script

--- test/data/oracle/cleanup.sql
+++ test/data/oracle/cleanup.sql
@@ -1,8 +1,27 @@
-select table_name from user_tables;
-drop table SIMPLE_CLOUD;
-delete from SIMPLE_BLOCKS;
-drop table SIMPLE_BLOCKS;
-CREATE TABLE SIMPLE_CLOUD (id number, CLOUD SDO_PC, BOUNDARY SDO_GEOMETRY);
+set echo on;
+declare 
+    blks VARCHAR(40);
+    clouds VARCHAR(40);
+    name VARCHAR(40);
+begin
 
-delete from user_sdo_geom_metadata where table_name = 'SIMPLE_BLOCKS';
+name := '&1';
+clouds := name ||'_CLOUD_095';
+blks := name ||'_BLOCKS_095';
+
+FOR i IN (SELECT table_name FROM user_tables WHERE table_name = clouds) LOOP
+    EXECUTE IMMEDIATE 'DROP TABLE ' || clouds;
+END LOOP;
+
+FOR i IN (SELECT table_name FROM user_tables WHERE table_name = blks) LOOP
+    EXECUTE IMMEDIATE 'delete from ' || blks;
+    EXECUTE IMMEDIATE 'DROP TABLE ' || blks;
+END LOOP;
+
+execute immediate 'CREATE TABLE '|| clouds ||'(id number, CLOUD SDO_PC, BOUNDARY SDO_GEOMETRY)';
+execute immediate 'delete from user_sdo_geom_metadata where table_name = '' || blks || ''';
+
+end;
+/
+
 exit;
