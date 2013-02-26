1283bfcf26adef8fcb7dab2260759acdb362d7ca
Howard Butler <hobu.inc@gmail.com>
2013-02-26 13:08:48 -0600

    PURGE to ensure that dumped tables don't end up in the recycling bin

--- test/data/oracle/cleanup.sql
+++ test/data/oracle/cleanup.sql
@@ -10,12 +10,12 @@ clouds := name ||'_CLOUD';
 blks := name ||'_BLOCK';
 
 FOR i IN (SELECT table_name FROM user_tables WHERE table_name = clouds) LOOP
-    EXECUTE IMMEDIATE 'DROP TABLE ' || clouds;
+    EXECUTE IMMEDIATE 'DROP TABLE ' || clouds || ' PURGE';
 END LOOP;
 
 FOR i IN (SELECT table_name FROM user_tables WHERE table_name = blks) LOOP
     EXECUTE IMMEDIATE 'delete from ' || blks;
-    EXECUTE IMMEDIATE 'DROP TABLE ' || blks;
+    EXECUTE IMMEDIATE 'DROP TABLE ' || blks || ' PURGE';
 END LOOP;
 
 execute immediate 'CREATE TABLE '|| clouds ||'(id number, CLOUD SDO_PC, BOUNDARY SDO_GEOMETRY)';
