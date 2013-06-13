1f21d25a615f6aa0b4ae7fd2caf2b2817ea17b56
Paul Ramsey <pramsey@opengeo.org>
2013-06-13 16:11:42 -0700

    Update for pgpointcloud

--- pramsey-config.sh
+++ pramsey-config.sh
@@ -7,10 +7,10 @@ LIBXML2_HOME=$OPT
 GEOTIFF_HOME=$USR_LOCAL
 P2G_HOME=$USR_LOCAL
 SO_EXT=dylib
-EMBED=OFF
+EMBED=ON
 CC=/usr/bin/gcc
 CXX=/usr/bin/g++
-CPPCLAGS=-I/usr/pgsql/9.2/include
+CPPCLAGS=-I/usr/pgsql-9.2/include
 
 ORACLE_HOME=$HOME/oracle
 export ORACLE_HOME
@@ -32,24 +32,24 @@ cmake   -G "$CONFIG"  \
         -DWITH_LIBXML2=ON \
         -DWITH_PYTHON=ON \
         -DWITH_FLANN=OFF \
-        -DWITH_P2G=ON \
-        -DGEOTIFF_INCLUDE_DIR=${GEOTIFF_HOME}/include/ \
+        -DWITH_P2G=OFF \
+        -DWITH_SOCI=OFF \
+        -DWITH_PGPOINTCLOUD=ON \
+        -DGEOTIFF_INCLUDE_DIR=${GEOTIFF_HOME}/include \
         -DGEOTIFF_LIBRARY=${GEOTIFF_HOME}/lib/libgeotiff.${SO_EXT} \
         -DICONV_INCLUDE_DIR=/usr/include \
-        -DP2G_INCLUDE_DIR=${P2G_HOME}/include \
-        -DP2G_LIBRARY=${P2G_HOME}/lib/libpts2grd.${SO_EXT} \
         -DLIBXML2_INCLUDE_DIR=${LIBXML2_HOME}/include/libxml2 \
         -DLIBXML2_LIBRARIES=${LIBXML2_HOME}/lib/libxml2.${SO_EXT} \
         -DTIFF_INCLUDE_DIR=/${TIFF_HOME}/include \
         -DTIFF_LIBRARY=${TIFF_HOME}/lib/libtiff.${SO_EXT} \
-        -DWITH_SOCI=ON \
-	-DSOCI_INCLUDE_DIR=${USR_LOCAL}/include/soci\;${USR_LOCAL}/pgsql/9.2/include \
-	-DSOCI_LIBRARY=${USR_LOCAL}/lib/libsoci_core.${SO_EXT} \
-	-DSOCI_postgresql_PLUGIN=${USR_LOCAL}/lib/libsoci_postgresql.${SO_EXT} ../PDAL
+	-DPOSTGRESQL_INCLUDE_DIR=${USR_LOCAL}/pgsql/9.2/include \
+	-DPOSTGRESQL_LIBRARIES=${USR_LOCAL}/pgsql/9.2/lib/libpq.${SO_EXT} \
+	../PDAL
 
-#        -DLASZIP_INCLUDE_DIR=${LASZIP_HOME}/include \
-#        -DLASZIP_LIBRARY=${LASZIP_HOME}/lib/liblaszip.${SO_EXT} \
-    # -DUSE_PDAL_PLUGIN_SOCI=ON \
-    # -DUSE_PDAL_PLUGIN_PCD=ON \
-    # -DUSE_PDAL_PLUGIN_OCI=ON \
-    # -DUSE_PDAL_PLUGIN_TEXT=ON \
+
+# -DLASZIP_INCLUDE_DIR=${LASZIP_HOME}/include \
+# -DLASZIP_LIBRARY=${LASZIP_HOME}/lib/liblaszip.${SO_EXT} \
+# -DUSE_PDAL_PLUGIN_SOCI=ON \
+# -DUSE_PDAL_PLUGIN_PCD=ON \
+# -DUSE_PDAL_PLUGIN_OCI=ON \
+# -DUSE_PDAL_PLUGIN_TEXT=ON \
