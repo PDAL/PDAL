2d3fc3e82fcf396002db5c9daac258cb3271068b
Howard Butler <hobu.inc@gmail.com>
2013-06-17 12:55:58 -0500

    increment SHA

--- src/gitsha.cpp
+++ src/gitsha.cpp
@@ -1,3 +1,3 @@
 #include <pdal/gitsha.h>
-#define GIT_SHA1 "918693d1d8c83b9bb4c77ec9cadd68fc25a05196"
+#define GIT_SHA1 "74a6cc62b4fab4e0fb73dc5d95316f0da957c864"
 const char g_GIT_SHA1[] = GIT_SHA1;
