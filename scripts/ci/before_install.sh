60e07ab6df03e184eacedef3ddc8c900b8b2e2c8
Mateusz Loskot <mateusz@loskot.net>
2013-06-26 23:18:01 +0100

    Enable verbose apt-get

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -4,7 +4,7 @@ source ./scripts/ci/common.sh
 #echo "$(tmstamp) *** before_install::apt-get starting $(date) ***"
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get update -qq
-sudo apt-get install -qq cmake libgdal-dev libproj-dev libxml2-dev 
+sudo apt-get install cmake libgdal-dev libproj-dev libxml2-dev
 #echo "$(tmstamp) *** before_install::apt-get finished $(date) ***"
 gcc --version
 clang --version
