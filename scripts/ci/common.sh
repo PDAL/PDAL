22efc649ea1723b95ef57cafbe7045c38d7b61cd
Mateusz Loskot <mateusz@loskot.net>
2013-06-26 21:41:07 +0100

    Enable make -j N

--- scripts/ci/common.sh
+++ scripts/ci/common.sh
@@ -11,5 +11,5 @@ if [[ -f /sys/devices/system/cpu/online ]]; then
 	# Calculates 1.5 times physical threads
 	NUMTHREADS=$(( ( $(cut -f 2 -d '-' /sys/devices/system/cpu/online) + 1 ) * 15 / 10  ))
 fi
-NUMTHREADS=1 # disable MP
+#NUMTHREADS=1 # disable MP
 export NUMTHREADS
