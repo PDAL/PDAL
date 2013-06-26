d5a4fdc2b114e958310736912d75515875fd3630
Mateusz Loskot <mateusz@loskot.net>
2013-06-26 21:50:20 +0100

    Disable timestamp echo.
    It seems that echo function aliases return code from cmake/make/ctest.

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Installs requirements for PDAL
 source ./scripts/ci/common.sh
-echo "$(tmstamp) *** before_install::apt-get starting $(date) ***"
+#echo "$(tmstamp) *** before_install::apt-get starting $(date) ***"
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get update -qq
 sudo apt-get install -qq cmake 
-echo "$(tmstamp) *** before_install::apt-get finished $(date) ***"
+#echo "$(tmstamp) *** before_install::apt-get finished $(date) ***"
 gcc --version
 clang --version
--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -4,14 +4,14 @@ source ./scripts/ci/common.sh
 mkdir -p _build
 cd _build
 
-echo "$(tmstamp) *** script::cmake-config starting $(date) ***"
+#echo "$(tmstamp) *** script::cmake-config starting $(date) ***"
 cmake ..
-echo "$(tmstamp) *** script::cmake-config finished $(date) ***"
+#echo "$(tmstamp) *** script::cmake-config finished $(date) ***"
 
-echo "$(tmstamp) *** script::cmake-build make -j ${NUMTHREADS} $(date) ***"
+#echo "$(tmstamp) *** script::cmake-build make -j ${NUMTHREADS} $(date) ***"
 make -j ${NUMTHREADS}
-echo "$(tmstamp) *** script::cmake-build finished $(date) ***"
+#echo "$(tmstamp) *** script::cmake-build finished $(date) ***"
 
-echo "$(tmstamp) *** script::cmake-test starting $(date) ***"
+#echo "$(tmstamp) *** script::cmake-test starting $(date) ***"
 ctest -V --output-on-failure .
-echo "$(tmstamp) *** script::cmake-test finished $(date) ***"
+#echo "$(tmstamp) *** script::cmake-test finished $(date) ***"
