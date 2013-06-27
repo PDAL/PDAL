0d5c87c3d64ed585f2ff82a2e67bf78e294d0438
Mateusz Loskot <mateusz@loskot.net>
2013-06-27 15:09:02 +0100

    Install libtiff4-dev

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -5,6 +5,6 @@ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192 |
 sudo apt-get install python-software-properties -y || exit 1
 sudo add-apt-repository ppa:ubuntugis/ppa -y || exit 1
 sudo apt-get update -qq || exit 1
-sudo apt-get install cmake libgdal-dev libgeotiff-dev libproj-dev libxml2-dev || exit 1
+sudo apt-get install cmake libgdal-dev libgeotiff-dev libproj-dev libtiff4-dev libxml2-dev || exit 1
 gcc --version || exit 1
 clang --version || exit 1
