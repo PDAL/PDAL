901ceecdae46a80ebf558a9f8a58a66dcec3f060
Mateusz Loskot <mateusz@loskot.net>
2013-06-27 01:19:13 +0100

    Install libgeotiff-dev

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -6,7 +6,7 @@ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:ubuntugis/ppa -y
 sudo apt-get update -qq
-sudo apt-get install cmake libgdal-dev libproj-dev libxml2-dev
+sudo apt-get install cmake libgdal-dev libgeotiff-dev libproj-dev libxml2-dev
 #echo "$(tmstamp) *** before_install::apt-get finished $(date) ***"
 gcc --version
 clang --version
