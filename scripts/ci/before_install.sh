35192f914431014697996f30a56bbb88463814a2
Mateusz Loskot <mateusz@loskot.net>
2013-06-27 01:37:10 +0100

    Replace return 1 with exit 1

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Installs requirements for PDAL
 source ./scripts/ci/common.sh
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192 || return 1
-sudo apt-get install python-software-properties -y || return 1
-sudo add-apt-repository ppa:ubuntugis/ppa -y || return 1
-sudo apt-get update -qq || return 1
-sudo apt-get install cmake libgdal-dev libgeotiff-dev libproj-dev libxml2-dev || return 1
-gcc --version || return 1
-clang --version || return 1
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192 || exit 1
+sudo apt-get install python-software-properties -y || exit 1
+sudo add-apt-repository ppa:ubuntugis/ppa -y || exit 1
+sudo apt-get update -qq || exit 1
+sudo apt-get install cmake libgdal-dev libgeotiff-dev libproj-dev libxml2-dev || exit 1
+gcc --version || exit 1
+clang --version || exit 1
--- scripts/ci/script.sh
+++ scripts/ci/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Builds and tests PDAL
 source ./scripts/ci/common.sh
-mkdir -p _build || return 1
-cd _build || return 1
+mkdir -p _build || exit 1
+cd _build || exit 1
 
 cmake \
     -DWITH_GDAL=ON \
     -DWITH_GEOTIFF=ON \
     -DWITH_LIBXML2=ON \
     .. \
-    || return 1
+    || exit 1
 
-    make -j ${NUMTHREADS} || return 1
+    make -j ${NUMTHREADS} || exit 1
 
-ctest -V --output-on-failure . || return 1
+ctest -V --output-on-failure . || exit 1
