c2d0403b8a623aed71566bd32b1effab8e6a7fa3
Mateusz Loskot <mateusz@loskot.net>
2013-06-28 01:33:41 +0100

    Build libgeotiff from sources

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -5,10 +5,14 @@ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192 |
 sudo apt-get install python-software-properties -y || exit 1
 sudo add-apt-repository ppa:ubuntugis/ppa -y || exit 1
 sudo apt-get update -qq || exit 1
-sudo apt-get install cmake libgdal-dev libgeotiff-dev libproj-dev libtiff4-dev libxml2-dev || exit 1
+sudo apt-get install cmake libgdal-dev libproj-dev libtiff4-dev libxml2-dev || exit 1
+
+# install libgeotiff from sources
+wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.0.tar.gz || exit 1
+tar -xzf libgeotiff-1.4.0.tar.gz || exit 1
+cd libgeotiff-1.4.0 || exit 1
+./configure --prefix=/usr && make && sudo make install || exit 1
+cd $TRAVIS_BUILD_DIR
+
 gcc --version || exit 1
 clang --version || exit 1
-
-# TEST
-grep VERSION /usr/include/tiff.h
-grep VERSION /usr/include/tiffvers.h
