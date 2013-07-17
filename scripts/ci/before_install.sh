fbad531d9bcc4eafadb518ab8cbfb5b24b989829
Mateusz Loskot <mateusz@loskot.net>
2013-07-17 23:47:29 +0100

    Invoke Travis scripts with bash -e
    The -e switch makes Travis reliably reflect actual build status and allows to remove the tedious || exit 1 test

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 # Installs requirements for PDAL
 source ./scripts/ci/common.sh
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192 || exit 1
-sudo apt-get install python-software-properties -y || exit 1
-sudo add-apt-repository ppa:ubuntugis/ppa -y || exit 1
-sudo apt-get update -qq || exit 1
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:ubuntugis/ppa -y
+sudo apt-get update -qq
 sudo apt-get install \
     cmake \
     libflann-dev \
@@ -12,15 +12,14 @@ sudo apt-get install \
     libpq-dev \
     libproj-dev \
     libtiff4-dev \
-    libxml2-dev \
-    || exit 1
+    libxml2-dev
 
 # install libgeotiff from sources
-wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.0.tar.gz || exit 1
-tar -xzf libgeotiff-1.4.0.tar.gz || exit 1
-cd libgeotiff-1.4.0 || exit 1
-./configure --prefix=/usr && make && sudo make install || exit 1
+wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.0.tar.gz
+tar -xzf libgeotiff-1.4.0.tar.gz
+cd libgeotiff-1.4.0
+./configure --prefix=/usr && make && sudo make install
 cd $TRAVIS_BUILD_DIR
 
-gcc --version || exit 1
-clang --version || exit 1
+gcc --version
+clang --version
