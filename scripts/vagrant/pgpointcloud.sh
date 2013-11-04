8b768382860076cc024a98d8fa2cace7dc789b45
Howard Butler <hobu.inc@gmail.com>
2013-11-04 15:25:16 -0600

    fix up pgpointcloud build, add read xml example

--- scripts/vagrant/pgpointcloud.sh
+++ scripts/vagrant/pgpointcloud.sh
@@ -1,13 +1,19 @@
 git clone https://github.com/pramsey/pointcloud.git
+cd pointcloud
 cmake -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_FLAGS="-fPIC" \
     -DCMAKE_C_FLAGS="-fPIC"
 make
 sudo make install
+sudo service postgresql start
 sudo -u postgres createuser -s vagrant
 createdb points
+echo "CREATE EXTENSION postgis;" | psql -d points -U vagrant
 echo "CREATE EXTENSION pointcloud;" | psql -d points -U vagrant
+echo "CREATE EXTENSION pointcloud_postgis;" | psql -d points -U vagrant
 wget http://liblas.org/samples/st-helens-small.las
-https://raw.github.com/PDAL/PDAL/master/scripts/vagrant/loadpgpointcloud.xml
+wget https://raw.github.com/PDAL/PDAL/master/scripts/vagrant/loadpgpointcloud.xml
+wget https://raw.github.com/PDAL/PDAL/master/scripts/vagrant/readpgpointcloud.xml
 pdal pipeline --input loadpgpointcloud.xml
+pdal info --input readpgpointcloud.xml
