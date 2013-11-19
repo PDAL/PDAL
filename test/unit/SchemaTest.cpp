168134690bce852caac332a91efab8c1eb57bf89
Howard Butler <hobu.inc@gmail.com>
2013-11-18 22:12:13 -0600

    test pdal::Schema::getOrientation #209

--- test/unit/SchemaTest.cpp
+++ test/unit/SchemaTest.cpp
@@ -144,5 +144,19 @@ BOOST_AUTO_TEST_CASE(test_find)
 }
 
 
+BOOST_AUTO_TEST_CASE(SchemaTest_orientation)
+{
+
+    Dimension x("X", dimension::SignedInteger, 4);
+    Dimension y("Y", dimension::SignedInteger, 4);
+    Schema s;
+    s.appendDimension(x);
+    s.appendDimension(y);
+    
+    BOOST_CHECK_EQUAL(s.getOrientation(), pdal::schema::POINT_INTERLEAVED);
+
+
+    return;
+}
 
 BOOST_AUTO_TEST_SUITE_END()
