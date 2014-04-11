4720edd719ccc01ec707681123a7e40f167bcc73
Andrew Bell <andrew.bellia@gmail.com>
2014-04-11 15:42:51 -0500

    Format cleanup and so on for scaling filter.

--- include/pdal/filters/Scaling.hpp
+++ include/pdal/filters/Scaling.hpp
@@ -35,7 +35,7 @@
 #ifndef INCLUDED_FILTERS_SCALINGFILTER_HPP
 #define INCLUDED_FILTERS_SCALINGFILTER_HPP
 
-#include <boost/shared_ptr.hpp>
+#include <boost/numeric/conversion/cast.hpp>
 
 #include <pdal/Filter.hpp>
 #include <pdal/FilterIterator.hpp>
@@ -58,6 +58,9 @@ namespace scaling
 
 struct PDAL_DLL Scaler
 {
+public:
+    Scaler() : scale(1.0), offset(0), size(0)
+    {}
 
     std::string name;
     std::string type;
@@ -75,6 +78,8 @@ public:
     SET_STAGE_LINK("http://pdal.io/stages/filters.scaling.html")  
 
     Scaling(Stage& prevStage, const Options&);
+    Scaling& operator=(const Scaling&) = delete;
+    Scaling(const Scaling&) = delete;
 
     static Options getDefaultOptions();
     virtual void initialize();
@@ -88,27 +93,19 @@ public:
     
     std::map<dimension::id, dimension::id> const& getScaleMap() const
         { return m_scale_map; }
-    dimension::Interpretation getInterpretation(std::string const& t) const;
+    dimension::Interpretation getInterpretation(std::string t) const;
     
 private:
     void checkImpedance();
-    
     Schema alterSchema(Schema const& schema);
 
-    Scaling& operator=(const Scaling&); // not implemented
-    Scaling(const Scaling&); // not implemented
-
     std::vector<scaling::Scaler> m_scalers;
-
     std::map<dimension::id, dimension::id> m_scale_map;
-;
 };
 
 
 namespace iterators
 {
-    
-
 namespace scaling
 {
     
@@ -124,15 +121,14 @@ protected:
                          Dimension const& from_dimension,
                          Dimension const& to_dimension,
                          boost::uint32_t pointIndex);
-    template<class T> void scale(Dimension const& from_dimension,
-                                 Dimension const& to_dimension,
-                                 T& value) const;
+    template<class T>
+    void scale(Dimension const& from_dimension, Dimension const& to_dimension,
+        T& value) const;
 
-    std::map<boost::optional<pdal::Dimension const&>, boost::optional<pdal::Dimension const&> > m_dimension_map;
-
+    std::map<boost::optional<pdal::Dimension const&>,
+        boost::optional<pdal::Dimension const&> > m_dimension_map;
     void readBufferBeginImpl(PointBuffer&);
     boost::uint32_t readBufferImpl(PointBuffer&);    
-
     void scaleData(PointBuffer& buffer, boost::uint32_t numRead);
 
 private:
@@ -145,21 +141,20 @@ namespace sequential
 {
 
 
-class PDAL_DLL Scaling : public pdal::FilterSequentialIterator, public scaling::IteratorBase
+class PDAL_DLL Scaling : public pdal::FilterSequentialIterator,
+    public scaling::IteratorBase
 {
 public:
     Scaling(const pdal::filters::Scaling& filter, PointBuffer& buffer);
 
 protected:
-
-    inline virtual void readBufferBeginImpl(PointBuffer& buffer) { scaling::IteratorBase::readBufferBeginImpl(buffer); }
+    virtual void readBufferBeginImpl(PointBuffer& buffer)
+        { scaling::IteratorBase::readBufferBeginImpl(buffer); }
     virtual boost::uint32_t readBufferImpl(PointBuffer& buffer);
-    
 
 private:
     boost::uint64_t skipImpl(boost::uint64_t);
     bool atEndImpl() const;
-
 };
 
 } // namespace sequential
@@ -167,141 +162,74 @@ private:
 namespace random
 {
     
-class PDAL_DLL Scaling : public pdal::FilterRandomIterator, public scaling::IteratorBase
+class PDAL_DLL Scaling : public pdal::FilterRandomIterator,
+    public scaling::IteratorBase
 {
 public:
     Scaling(const pdal::filters::Scaling& filter, PointBuffer& buffer);
     virtual ~Scaling() {};
 
 protected:
-    inline virtual void readBufferBeginImpl(PointBuffer& buffer) { scaling::IteratorBase::readBufferBeginImpl(buffer); }
+    inline virtual void readBufferBeginImpl(PointBuffer& buffer)
+        { scaling::IteratorBase::readBufferBeginImpl(buffer); }
     virtual boost::uint32_t readBufferImpl(PointBuffer& buffer);
-    
     virtual boost::uint64_t seekImpl(boost::uint64_t);
-
 };
 
 } // namespace random
     
-#ifdef PDAL_COMPILER_MSVC
-// when template T is know, std::numeric_limits<T>::is_exact is a constant...
-#  pragma warning(push)
-#  pragma warning(disable: 4127)  // conditional expression is constant
-#endif
-
 template <class T>
-inline void scaling::IteratorBase::scale(Dimension const& from_dimension,
-                           Dimension const& to_dimension,
-                           T& value) const
+void scaling::IteratorBase::scale(Dimension const& from_dimension,
+    Dimension const& to_dimension, T& value) const
 {
+    //ABELL - This bit was confusing to me.  What we appear to be doing is
+    //  getting a value that assumes that we were going to scale with the old
+    //  offset/scale and then instead apply the new scaling.
+    //  In other words, scale up to "actual" value (from nominal) and then
+    //  adjust to a new nominal value that assumes we'll get an actual value
+    //  by applying the new scale factor.
+    //ABELL -  Really don't understand why we don't just adjust the scale factor
+    //  appropriately.
+    double scaled = (value * from_dimension.getNumericScale() +
+        from_dimension.getNumericOffset() - to_dimension.getNumericOffset()) /
+        to_dimension.getNumericScale();
+    // FIXME: This only downscales, not upscales. If
+    // from_dimension.getNumericScale is > to_dimension.getNumericScale ==> BOOM
 
-    double v = static_cast<double>(value);
-    double scaled = (v*from_dimension.getNumericScale() + from_dimension.getNumericOffset() - to_dimension.getNumericOffset())/to_dimension.getNumericScale();
-    // FIXME: This only downscales, not upscales. If from_dimension.getNumericScale is > to_dimension.getNumericScale ==> BOOM
-
-    T output(0);
-    boost::int64_t i64(0);
-    boost::uint64_t u64(0);
-    if (boost::is_floating_point<T>::value)
-    {   
-        // Since we're a float already, just 
-        // cast it and be done with it.
-        value = static_cast<T>(scaled);
+    std::string err;
+    T errVal;
+    try
+    {
+        value = boost::numeric_cast<T>(scaled);
         return;
     }
-
-    bool bSigned(boost::is_signed<T>::value);
-    bool bGreater(false);
-    bool bLess(false);
-    if (bSigned)
+    catch (boost::numeric::positive_overflow)
     {
-        i64 = static_cast<boost::int64_t>(scaled);
-        boost::int64_t mn = static_cast<boost::int64_t>((std::numeric_limits<T>::min)());            
-        boost::int64_t mx = static_cast<boost::int64_t>((std::numeric_limits<T>::max)());            
-        bGreater = (i64 > mx);
-        bLess = (i64 < mn);
-        output = static_cast<T>(i64);              
-        if (!bGreater && !bLess)
-        {
-            // We didn't overflow, we're done
-            value = output;
-            return;
-        }
+        err = "greater than std::numeric_limits::max()";
+        errVal = std::numeric_limits<T>::max();
     }
-    else
+    catch (boost::numeric::negative_overflow)
     {
-        u64 = static_cast<boost::uint64_t>(scaled);
-        boost::uint64_t mn = static_cast<boost::uint64_t>((std::numeric_limits<T>::min)());
-        boost::uint64_t mx = static_cast<boost::uint64_t>((std::numeric_limits<T>::max)());
-        bGreater = (u64 > mx);
-        bLess = (u64 < mn);
-        output = static_cast<T>(u64);
-        if (!bGreater && !bLess)
-        {
-            // We didn't overflow, we're done
-            value = output;
-            return;
-        }
+        err = "less than std::numeric_limits::min()";
+        errVal = std::numeric_limits<T>::min();
     }
+    std::ostringstream oss;
+    oss.precision(12);
+    oss.setf(std::ios::fixed);
+    oss << "scaling::IteratorBase::scale: '" <<
+        to_dimension.getNumericScale() << "' and/or offset: " <<
+        to_dimension.getNumericOffset() <<"' combination causes "
+        "de-scaled value to be " << err << "for dimension '" <<
+        to_dimension.getFQName() << "'. " << "(v - offset)/ scale) is: (" <<
+        value << " - " << to_dimension.getNumericOffset()  << ")/" <<
+        to_dimension.getNumericScale() <<") == '" << scaled <<
+        "' but max() for the datatype is: " << errVal;
+    throw std::out_of_range(oss.str());
+} // namespace scaling
 
+} // namespace iterators
 
-    if (bGreater)
-    {
-        std::ostringstream oss;
-        boost::int64_t out(0);
-        if (bSigned)
-            out = static_cast<boost::int64_t>(i64);
-        else
-            out = static_cast<boost::int64_t>(u64);
-        
-        oss.precision(12);
-        oss.setf(std::ios::fixed);
-        oss << "scaling::IteratorBase::scale: '" << to_dimension.getNumericScale() 
-            << "' and/or offset: " << to_dimension.getNumericOffset() <<"' combination causes "
-            "de-scaled value to be greater than std::numeric_limits::max for dimension '" 
-            << to_dimension.getFQName() << "'. " <<
-            "(v - offset)/ scale) is: (" 
-            << v << " - " << to_dimension.getNumericOffset()  << ")/" 
-            << to_dimension.getNumericScale() <<") == '" << out 
-            << "' but max() for the datatype is: " 
-            << (std::numeric_limits<T>::max)();
-        throw std::out_of_range(oss.str());
-    }
-    else if (bLess)
-    {
-        std::ostringstream oss;
-        boost::int64_t out(0);
-        if (bSigned)
-            out = static_cast<boost::int64_t>(i64);
-        else
-            out = static_cast<boost::int64_t>(u64);
-        oss.precision(12);
-        oss.setf(std::ios::fixed);
-        oss << "scaling::IteratorBase::scale: '" << to_dimension.getNumericScale() 
-            << "' and/or offset: " << to_dimension.getNumericOffset() <<"' combination causes "
-            "de-scaled value to be less than std::numeric_limits::mine for dimension '" 
-            << to_dimension.getFQName() << "'. " <<
-            "(v - offset)/ scale) is: (" 
-            << v << " - " << to_dimension.getNumericOffset()  << ")/" 
-            << to_dimension.getNumericScale() <<") == '" << out 
-            << "' but min() for the datatype is: " 
-            << (std::numeric_limits<T>::min)();
-
-        throw std::out_of_range(oss.str());
-    }
-
-
-    return;
-}
-
-#ifdef PDAL_COMPILER_MSVC
-#  pragma warning(pop)
-#endif
-
-}
-
-
-}
-} // namespaces
+} // namespace filters
+} // namespace pdal
 
 #endif
