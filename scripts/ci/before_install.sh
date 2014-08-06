df96a980020a9905135e805945e621c7a2b9e500
Howard Butler <hobu.inc@gmail.com>
2014-08-06 13:23:31 -0500

    try debiangis-unstable for Travis packages

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -6,7 +6,8 @@ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get update -y
 sudo apt-get install software-properties-common -y
 sudo apt-get install python-software-properties -y
-sudo add-apt-repository ppa:ubuntugis/ppa -y
+# sudo add-apt-repository ppa:ubuntugis/ppa -y
+sudo add-apt-repository ppa:ubuntugis-unstable/ppa -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 if [[ $PDAL_EMBED_BOOST == "OFF" ]]
 then
@@ -46,7 +47,7 @@ fi
 if [[ $PDAL_OPTIONAL_COMPONENTS == "all" ]]
 then
     sudo apt-get install \
-        libgdal-dev \
+        libgdal1-dev \
         libgeos-dev \
         libgeos++-dev \
         libpq-dev \
@@ -54,14 +55,15 @@ then
         python-numpy \
         libxml2-dev \
         libflann-dev \
-        libtiff4-dev
+        libtiff4-dev 
+        
 
     # install libgeotiff from sources
-    wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.0.tar.gz
-    tar -xzf libgeotiff-1.4.0.tar.gz
-    cd libgeotiff-1.4.0
-    ./configure --prefix=/usr && make && sudo make install
-    cd $TRAVIS_BUILD_DIR
+#    wget http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.0.tar.gz
+#    tar -xzf libgeotiff-1.4.0.tar.gz
+#    cd libgeotiff-1.4.0
+#    ./configure --prefix=/usr && make && sudo make install
+#    cd $TRAVIS_BUILD_DIR
 fi
 
 gcc --version
