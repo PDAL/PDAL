65271dd1e49f447bb4ec6846ad185c8f5b57b195
Howard Butler <hobu.inc@gmail.com>
2014-08-07 12:00:15 -0500

    more travis

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -48,7 +48,7 @@ fi
 
 if [[ $PDAL_OPTIONAL_COMPONENTS == "all" ]]
 then
-    sudo apt-get install libgdal1h libgdal-dev libhdf5-dev 
+    sudo apt-get install libgdal1h libgdal-dev libhdf5-serial-dev
     sudo apt-get install \
         libproj-dev \
         python-numpy \
