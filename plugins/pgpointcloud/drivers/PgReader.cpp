df2882425ee18820b4feaa1117d2a84dfb5845bb
Paul Ramsey <pramsey@cleverelephant.ca>
2014-11-19 10:49:40 -0800

    Allow connection for addDimension to get access to the database

--- plugins/pgpointcloud/drivers/PgReader.cpp
+++ plugins/pgpointcloud/drivers/PgReader.cpp
@@ -168,21 +168,15 @@ uint32_t PgReader::fetchPcid() const
 
     char *pcid_str = pg_query_once(m_session, oss.str());
 
-    if (! pcid_str)
+    uint32_t pcid = 0;
+    if (pcid_str)
     {
-        std::ostringstream oss;
-        oss << "Unable to fetch pcid with column '"
-            << m_column_name <<"' and  table '"
-            << m_table_name <<"'";
-        throw pdal_error(oss.str());
+       pcid = atoi(pcid_str);
+       free(pcid_str);
     }
 
-    uint32_t pcid = atoi(pcid_str);
-    free(pcid_str);
-
-    if (!pcid)
+    if (!pcid) // Are pcid == 0 valid?
     {
-        // Are pcid == 0 valid?
         std::ostringstream oss;
         oss << "Unable to fetch pcid with column '"
             << m_column_name <<"' and  table '"
@@ -199,6 +193,10 @@ uint32_t PgReader::fetchPcid() const
 void PgReader::addDimensions(PointContextRef ctx)
 {
     log()->get(LogLevel::Debug) << "Fetching schema object" << std::endl;
+
+    // Need this before we can fetchPcid() below
+    if (!m_session)
+        m_session = pg_connect(m_connection);
 
     uint32_t pcid = fetchPcid();
 
@@ -252,7 +250,8 @@ void PgReader::ready(PointContextRef ctx)
     m_cur_nrows = 0;
     m_cur_result = NULL;
 
-    m_session = pg_connect(m_connection);
+    if (!m_session)
+        m_session = pg_connect(m_connection);
 
     if (getSpatialReference().empty())
         setSpatialReference(fetchSpatialReference());
