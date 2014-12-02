6287a3a5bf6d79c86316993511fc89143416af9e
Andrew Bell <andrew.bell.ia@gmail.com>
2014-12-02 10:05:25 -0600

    Ignore float equality warning.

--- include/pdal/pdal_types.hpp
+++ include/pdal/pdal_types.hpp
@@ -65,7 +65,10 @@ public:
 
     bool nonstandard() const
     {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
         return m_autoScale || m_autoOffset || m_scale != 1.0 || m_offset != 0.0;
+#pragma GCC diagnostic pop
     }
 };
 
