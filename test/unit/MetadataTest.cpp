35c56380a3fdd4c8a700c8991341bd02532157cd
Andrew Bell <andrew.bell.ia@gmail.com>
2015-01-05 16:08:37 -0600

    Remove Metadata -> JSON test (moved to PDALUtils).

--- test/unit/MetadataTest.cpp
+++ test/unit/MetadataTest.cpp
@@ -285,23 +285,6 @@ TEST(MetadataTest, sanitize)
 }
 **/
 
-TEST(MetadataTest, toJSON)
-{
-/**
-    MetadataNode top("stats");
-    top.add("test", 1);
-    MetadataNode l2 = top.add("test", 2);
-    top.add("test", "3");
-    top.add("test2", "foo");
-
-    l2.add("foo", 3.56);
-    l2.add("bar", "ballet");
-    l2.add("foo", 25);
-    std::string s = top.toJSON();
-    std::cerr << s << "---\n";
-**/
-}
-
 TEST(MetadataTest, test_metadata_stage)
 {
 //ABELL
