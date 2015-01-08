01fd4911bd40b5cb1479f0bb72b798300744347f
Howard Butler <howard@hobu.co>
2015-01-08 13:38:03 -0600

    add laz-perf to vagrant config, switch on plugins

--- Vagrantfile
+++ Vagrantfile
@@ -156,6 +156,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
       "libgeotiff.sh",
       "nitro.sh",
       "hexer.sh",
+      "laz-perf.sh",
       "p2g.sh",
       "soci.sh",
       "laszip.sh",
--- scripts/vagrant/pdal.sh
+++ scripts/vagrant/pdal.sh
@@ -14,7 +14,20 @@ cd build
 cmake   -G "Unix Makefiles"  \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=/usr \
+        -DWITH_ICONV=ON \
         -DBUILD_PLUGIN_PCL=ON \
+        -DWITH_LASZIP=ON \
+        -DWITH_GEOTIFF=ON \
+        -DWITH_LAZPERF=ON \
+        -DWITH_LIBXML2=ON \
+        -DBUILD_PLUGIN_ATTRIBUTE=ON \
+        -DBUILD_PLUGIN_HEXBIN=ON \
+        -DBUILD_PLUGIN_ICEBRIDGE=ON \
+        -DBUILD_PLUGIN_NITF=ON \
+        -DBUILD_PLUGIN_P2G=ON \
+        -DBUILD_PLUGIN_PGPOINTCLOUD=ON \
+        -DBUILD_PLUGIN_PYTHON=ON \
+        -DBUILD_PLUGIN_SQLITE=ON \
         ..
 
 make -j $NUMTHREADS
