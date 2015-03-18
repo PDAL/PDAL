afde42459c084b1cbe263e54d61f51f537994871
chambbj <brad.chambers@gmail.com>
2015-03-18 07:26:23 -0400

    Update install.cmd
    CMake requires some numpy info to find it

--- scripts/appveyor/install.cmd
+++ scripts/appveyor/install.cmd
@@ -23,5 +23,7 @@ cmake -G "Visual Studio 11 2012 Win64" ^
     -DWITH_LASZIP=%PDAL_OPTIONAL_COMPONENTS% ^
     -DWITH_LIBXML2=OFF ^
     -DWITH_TESTS=ON ^
+    -DNUMPY_INCLUDE_DIR=%OSGEODIR%\apps\python27\lib\site-packages\numpy\core\include ^
+	-DNUMPY_VERSION=1.8.1 ^
     -Dgtest_force_shared_crt=ON ^
     .
