4bc4aa7733d79435a921a49c330dcc36c165b22d
Pete Gadomski <pete.gadomski@gmail.com>
2015-04-05 14:09:37 -0500

    Reorganize apt-get installs
    Split out the installs by expected ppa source, so the ppa <-> package
    links are easier to maintain over time.

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -4,11 +4,11 @@ source ./scripts/ci/common.sh
 
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo mv /etc/apt/sources.list.d/pgdg-source.list* /tmp
-sudo apt-get -qq remove postgis
 sudo apt-get update -qq
-sudo apt-get install software-properties-common -y
-sudo apt-get install python-software-properties -y
-# sudo add-apt-repository ppa:ubuntugis/ppa -y
+
+sudo apt-get install \
+    software-properties-common \
+    python-software-properties
 sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo add-apt-repository ppa:boost-latest/ppa -y
@@ -16,36 +16,51 @@ sudo add-apt-repository ppa:kalakris/cmake -y
 sudo add-apt-repository ppa:pdal/travis -y
 sudo apt-get update -qq
 
-# Install g++-4.8 (even if we're building clang) for updated libstdc++
-sudo apt-get install g++-4.8
+sudo apt-get -qq remove postgis
 
-sudo apt-get install boost1.55
-sudo apt-get install cmake
-
+# From main
 if [[ $PDAL_CMAKE_GENERATOR == "Ninja" ]]
 then
     sudo apt-get install ninja-build
 fi
 
+# From ppa:ubuntu-toolchain-r/test
+# Install g++-4.8 (even if we're building clang) for updated libstdc++
+sudo apt-get install g++-4.8
 
-# GDAL is now always required
+# From ppa:boost-latest/ppa
+sudo apt-get install boost1.55
+
+# From ppa:kalakris/cmake
+sudo apt-get install cmake
+
+# From ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get install \
     libgdal1h \
     libgdal-dev
 
-
 if [[ $PDAL_OPTIONAL_COMPONENTS == "all" ]]
 then
+    # From main
     sudo apt-get install \
-        libhdf5-serial-dev \
-        libproj-dev \
-        libgeos++-dev \
-        python-numpy \
-        libxml2-dev \
         libflann-dev \
+        libhdf5-serial-dev \
         libtiff4-dev \
-        libgeotiff-dev \
         postgresql-server-dev-9.1 \
+        python-numpy
+
+    # From ppa:ubuntugis/ppa
+    sudo apt-get install \
+        libgeotiff-dev \
+        libxml2-dev
+
+    # From ppa:ubuntugis/ubuntugis-unstable
+    sudo apt-get install \
+        libgeos++-dev \
+        libproj-dev
+
+    # From ppa:pdal/travis
+    sudo apt-get install \
         pgpointcloud
 fi
 
