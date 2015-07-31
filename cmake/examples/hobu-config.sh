241fb3f1c1ccc14b61d8e3c4568317a143375171
Howard Butler <howard@hobu.co>
2015-07-31 15:15:16 -0500

    fix up greyhound compilation failure

--- cmake/examples/hobu-config.sh
+++ cmake/examples/hobu-config.sh
@@ -38,6 +38,7 @@ CC=$CC CXX=$CXX cmake   -G "$CONFIG"  \
         -DBUILD_PLUGIN_ICEBRIDGE=ON \
         -DBUILD_PLUGIN_PCL=ON \
         -DBUILD_PLUGIN_ATTRIBUTE=ON \
+        -DBUILD_PLUGIN_GREYHOUND=ON \
         -DWITH_LAZPERF=ON \
         -DMRSID_INCLUDE_DIR=/Users/hobu/dev/release/mrsid/Lidar_DSDK/include \
         -DMRSID_LIBRARY=/Users/hobu/dev/release/mrsid/Lidar_DSDK/lib/liblti_lidar_dsdk.dylib \
