15af30070008a196c5eb49be3407657e405cfef9
Scott Lewis <scott.lewis@nsidc.org>
2015-11-12 12:33:58 -0700

    Updating Kernel tutorial to work with new code.
    Using StageFactory as described in the previous version of the
    tutorial, and updating the macro to use the general shared
    plugin, rather than the kernel-specific macro which no longer
    exists.

--- examples/writing-kernel/MyKernel.hpp
+++ examples/writing-kernel/MyKernel.hpp
@@ -3,23 +3,28 @@
 #pragma once
 
 #include <pdal/Kernel.hpp>
+#include <pdal/plugin.hpp>
 
 #include <string>
 
-class MyKernel : public pdal::Kernel
+namespace pdal
 {
-public:
-  SET_KERNEL_NAME ("MyKernel", "My Awesome Kernel")
-  SET_KERNEL_LINK ("http://link/to/documentation")
 
-  MyKernel() : Kernel() {};
-  int execute();
+  class PDAL_DLL MyKernel : public Kernel
+  {
+  public:
+    static void * create();
+    static int32_t destroy(void *);
+    std::string getName() const;
+    int execute(); // override
 
-private:
-  void validateSwitches();
-  void addSwitches();
+  private:
+    MyKernel();
+    void validateSwitches();
+    void addSwitches();
 
-  std::string m_input_file;
-  std::string m_output_file;
-};
+    std::string m_input_file;
+    std::string m_output_file;
+  };
 
+} // namespace pdal
