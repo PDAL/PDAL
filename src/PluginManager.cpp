ff97dbe1c699fad0c0bc2f08a976f3612d6b2b0d
Bas Couwenberg <sebastic@xs4all.nl>
2016-03-21 22:10:51 +0100

    Add support for kFreeBSD to PluginManager & SQLiteCommon.

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -61,7 +61,7 @@ static PluginManager s_instance;
 
 #if defined(__APPLE__) && defined(__MACH__)
     const std::string dynamicLibraryExtension(".dylib");
-#elif defined __linux__
+#elif defined(__linux__) || defined(__FreeBSD_kernel__)
     const std::string dynamicLibraryExtension(".so");
 #elif defined _WIN32
     const std::string dynamicLibraryExtension(".dll");
