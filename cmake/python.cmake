2e6339e9845a1134f8ccf3aec264ae437c218bd2
Andrew Bell <andrew.bell.ia@gmail.com>
2016-03-22 10:02:00 -0500

    Explicitly set PDAL_PYTHON_LIBRARY in python.cmake Close #1174

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -8,6 +8,7 @@ if(PYTHONLIBS_FOUND)
     set(CMAKE_REQUIRED_LIBRARIES "${PYTHON_LIBRARY}")
     include_directories(SYSTEM ${PYTHON_INCLUDE_DIR})
     add_definitions(-DHAVE_PYTHON=1)
+    add_definitions(-DPDAL_PYTHON_LIBRARY="${PYTHON_LIBRARY}")
     set(PDAL_HAVE_PYTHON 1)
 
     find_package(NumPy QUIET 1.5 REQUIRED)
