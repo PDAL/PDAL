ff4b63e24ea7aebe2adb7c20dbf024f9191d5b2a
Howard Butler <howard@hobu.co>
2016-05-10 17:00:53 -0500

    JSON all the things (python)

--- python/test/test_libpdal.py
+++ python/test/test_libpdal.py
@@ -3,42 +3,47 @@ from pdal import libpdalpython
 import unittest
 import os
 
-class TestPDALArray(unittest.TestCase):
-  DATADIRECTORY = "../test"
+DATADIRECTORY = os.environ.get('PDAL_TEST_DIR')
+if not DATADIRECTORY:
+    DATADIRECTORY = "../test"
 
-  def fetch_xml(self, filename):
+class TestPDALArray(unittest.TestCase):
+
+  DATADIRECTORY = os.environ.get('PDAL_TEST_DIR')
+  if not DATADIRECTORY:
+      DATADIRECTORY = "../test"
+
+  def fetch_json(self, filename):
     import os
-    DATADIRECTORY = os.environ.get('PDAL_TEST_DIR')
-    if not DATADIRECTORY:
-        DATADIRECTORY = self.DATADIRECTORY
     fn = DATADIRECTORY + os.path.sep +  filename
     output = ''
     with open(fn, 'rb') as f:
         output = f.read().decode('UTF-8')
     return output
 
-  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, '/data/pipeline/pipeline_read.xml')),
-                       "missing test data")
+  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, 'data/pipeline/pipeline_read.json')),
+                       os.path.join(DATADIRECTORY, 'data/pipeline/pipeline_read.json'))
   def test_construction(self):
     """Can we construct a PDAL pipeline"""
-    xml = self.fetch_xml('/data/pipeline/pipeline_read.xml')
-    r = libpdalpython.PyPipeline(xml)
+    json = self.fetch_json('/data/pipeline/pipeline_read.json')
+    r = libpdalpython.PyPipeline(json)
 
-  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, '/data/pipeline/pipeline_read.xml')),
+  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, 'data/pipeline/pipeline_read.json')),
                        "missing test data")
   def test_execution(self):
     """Can we execute a PDAL pipeline"""
-    xml = self.fetch_xml('/data/pipeline/pipeline_read.xml')
-    r = libpdalpython.PyPipeline(xml)
+    x = self.fetch_json('/data/pipeline/pipeline_read.json')
+    r = libpdalpython.PyPipeline(x)
     r.execute()
-    self.assertGreater(len(r.xml), 1000)
+    import sys
+    self.assertGreater(len(r.json), 200)
 
-  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, '/data/pipeline/pipeline_read.xml')),
+  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, 'data/pipeline/pipeline_read.json')),
                        "missing test data")
   def test_array(self):
     """Can we fetch PDAL data as a numpy array"""
-    xml = self.fetch_xml('/data/pipeline/pipeline_read.xml')
-    r = libpdalpython.PyPipeline(xml)
+    json = self.fetch_json('/data/pipeline/pipeline_read.json')
+    r = libpdalpython.PyPipeline(json)
     r.execute()
     arrays = r.arrays()
     self.assertEqual(len(arrays), 1)
@@ -47,12 +52,12 @@ class TestPDALArray(unittest.TestCase):
     self.assertAlmostEqual(a[0][0], 637012.24, 7)
     self.assertAlmostEqual(a[1064][2], 423.92, 7)
 
-  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, '/data/filters/chip.xml')),
+  @unittest.skipUnless(os.path.exists(os.path.join(DATADIRECTORY, 'data/filters/chip.json')),
                        "missing test data")
   def test_merged_arrays(self):
     """Can we fetch merged PDAL data """
-    xml = self.fetch_xml('/data/filters/chip.xml')
-    r = libpdalpython.PyPipeline(xml)
+    json = self.fetch_json('/data/filters/chip.json')
+    r = libpdalpython.PyPipeline(json)
     r.execute()
     arrays = r.arrays()
     self.assertEqual(len(arrays), 43)
