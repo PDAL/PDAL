e7232f2a9bdf6e03a8dc28c26f762711f2656bfa
Bradley J Chambers <brad.chambers@gmail.com>
2016-09-08 13:55:44 -0400

    Initial Local Outlier Factor (LOF) filter implementation

---
+++ doc/stages/filters.lof.rst
@@ -0,0 +1,61 @@
+.. _filters.lof:
+
+filters.lof
+===============================================================================
+
+Local Outlier Factor (LOF) was introduced as a method of determining the degree
+to which an object is an outlier. This filter is an implementation of the method
+described in [Breunig2000]_.
+
+The filter creates three new dimensions, all of which are doubles. The
+``KDistance`` dimension records the Euclidean distance between a point and it's
+``k-th`` nearest neighbor (the number of ``k`` neighbors is set with the
+``minpts`` option). The ``LocalReachabilityDistance`` is the inverse of the mean
+of all reachability distances for a neighborhood of points. This reachability
+distance is defined as the max of the Euclidean distance to a neighboring point
+and that neighbor's own previously computed ``KDistance``. Finally, each point
+has a ``LocalOutlierFactor`` which is the mean of all
+``LocalReachabilityDistance`` values for the neighborhood. In each case, the
+neighborhood is the set of ``k`` nearest neighbors, where ``k`` is set with the
+``minpts`` option.
+
+In practice, setting the ``minpts`` parameter appropriately and subsequently
+filtering outliers based on the computed ``LocalOutlierFactor`` can be
+difficult. The authors present some work on establishing upper and lower bounds
+on LOF values, and provide some guidelines on selecting ``minpts`` values, which
+users of ``filters.lof`` should find instructive.
+
+.. note::
+  
+  To inspect the newly created, non-standard dimensions, be sure to write to an
+  output format that can support arbitrary dimensions, such as BPF.
+  
+.. [Breunig2000] Breunig, M.M., Kriegel, H.-P., Ng, R.T., Sander, J., 2000. LOF: Identifying Density-Based Local Outliers. Proc. 2000 Acm Sigmod Int. Conf. Manag. Data 1–12.
+
+Example
+-------
+
+The sample pipeline below uses ``filters.lof`` to compute the LOF with a neighborhood of 20 neighbors, followed by a range filter to crop out points whose ``LocalOutlierFactor`` exceeds 1.2, before writing the output.
+
+.. code-block:: json
+
+    {
+      "pipeline":[
+        "input.las",
+        {
+          "type":"filters.lof",
+          "minpts":20
+        },
+        {
+          "type":"filters.range",
+          "limits":"LocalOutlierFactor[:1.2]"
+        },
+        "output.laz"
+      ]
+    }
+
+Options
+-------------------------------------------------------------------------------
+
+minpts
+  The number of k nearest neighbors. [Default: **10**]
---
+++ filters/lof/LOFFilter.cpp
@@ -0,0 +1,144 @@
+/******************************************************************************
+* Copyright (c) 2016, Bradley J Chambers (brad.chambers@gmail.com)
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following
+* conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in
+*       the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
+*       names of its contributors may be used to endorse or promote
+*       products derived from this software without specific prior
+*       written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+* OF SUCH DAMAGE.
+****************************************************************************/
+
+#include "LOFFilter.hpp"
+
+#include <pdal/KDIndex.hpp>
+#include <pdal/pdal_macros.hpp>
+
+#include <string>
+#include <vector>
+
+namespace pdal
+{
+
+static PluginInfo const s_info =
+    PluginInfo("filters.lof", "LOF Filter",
+               "http://pdal.io/stages/filters.lof.html");
+
+CREATE_STATIC_PLUGIN(1, 0, LOFFilter, Filter, s_info)
+
+std::string LOFFilter::getName() const
+{
+    return s_info.name;
+}
+
+void LOFFilter::addArgs(ProgramArgs& args)
+{
+    args.add("minpts", "Minimum number of points", m_minpts, 10);
+}
+
+void LOFFilter::addDimensions(PointLayoutPtr layout)
+{
+    using namespace Dimension;
+    m_kdist = layout->registerOrAssignDim("KDistance", Type::Double);
+    m_lrd = layout->registerOrAssignDim("LocalReachabilityDistance", Type::Double);
+    m_lof = layout->registerOrAssignDim("LocalOutlierFactor", Type::Double);
+}
+
+void LOFFilter::filter(PointView& view)
+{
+    using namespace Dimension;
+    
+    // Build the 3D KD-tree.
+    KD3Index index(view);
+    log()->get(LogLevel::Debug) << "Building 3D KD-tree...\n";
+    index.build();
+    
+    // Increment the minimum number of points, as knnSearch will be returning
+    // the neighbors along with the query point.
+    m_minpts++;
+  
+    // First pass: Compute the k-distance for each point.
+    // The k-distance is the Euclidean distance to k-th nearest neighbor.
+    log()->get(LogLevel::Debug) << "Computing k-distances...\n";
+    for (PointId i = 0; i < view.size(); ++i)
+    {
+        double x = view.getFieldAs<double>(Id::X, i);
+        double y = view.getFieldAs<double>(Id::Y, i);
+        double z = view.getFieldAs<double>(Id::Z, i);
+        std::vector<PointId> indices(m_minpts);
+        std::vector<double> sqr_dists(m_minpts);
+        index.knnSearch(x, y, z, m_minpts, &indices, &sqr_dists);
+        view.setField(m_kdist, i, std::sqrt(sqr_dists[m_minpts-1]));
+    }
+    
+    // Second pass: Compute the local reachability distance for each point.
+    // For each neighbor point, the reachability distance is the maximum value
+    // of that neighbor's k-distance and the distance between the neighbor and
+    // the current point. The lrd is the inverse of the mean of the reachability
+    // distances.
+    log()->get(LogLevel::Debug) << "Computing lrd...\n";
+    for (PointId i = 0; i < view.size(); ++i)
+    {
+        double x = view.getFieldAs<double>(Id::X, i);
+        double y = view.getFieldAs<double>(Id::Y, i);
+        double z = view.getFieldAs<double>(Id::Z, i);
+        std::vector<PointId> indices(m_minpts);
+        std::vector<double> sqr_dists(m_minpts);
+        index.knnSearch(x, y, z, m_minpts, &indices, &sqr_dists);
+        double M1 = 0.0;
+        point_count_t n = 0;
+        for (PointId j = 0; j < indices.size(); ++j)
+        {
+            double k = view.getFieldAs<double>(m_kdist, indices[j]);
+            double reachdist = std::max(k, std::sqrt(sqr_dists[j]));
+            M1 += (reachdist - M1) / ++n;
+        }
+        view.setField(m_lrd, i, 1.0 / M1);
+    }
+    
+    // Third pass: Compute the local outlier factor for each point.
+    // The LOF is the average of the lrd's for a neighborhood of points.
+    log()->get(LogLevel::Debug) << "Computing LOF...\n";
+    for (PointId i = 0; i < view.size(); ++i)
+    {
+        double x = view.getFieldAs<double>(Id::X, i);
+        double y = view.getFieldAs<double>(Id::Y, i);
+        double z = view.getFieldAs<double>(Id::Z, i);
+        double lrdp = view.getFieldAs<double>(m_lrd, i);
+        std::vector<PointId> indices(m_minpts);
+        std::vector<double> sqr_dists(m_minpts);
+        index.knnSearch(x, y, z, m_minpts, &indices, &sqr_dists);
+        double M1 = 0.0;
+        point_count_t n = 0;
+        for (auto const& j : indices)
+        {
+            M1 += (view.getFieldAs<double>(m_lrd, j) / lrdp - M1) / ++n;
+        }
+        view.setField(m_lof, i, M1);
+    }
+}
+
+} // namespace pdal
