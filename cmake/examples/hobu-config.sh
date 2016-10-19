1953c4d88de96cfda1b020c8d74d85d3e3c1b306
Howard Butler <howard@hobu.co>
2016-10-19 15:39:24 -0500

    if we can't find curl, don't build and depend on arbiter stuff

--- cmake/examples/hobu-config.sh
+++ cmake/examples/hobu-config.sh
@@ -27,6 +27,8 @@ CC=$CC CXX=$CXX cmake   -G "$CONFIG"  \
         -DCMAKE_INSTALL_PREFIX=/Users/hobu/pdal-build \
         -DBUILD_PLUGIN_OCI=ON \
         -DWITH_GEOTIFF=ON \
+        -DCURL_LIBRARY= \
+        -DCURL_INCLUDE_DIR= \
         -DBUILD_PLUGIN_SQLITE=ON \
         -DBUILD_PLUGIN_PGPOINTCLOUD=ON \
         -DBUILD_OCI_TESTS=ON \
