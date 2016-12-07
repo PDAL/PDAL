055c45b498a438eb6f13059c77f98a614bb2b4b3
Andrew Bell <andrew.bell.ia@gmail.com>
2016-12-07 11:23:59 -0500

    Properly build targets dependent on libxml2.

--- cmake/libxml2.cmake
+++ cmake/libxml2.cmake
@@ -6,12 +6,12 @@ endif()
 # libxml2
 #
 
-
 find_package(LibXml2)
 set_package_properties(LibXml2 PROPERTIES TYPE OPTIONAL)
 mark_as_advanced(CLEAR LIBXML2_INCLUDE_DIR)
 mark_as_advanced(CLEAR LIBXML2_LIBRARIES)
-include_directories(${LIBXML2_INCLUDE_DIR})
-set(PDAL_HAVE_LIBXML2 1)
+if (LIBXML2_FOUND)
+    set(PDAL_HAVE_LIBXML2 1)
+endif()
 
 set_property(GLOBAL PROPERTY _LIBXML2_INCLUDED TRUE)
