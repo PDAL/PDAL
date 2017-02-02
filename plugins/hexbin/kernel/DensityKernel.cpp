8768d7dc10781eac0a1abe30147590c3ce11b460
Pete Gadomski <pete.gadomski@gmail.com>
2017-02-02 06:26:50 -0700

    Expose hexbin filter options to density kernel
    It's nice to be able to tweak the parameters of the density kernel,
    instead of being locked into the defaults.

--- plugins/hexbin/kernel/DensityKernel.cpp
+++ plugins/hexbin/kernel/DensityKernel.cpp
@@ -59,6 +59,13 @@ void DensityKernel::addSwitches(ProgramArgs& args)
     args.add("ogrdriver,f", "OGR driver name to use ", m_driverName,
         "ESRI Shapefile");
     args.add("lyr_name", "OGR layer name to use", m_layerName, "");
+    args.add("sample_size", "Sample size for auto-edge length calculation",
+        m_sampleSize, 5000U);
+    args.add("threshold", "Required cell density", m_density, 15);
+    args.add("edge_length", "Length of hex edge", m_edgeLength);
+    args.add("hole_cull_area_tolerance", "Tolerance area to "
+            "apply to holes before cull", m_cullArea);
+    args.add("smooth", "Smooth boundary output", m_doSmooth, true);
 }
 
 
@@ -91,8 +98,14 @@ int DensityKernel::execute()
     {
         m_manager.makeReader(m_inputFile, "");
     }
+    Options options;
+    options.add("sample_size", m_sampleSize);
+    options.add("threshold", m_density);
+    options.add("edge_length", m_edgeLength);
+    options.add("hole_cull_area_tolerance", m_cullArea);
+    options.add("smooth", m_doSmooth);
     m_hexbinStage = &(m_manager.makeFilter("filters.hexbin",
-        *m_manager.getStage()));
+        *m_manager.getStage(), options));
     m_manager.execute();
     outputDensity(m_manager.pointTable().anySpatialReference());
     return 0;
