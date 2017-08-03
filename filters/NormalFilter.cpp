71d96c9ca1a2c04cfc56202785d6d66b296c6685
Bradley J Chambers <brad.chambers@gmail.com>
2017-08-03 12:33:53 -0400

    Provide for normal flipping
    User can always set a preferred viewpoint by providing X, Y, and Z coordinates.
    If a viewpoint coordinate is not provided, it will be replaced as follows.
    
    * X and Y will default to the midpoint of the X/Y extents
    * Z will default to the maximum Z plus 1000
    
    Normals are flipped when the dot product of the normal and the vector between
    the current point and the viewpoint is less than zero.

--- filters/NormalFilter.cpp
+++ filters/NormalFilter.cpp
@@ -1,36 +1,36 @@
 /******************************************************************************
-* Copyright (c) 2016, Bradley J Chambers (brad.chambers@gmail.com)
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following
-* conditions are met:
-*
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in
-*       the documentation and/or other materials provided
-*       with the distribution.
-*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
-*       names of its contributors may be used to endorse or promote
-*       products derived from this software without specific prior
-*       written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-* OF SUCH DAMAGE.
-****************************************************************************/
+ * Copyright (c) 2016-2017, Bradley J Chambers (brad.chambers@gmail.com)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
 
 #include "NormalFilter.hpp"
 
@@ -58,21 +58,21 @@ std::string NormalFilter::getName() const
     return s_info.name;
 }
 
-
 void NormalFilter::addArgs(ProgramArgs& args)
 {
     args.add("knn", "k-Nearest Neighbors", m_knn, 8);
+    m_vxArg = &args.add("vx", "Viewpoint X coordinate", m_vx);
+    m_vyArg = &args.add("vy", "Viewpoint Y coordinate", m_vy);
+    m_vzArg = &args.add("vz", "Viewpoint Z coordinate", m_vz);
 }
-
 
 void NormalFilter::addDimensions(PointLayoutPtr layout)
 {
     using namespace Dimension;
 
-    layout->registerDims({Id::NormalX, Id::NormalY, Id::NormalZ,
-        Id::Curvature});
+    layout->registerDims(
+        {Id::NormalX, Id::NormalY, Id::NormalZ, Id::Curvature});
 }
-
 
 void NormalFilter::doFilter(PointView& view)
 {
@@ -80,10 +80,18 @@ void NormalFilter::doFilter(PointView& view)
     filter(view);
 }
 
-
 void NormalFilter::filter(PointView& view)
 {
     KD3Index& kdi = view.build3dIndex();
+
+    BOX3D bounds;
+    view.calculateBounds(bounds);
+
+    double vx =
+        m_vxArg->set() ? m_vx : bounds.minx + (bounds.maxx - bounds.minx) * 0.5;
+    double vy =
+        m_vyArg->set() ? m_vy : bounds.miny + (bounds.maxy - bounds.miny) * 0.5;
+    double vz = m_vzArg->set() ? m_vz : bounds.maxz + 1000;
 
     for (PointId i = 0; i < view.size(); ++i)
     {
@@ -98,15 +106,23 @@ void NormalFilter::filter(PointView& view)
         if (solver.info() != Eigen::Success)
             throwError("Cannot perform eigen decomposition.");
         auto eval = solver.eigenvalues();
-        auto evec = solver.eigenvectors().col(0);
+        Eigen::Vector3f normal = solver.eigenvectors().col(0);
 
-        view.setField(Dimension::Id::NormalX, i, evec[0]);
-        view.setField(Dimension::Id::NormalY, i, evec[1]);
-        view.setField(Dimension::Id::NormalZ, i, evec[2]);
+        // flip normal towards viewpoint
+        PointRef p = view.point(i);
+        Eigen::Vector3f vp(vx - p.getFieldAs<double>(Dimension::Id::X),
+                           vy - p.getFieldAs<double>(Dimension::Id::Y),
+                           vz - p.getFieldAs<double>(Dimension::Id::Z));
+        if (vp.dot(normal) < 0)
+            normal *= -1.0;
+
+        view.setField(Dimension::Id::NormalX, i, normal[0]);
+        view.setField(Dimension::Id::NormalY, i, normal[1]);
+        view.setField(Dimension::Id::NormalZ, i, normal[2]);
 
         double sum = eval[0] + eval[1] + eval[2];
         view.setField(Dimension::Id::Curvature, i,
-            sum ? std::fabs(eval[0] / sum) : 0);
+                      sum ? std::fabs(eval[0] / sum) : 0);
     }
 }
 
