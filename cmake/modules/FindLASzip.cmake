298a09a4f2913a9fc5c7f763ac2ba9aeb42df449
Howard Butler <howard@hobu.co>
2017-09-18 14:38:39 -0500

    laszip directory in FindLASzip.cmake

--- cmake/modules/FindLASzip.cmake
+++ cmake/modules/FindLASzip.cmake
@@ -53,7 +53,7 @@ FIND_LIBRARY(LASZIP_LIBRARY
   ${OSGEO4W_ROOT_DIR}/lib)
 
 # Comment out laszip.hpp version info
-SET(LASZIP_VERSION_H "${LASZIP_INCLUDE_DIR}/laszip_api_version.h")
+SET(LASZIP_VERSION_H "${LASZIP_INCLUDE_DIR}/laszip/laszip_api_version.h")
 IF(LASZIP_INCLUDE_DIR AND EXISTS ${LASZIP_VERSION_H})
   SET(LASZIP_VERSION 0)
 
