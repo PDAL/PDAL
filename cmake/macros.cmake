618b2c44fcbda337db76fecd6f5913eafa420348
Andrew Bell <andrew.bell.ia@gmail.com>
2017-11-01 11:51:25 -0400

    Supportobj (#1715)
    * Only compile Support.cpp once.
    
    * Generate dimension header file for Support.cpp.

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -162,14 +162,12 @@ macro(PDAL_ADD_TEST _name)
     set(oneValueArgs)
     set(multiValueArgs FILES LINK_WITH)
     cmake_parse_arguments(PDAL_ADD_TEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-    set(common_srcs
-        ${PROJECT_SOURCE_DIR}/test/unit/Support.cpp
-    )
     if (WIN32)
         list(APPEND ${PDAL_ADD_TEST_FILES} ${PDAL_TARGET_OBJECTS})
         add_definitions("-DPDAL_DLL_EXPORT=1")
     endif()
-    add_executable(${_name} ${PDAL_ADD_TEST_FILES} ${common_srcs})
+    add_executable(${_name} ${PDAL_ADD_TEST_FILES}
+        $<TARGET_OBJECTS:${PDAL_TEST_SUPPORT_OBJS}>)
     target_include_directories(${_name} PRIVATE
         ${ROOT_DIR}
         ${PDAL_INCLUDE_DIR}
