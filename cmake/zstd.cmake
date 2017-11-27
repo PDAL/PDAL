f413e65e5de91db6d2271e1752f9249025e85658
Andrew Bell <andrew.bell.ia@gmail.com>
2017-11-27 10:57:54 -0500

    Made compression modules optional.

--- cmake/zstd.cmake
+++ cmake/zstd.cmake
@@ -1,13 +1,17 @@
 #
 # LZMA support
 #
-find_package(ZSTD REQUIRED)
-set_package_properties(ZSTD PROPERTIES TYPE REQUIRED
+option(WITH_ZSTD
+    "Build support for compression/decompression with Zstd.")
+if (WITH_ZSTD)
+    find_package(ZSTD REQUIRED)
+    set_package_properties(ZSTD PROPERTIES TYPE REQUIRED
         PURPOSE "General compression support")
-if(ZSTD_FOUND)
-    set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES}
-        "${ZSTD_STATIC_LIB}")
-    mark_as_advanced(CLEAR ZSTD_INCLUDE_DIRS)
-    mark_as_advanced(CLEAR ZSTD_LIBRARIES)
-    set(PDAL_HAVE_ZSTD 1)
-endif()
+    if (ZSTD_FOUND)
+        set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES}
+            "${ZSTD_STATIC_LIB}")
+        mark_as_advanced(CLEAR ZSTD_INCLUDE_DIRS)
+        mark_as_advanced(CLEAR ZSTD_LIBRARIES)
+        set(PDAL_HAVE_ZSTD 1)
+    endif(ZSTD_FOUND)
+endif(WITH_ZSTD)
