4f63d2b3138d9addc4f55401155fa2668583a4fe
Andrew Bell <andrew.bell.ia@gmail.com>
2018-01-17 12:08:02 -0500

    Throw error if we can't open PLY file.

--- io/PlyReader.cpp
+++ io/PlyReader.cpp
@@ -291,6 +291,8 @@ std::string PlyReader::getName() const
 void PlyReader::initialize()
 {
     m_stream = Utils::openFile(m_filename, true);
+    if (!m_stream)
+        throwError("Couldn't open '" + m_filename + "'.");
     extractHeader();
     Utils::closeFile(m_stream);
     m_stream = nullptr;
