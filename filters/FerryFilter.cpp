50ace60ea647fdfeee08d939cbb9f3f11fcd25b3
Connor Manning <connor@hobu.co>
2018-01-19 09:39:02 -0600

    Register the FerryFilter's destination dimension with the same type as the source, rather than always a double.

--- filters/FerryFilter.cpp
+++ filters/FerryFilter.cpp
@@ -84,8 +84,12 @@ void FerryFilter::initialize()
 void FerryFilter::addDimensions(PointLayoutPtr layout)
 {
     for (auto& info : m_dims)
-        info.m_toId = layout->registerOrAssignDim(info.m_toName,
-            Dimension::Type::Double);
+    {
+        const Dimension::Id fromId = layout->findDim(info.m_fromName);
+        const Dimension::Type fromType = layout->dimType(fromId);
+
+        info.m_toId = layout->registerOrAssignDim(info.m_toName, fromType);
+    }
 }
 
 
