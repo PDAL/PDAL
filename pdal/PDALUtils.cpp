dc685cc8bc1ef18cf62c6ded7ed1f2048c7d1e49
Andrew Bell <andrew.bell.ia@gmail.com>
2018-01-24 12:33:12 -0500

    Fix typo.

--- pdal/PDALUtils.cpp
+++ pdal/PDALUtils.cpp
@@ -33,7 +33,7 @@
 ****************************************************************************/
 
 #include <pdal/PDALUtils.hpp>
-#include <pdal/pdal_features.hpp>
+#include <pdal/pdal_features.h>
 
 #ifdef PDAL_ARBITER_ENABLED
     #include <arbiter/arbiter.hpp>
