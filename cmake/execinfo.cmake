115837239fd2f8dd31ee8f12318288689a1dd09a
Andrew Bell <andrew.bell.ia@gmail.com>
2018-03-14 14:08:21 -0500

    Test.

--- cmake/execinfo.cmake
+++ cmake/execinfo.cmake
@@ -7,6 +7,8 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EXECINFO
     REQUIRED_VARS
         EXECINFO_LIBRARIES)
+message("#### EXECINFO LIBRARIES = ${EXECINFO_LIBRARIES}")
 if (HAVE_EXECINFO)
+    message("#### Found EXECINFO = ${EXECINFO_LIBRARIES}")
     set(EXECINFO_LIBRARY ${EXECINFO_LIBRARIES})
 endif()
