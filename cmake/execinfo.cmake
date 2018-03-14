458ebdf9059569d1e706b8a5bd77f61bed9b297b
Andrew Bell <andrew.bell.ia@gmail.com>
2018-03-14 14:21:58 -0500

    Remove debug.

--- cmake/execinfo.cmake
+++ cmake/execinfo.cmake
@@ -3,8 +3,6 @@
 #
 
 find_library(EXECINFO_LIBRARIES execinfo)
-message("#### EXECINFO = ${EXECINFO_LIBRARIES}")
 if (EXECINFO_LIBRARIES)
-    message("#### Found EXECINFO = ${EXECINFO_LIBRARIES}")
     set(EXECINFO_LIBRARY ${EXECINFO_LIBRARIES})
 endif()
