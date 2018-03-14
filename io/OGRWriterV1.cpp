b2cf44bed9027196b1bd66ade8576e778daf79e3
Andrew Bell <andrew.bell.ia@gmail.com>
2018-03-14 09:09:31 -0500

    Missed comma.

--- io/OGRWriterV1.cpp
+++ io/OGRWriterV1.cpp
@@ -55,7 +55,7 @@ static StaticPluginInfo const s_info
 {
     "writers.ogr",
     "Write a point cloud as a set of OGR points/multipoints",
-    "http://pdal.io/stages/writers.ogr.html"
+    "http://pdal.io/stages/writers.ogr.html",
     { "shp", "geojson" }
 };
 
