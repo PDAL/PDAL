d7b20fdcb99060f33a8f1afda9543701c381cdfd
Andrew Bell <andrew.bell.ia@gmail.com>
2018-03-14 13:50:32 -0500

    Finding libraries uses strange logic.

--- cmake/execinfo.cmake
+++ cmake/execinfo.cmake
@@ -2,4 +2,11 @@
 # See if we have an execinfo library.
 #
 
-find_library(EXECINFO_LIBRARY execinfo)
+find_library(EXECINFO_LIBRARIES execinfo)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(EXECINFO
+    REQUIRED_VARS
+        EXECINFO_LIBRARIES)
+if (HAVE_EXECINFO)
+    set(EXECINFO_LIBRARY ${EXECINFO_LIBRARIES})
+endif()
