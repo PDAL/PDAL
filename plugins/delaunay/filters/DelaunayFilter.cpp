05d5915f203b3631a09b29656d6df73f902f00ca
Peter Limkilde Svendsen <pelis@sdfe.dk>
2018-03-22 18:04:09 +0100

    Added Geogram PSM, currently crashes when run

--- plugins/delaunay/filters/DelaunayFilter.cpp
+++ plugins/delaunay/filters/DelaunayFilter.cpp
@@ -1,6 +1,7 @@
 // TODO: insert copyright notice
 
 #include "DelaunayFilter.hpp"
+#include "Delaunay_psm.h"
 
 namespace pdal
 {
@@ -21,6 +22,31 @@ std::string DelaunayFilter::getName() const
 
 PointViewSet DelaunayFilter::run(PointViewPtr pointView)
 {
+    std::cout << "Point view size is " << pointView->size() << "." << std::endl;
+    
+    std::vector<double> delaunayPoints;
+    for (PointId i = 0; i < pointView->size(); i++)
+    {
+	PointRef point(*pointView, i);
+	double x(point.getFieldAs<double>(Dimension::Id::X));
+	double y(point.getFieldAs<double>(Dimension::Id::Y));
+	double z(point.getFieldAs<double>(Dimension::Id::Z));
+	std::cout << "(" << x << ", " << y << ", " << z <<  ")" << std::endl;
+	
+	delaunayPoints.push_back(x);
+	delaunayPoints.push_back(y);
+	delaunayPoints.push_back(z);
+    }
+    
+    GEO::initialize();
+    //GEO::CmdLine::import_arg_group("standard");
+    
+    int numDimensions = 3;
+    GEO::Delaunay_var triangulation = GEO::Delaunay::create(GEO::coord_index_t(numDimensions));
+    GEO::index_t numPoints = delaunayPoints.size() / numDimensions;
+    //triangulation->set_vertices(numPoints, delaunayPoints.data());
+    
+    
     // TODO Check for null (= already exists)
     TriangularMesh *mesh = pointView->createMesh("delaunay");
     
