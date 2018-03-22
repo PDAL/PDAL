df7ec90910c025df354f89279b32eeed49497d1b
Andrew Bell <andrew.bell.ia@gmail.com>
2018-03-22 10:32:08 +0100

    More warning suppression.

--- plugins/nitf/io/MetadataReader.hpp
+++ plugins/nitf/io/MetadataReader.hpp
@@ -42,6 +42,7 @@
 #pragma GCC diagnostic ignored "-Wredundant-decls"
 #pragma GCC diagnostic ignored "-Wextra"
 #pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wunused-private-field"
 
 #include <nitro/c++/import/nitf.hpp>
 
