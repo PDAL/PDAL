a7d3f426cb3ed0d3c93fcda8a2355d4a22c0401a
Howard Butler <howard@hobu.co>
2018-04-06 00:11:40 -0500

    run install in the build directory

--- scripts/appveyor/install.cmd
+++ scripts/appveyor/install.cmd
@@ -6,7 +6,11 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd6
 
 set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\7-Zip;C:\Program Files\Microsoft Windows Performance Toolkit\;C:\Program Files (x86)\Windows Kits\8.1\Windows Performance Toolkit\;C:\Tools\GitVersion;C:\Program Files (x86)\CMake\bin;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin;C:\Program Files\AppVeyor\BuildAgent\
 
+pushd c:\projects\pdal\build
+
 nmake /f Makefile install  DESTDIR=C:\projects\pdal\install
+
+popd
 
 pushd c:\projects\pdal\install\osgeo4w64
 
