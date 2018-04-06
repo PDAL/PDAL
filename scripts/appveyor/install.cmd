40f1b809263f2f44588260ad6cb52af4a54ae005
Howard Butler <howard@hobu.co>
2018-04-05 22:08:00 -0500

    turn back on OSGeo4W package building

--- scripts/appveyor/install.cmd
+++ scripts/appveyor/install.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-REM call %OSGEO4W_ROOT%\bin\o4w_env.bat
-REM call %OSGEO4W_ROOT%\bin\py3_env.bat
+call %OSGEO4W_ROOT%\bin\o4w_env.bat
+call %OSGEO4W_ROOT%\bin\py3_env.bat
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 
-REM set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\7-Zip;C:\Program Files\Microsoft Windows Performance Toolkit\;C:\Program Files (x86)\Windows Kits\8.1\Windows Performance Toolkit\;C:\Tools\GitVersion;C:\Program Files (x86)\CMake\bin;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin;C:\Program Files\AppVeyor\BuildAgent\
+set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\7-Zip;C:\Program Files\Microsoft Windows Performance Toolkit\;C:\Program Files (x86)\Windows Kits\8.1\Windows Performance Toolkit\;C:\Tools\GitVersion;C:\Program Files (x86)\CMake\bin;C:\Program Files\Git\cmd;C:\Program Files\Git\usr\bin;C:\Program Files\AppVeyor\BuildAgent\
 
 nmake /f Makefile install  DESTDIR=C:\projects\pdal\install
 
