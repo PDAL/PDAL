bfc912eacd126066b6a578bdda676ed6e75a6c42
Peter Limkilde Svendsen <peter.limkilde@gmail.com>
2018-04-09 13:34:47 +0200

    Add license text

--- plugins/delaunay/filters/DelaunayFilter.cpp
+++ plugins/delaunay/filters/DelaunayFilter.cpp
@@ -1,4 +1,36 @@
-// TODO: insert copyright notice
+/******************************************************************************
+* Copyright (c) 2018, Danish Agency for Data Supply and Efficiency,
+* sdfe@sdfe.dk
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following
+* conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in
+*       the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of SDFE nor the names of its contributors may be
+*       used to endorse or promote products derived from this software
+*       without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+* OF SUCH DAMAGE.
+****************************************************************************/
 
 #include "DelaunayFilter.hpp"
 #include "Delaunay_psm.h"
@@ -27,12 +59,12 @@ PointViewSet DelaunayFilter::run(PointViewPtr pointView)
     std::vector<double> delaunayPoints;
     for (PointId i = 0; i < pointView->size(); i++)
     {
-	PointRef point(*pointView, i);
-	double x(point.getFieldAs<double>(Dimension::Id::X));
-	double y(point.getFieldAs<double>(Dimension::Id::Y));
-	
-	delaunayPoints.push_back(x);
-	delaunayPoints.push_back(y);
+        PointRef point(*pointView, i);
+        double x(point.getFieldAs<double>(Dimension::Id::X));
+        double y(point.getFieldAs<double>(Dimension::Id::Y));
+        
+        delaunayPoints.push_back(x);
+        delaunayPoints.push_back(y);
     }
     
     GEO::initialize();
@@ -49,11 +81,11 @@ PointViewSet DelaunayFilter::run(PointViewPtr pointView)
     std::cout << triangulation->nb_cells() << " cells in triangulation." << std::endl;
     for (GEO::index_t i = 0; i < triangulation->nb_cells(); i++)
     {
-	GEO::index_t v_0 = triangulation->cell_vertex(i, 0);
-	GEO::index_t v_1 = triangulation->cell_vertex(i, 1);
-	GEO::index_t v_2 = triangulation->cell_vertex(i, 2);
-	
-	mesh->add((int)v_0, (int)v_1, (int)v_2);
+        GEO::index_t v_0 = triangulation->cell_vertex(i, 0);
+        GEO::index_t v_1 = triangulation->cell_vertex(i, 1);
+        GEO::index_t v_2 = triangulation->cell_vertex(i, 2);
+        
+        mesh->add((int)v_0, (int)v_1, (int)v_2);
     }
     
     PointViewSet viewSet;
