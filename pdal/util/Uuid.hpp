b063449f99aea3d250811b1cdaa4187c087efda7
Andrew Bell <andrew.bell.ia@gmail.com>
2018-05-02 11:57:03 -0400

    Move Uuid code to .cpp file to simplify Windows linking.

--- pdal/util/Uuid.hpp
+++ pdal/util/Uuid.hpp
@@ -59,13 +59,6 @@
 
 #include "pdal_util_export.hpp"
 
-#include "Inserter.hpp"
-#include "Extractor.hpp"
-
-#pragma warning(push)
-#pragma warning(disable: 4267 4244)  // ignore conversion warnings
-
-
 namespace pdal
 {
 
@@ -97,102 +90,20 @@ inline bool operator < (const uuid& u1, const uuid& u2)
 
 class PDAL_DLL Uuid
 {
-    friend inline bool operator < (const Uuid& u1, const Uuid& u2);
+    friend bool operator < (const Uuid& u1, const Uuid& u2);
 public:
-    Uuid()
-        { clear(); }
-    Uuid(const char *c)
-        { unpack(c); }
-    Uuid(const std::string& s)
-        { parse(s); }
+    Uuid();
+    Uuid(const char *c);
+    Uuid(const std::string& s);
 
-    void clear()
-        { memset(&m_data, 0, sizeof(m_data)); }
-
-    void unpack(const char *c)
-    {
-        BeExtractor e(c, 10);
-
-        e >> m_data.time_low >> m_data.time_mid >>
-            m_data.time_hi_and_version >> m_data.clock_seq;
-        c += 10;
-        std::copy(c, c + 6, m_data.node);
-    }
-
-    void pack(char *c) const
-    {
-        BeInserter i(c, 10);
-
-        i << m_data.time_low << m_data.time_mid <<
-            m_data.time_hi_and_version << m_data.clock_seq;
-        c += 10;
-        std::copy(m_data.node, m_data.node + 6, c);
-    }
-
-    bool parse(const std::string& s)
-    {
-        if (s.length() != 36)
-            return false;
-
-        // Format validation.
-        const char *cp = s.data();
-        for (size_t i = 0; i < 36; i++) {
-            if ((i == 8) || (i == 13) || (i == 18) || (i == 23))
-            {
-                if (*cp != '-')
-                    return false;
-            }
-            else if (!isxdigit(*cp))
-                return false;
-            ++cp;
-        }
-
-        cp = s.data();
-        m_data.time_low = strtoul(cp, NULL, 16);
-        m_data.time_mid = (uint16_t)strtoul(cp + 9, NULL, 16);
-        m_data.time_hi_and_version = (uint16_t)strtoul(cp + 14, NULL, 16);
-        m_data.clock_seq = (uint16_t)strtoul(cp + 19, NULL, 16);
-
-        // Extract bytes as pairs of hex digits.
-        cp = s.data() + 24;
-        char buf[3];
-        buf[2] = 0;
-        for (size_t i = 0; i < 6; i++) {
-            buf[0] = *cp++;
-            buf[1] = *cp++;
-            m_data.node[i] = (uint8_t)strtoul(buf, NULL, 16);
-        }
-        return true;
-    }
-
-    std::string unparse() const
-    {
-        std::vector<char> buf(36 + 1);
-        const char fmt[] = "%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X";
-
-        // TODO: use snprintf after switch to C++11
-        sprintf(buf.data(), fmt,
-            m_data.time_low, m_data.time_mid, m_data.time_hi_and_version,
-            m_data.clock_seq >> 8, m_data.clock_seq & 0xFF,
-            m_data.node[0], m_data.node[1], m_data.node[2],
-            m_data.node[3], m_data.node[4], m_data.node[5]);
-        return std::string(buf.data());
-    }
-
-    std::string toString() const
-        { return unparse(); }
-
-    bool empty() const
-    { return isNull(); }
-
-    bool isNull() const
-    {
-        const char *c = (const char *)&m_data;
-        for (size_t i = 0; i < sizeof(m_data); ++i)
-            if (*c++ != 0)
-                return false;
-        return true;
-    }
+    void clear();
+    void unpack(const char *c);
+    void pack(char *c) const;
+    bool parse(const std::string& s);
+    std::string unparse() const;
+    std::string toString() const;
+    bool empty() const;
+    bool isNull() const;
 
 /**
     // Sadly, MS doesn't do constexpr.
@@ -205,32 +116,10 @@ private:
     uuid m_data;
 };
 
-inline bool operator == (const Uuid& u1, const Uuid& u2)
-{
-    return !(u1 < u2) && !(u2 < u1);
-}
-
-inline bool operator < (const Uuid& u1, const Uuid& u2)
-{
-    return u1.m_data < u2.m_data;
-}
-
-inline std::ostream& operator << (std::ostream& out, const Uuid& u)
-{
-    out << u.toString();
-    return out;
-}
-
-inline std::istream& operator >> (std::istream& in, Uuid& u)
-{
-    std::string s;
-    in >> s;
-    if (!u.parse(s))
-        in.setstate(std::ios::failbit);
-    return in;
-}
+bool operator == (const Uuid& u1, const Uuid& u2);
+bool operator < (const Uuid& u1, const Uuid& u2);
+std::ostream& operator << (std::ostream& out, const Uuid& u);
+std::istream& operator >> (std::istream& in, Uuid& u);
 
 } // namespace pdal
-
-#pragma warning(pop)
 
