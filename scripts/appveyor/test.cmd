712bf11e090cb71c8a7582a9682eaa2e0741a63b
Howard Butler <howard@hobu.co>
2018-10-24 12:01:29 -0500

    sprink some echo bs around to try to figure out WTH conda does to my environment

--- scripts/appveyor/test.cmd
+++ scripts/appveyor/test.cmd
@@ -1,9 +1,14 @@
 pushd build
 
+echo %CD%
 set CONDA_ROOT="C:\Miniconda3-x64"
 SET PDAL_PLUGIN_INSTALL_PATH="C:/projects/pdal/build/bin"
-set PATH="%CONDA_ROOT%;%CONDA_ROOT%\scripts;%CONDA_ROOT%\Library\bin;%PATH%;C:\Program Files (x86)\CMake\bin"
 call "%CONDA_ROOT%\Scripts\activate.bat" base
+set PATH="%PATH%;C:\Program Files (x86)\CMake\bin"
+echo %CD%
+cd "c:\projects\pdal\build"
+echo %CD%
 ctest -V --output-on-failure
+echo %CD%
 
 popd
