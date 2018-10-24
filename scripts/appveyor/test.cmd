a051ac1cc12bd7eae61c3151e4fcaea5a1cbc9a6
Howard Butler <howard@hobu.co>
2018-10-24 14:10:21 -0500

    ugh

--- scripts/appveyor/test.cmd
+++ scripts/appveyor/test.cmd
@@ -1,13 +1,11 @@
 pushd build
 
-echo %CD%
 set CONDA_ROOT="C:\Miniconda3-x64"
 SET PDAL_PLUGIN_INSTALL_PATH="C:/projects/pdal/build/bin"
 call "%CONDA_ROOT%\Scripts\activate.bat" base
-set PATH="%PATH%;C:\Program Files (x86)\CMake\bin"
-echo %CD%
+set PATH="%CONDA_ROOT%;%CONDA_ROOT%\scripts;%CONDA_ROOT%\Library\bin;%PATH%;C:\Program Files (x86)\CMake\bin"
+echo %PATH%
 cd "c:\projects\pdal\build"
-echo %CD%
 ctest -V --output-on-failure
 echo %CD%
 
