433034eb1a71cd5fcd7fd04b62a7381d2cbd0713
Howard Butler <howard@hobu.co>
2018-10-24 16:59:42 -0500

    random conda

--- scripts/appveyor/test.cmd
+++ scripts/appveyor/test.cmd
@@ -1,8 +1,8 @@
 pushd build
 
-set CONDA_ROOT=C:\\Miniconda3-x64
+REM set CONDA_ROOT=C:\\Miniconda3-x64
 SET PDAL_DRIVER_PATH="C:\projects\pdal\build\bin"
-call %CONDA_ROOT%\\Scripts\\activate.bat base
+SET GEOTIFF_CSV=c:\\Minicoda3-x64\\Library\\share\\epsg_csv
 cd "c:\projects\pdal\build"
 ctest -V --output-on-failure
 echo %CD%
