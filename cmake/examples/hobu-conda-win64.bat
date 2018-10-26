2c7d37ceaa1d9265f37be990df3c017a0d014c39
Howard Butler <howard@hobu.co>
2018-10-26 10:39:33 -0500

    howard conda-based windows build

--- cmake/examples/hobu-conda-win64.bat
+++ cmake/examples/hobu-conda-win64.bat
@@ -1,18 +1,40 @@
-del /s /q build-project
-mkdir build-project
-cd build-project
+set CONDA_ENVIRO=pdal-build
 
-set GENERATOR="Visual Studio 14 2015 Win64"
+del /s /q  %CONDA_ENVIRO%
+mkdir %CONDA_ENVIRO%
+pushd %CONDA_ENVIRO%
+
+set GDAL_VERSION=2.2.4
+set NUMPY_VERSION=1.15.3
+
+call conda config --set always_yes yes
+IF ERRORLEVEL 1 GOTO CLEANUP
+
+call conda remove --name %CONDA_ENVIRO% -y --all
+IF ERRORLEVEL 1 GOTO CLEANUP
+
+call conda create --name %CONDA_ENVIRO% -y
+IF ERRORLEVEL 1 GOTO CLEANUP
+
+call %CONDA_PREFIX%\\Scripts\activate.bat %CONDA_ENVIRO%
+IF ERRORLEVEL 1 GOTO CLEANUP
+
+call conda config --add channels conda-forge
+IF ERRORLEVEL 1 GOTO CLEANUP
+
+call conda install geotiff laszip nitro curl gdal=%GDAL_VERSION% pcl cmake eigen ninja libgdal=%GDAL_VERSION% geos zstd numpy=%NUMPY_VERSION% xz libxml2 laz-perf qhull sqlite hdf5 oracle-instantclient numpy-base=%NUMPY_VERSION%
+
+IF ERRORLEVEL 1 GOTO CLEANUP
+
+REM set GENERATOR="Visual Studio 14 2015 Win64"
 REM set GENERATOR="NMake Makefiles"
 set GENERATOR="Ninja"
 
-set CONDA_ROOT=c:\miniconda3-x64
-set ORACLE_HOME=%CONDA_ROOT%
+set ORACLE_HOME=%CONDA_PREFIX%
 cmake -G %GENERATOR% ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_ROOT%" ^
       -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo ^
-      -DCMAKE_LIBRARY_PATH:FILEPATH="=%CONDA_ROOT%/Library/lib"ninj ^
-      -DCMAKE_INCLUDE_PATH:FILEPATH="%CONDA_ROOT%/Library/include" ^
+      -DCMAKE_LIBRARY_PATH:FILEPATH="=%CONDA_PREFIX%/Library/lib" ^
+      -DCMAKE_INCLUDE_PATH:FILEPATH="%CONDA_PREFIX%/Library/include" ^
       -DBUILD_PLUGIN_GREYHOUND=ON ^
       -DBUILD_PLUGIN_PCL=ON ^
       -DBUILD_PLUGIN_PYTHON=ON ^
@@ -24,17 +46,20 @@ cmake -G %GENERATOR% ^
       -DBUILD_PLUGIN_NITF=ON ^
       -DENABLE_CTEST=OFF ^
       -DWITH_TESTS=ON ^
-	-DORACLE_INCLUDE_DIR=%CONDA_ROOT%/include ^
-	-DORACLE_LIBRARY=%CONDA_ROOT%/libs/oci.lib ^
-    -DLazperf_DIR=%CONDA_ROOT%/Library/cmake ^
-	-DPYTHON_DEBUG_LIBRARY= ^
-    -DHDF5_DIR=%CONDA_ROOT%/Library/cmake ^
-    -DPCL_DIR=%CONDA_ROOT%/Library/cmake ^
-    -DWITH_ZLIB=ON ^
-    -DBUILD_PGPOINTCLOUD_TESTS=OFF ^
-    -DBUILD_SQLITE_TESTS=OFF ^
-    -DBUILD_OCI_TESTS=OFF ^
-    -DCMAKE_VERBOSE_MAKEFILE=OFF ^
+      -DWITH_ZLIB=ON ^
+      -DBUILD_PGPOINTCLOUD_TESTS=OFF ^
+      -DBUILD_SQLITE_TESTS=OFF ^
+      -DBUILD_OCI_TESTS=OFF ^
+      -DCMAKE_VERBOSE_MAKEFILE=OFF ^
       -DWITH_LAZPERF=ON ^
       -DWITH_LASZIP=ON ^
       .. --debug-trycompile
+
+call ninja
+IF ERRORLEVEL 1 GOTO CLEANUP
+
+:CLEANUP
+call conda deactivate
+popd
+exit /b
+
