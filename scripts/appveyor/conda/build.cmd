39fc75a84cbd21bc3af51f98dde4db6061fbb63c
Howard Butler <howard@hobu.co>
2019-02-08 19:44:44 -0600

    positional argument at the end

--- scripts/appveyor/conda/build.cmd
+++ scripts/appveyor/conda/build.cmd
@@ -2,5 +2,5 @@ call "%CONDA_ROOT%\Scripts\activate.bat" base
 
 pushd "scripts\\appveyor\\conda\\recipe"
 dir
-conda build . --output c:\projects\pdal\build\pdal-%APPVEYOR_REPO_COMMIT%-py37_appveyor.tar.bz2
+conda build  --output c:\projects\pdal\build\pdal-%APPVEYOR_REPO_COMMIT%-py37_appveyor.tar.bz2 .
 popd
