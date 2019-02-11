84ba17455cef90fac2b91449f3d4ba7c2061c37a
Howard Butler <howard@hobu.co>
2019-02-11 13:16:07 -0600

    start /wait

--- scripts/appveyor/conda/build.cmd
+++ scripts/appveyor/conda/build.cmd
@@ -3,7 +3,7 @@ call conda install conda-build
 pushd "scripts\\appveyor\\conda\\recipe"
 REM pdal-%APPVEYOR_REPO_COMMIT%-py37_appveyor
 dir
-call conda build  --output-folder c:\projects\pdal\build .
+start /wait conda build  --output-folder c:\projects\pdal\build .
 popd
 
 pushd build
