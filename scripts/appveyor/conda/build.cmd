9e97f006d292a8e9582dadc32bc21a05a6d3fd2a
Howard Butler <howard@hobu.co>
2019-02-11 13:32:00 -0600

    try build mkdir

--- scripts/appveyor/conda/build.cmd
+++ scripts/appveyor/conda/build.cmd
@@ -2,8 +2,9 @@ call "%CONDA_ROOT%\Scripts\activate.bat" base
 call conda install conda-build
 pushd "scripts\\appveyor\\conda\\recipe"
 REM pdal-%APPVEYOR_REPO_COMMIT%-py37_appveyor
+mkdir build
 dir
-start /wait conda build  --output-folder c:\projects\pdal\build .
+conda build  --output-folder c:\projects\pdal\build .
 popd
 
 pushd build
