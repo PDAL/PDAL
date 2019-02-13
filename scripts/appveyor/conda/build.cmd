77a3269d56ad6253aa877e240e9dfa17dcdcc2e2
Howard Butler <howard@hobu.co>
2019-02-13 15:30:44 -0600

    debugging lint

--- scripts/appveyor/conda/build.cmd
+++ scripts/appveyor/conda/build.cmd
@@ -4,17 +4,12 @@ call conda config --set path_conflict clobber
 pushd "scripts\\appveyor\\conda\\recipe"
 REM pdal-%APPVEYOR_REPO_COMMIT%-py37_appveyor
 mkdir build
-dir
 call conda build  --output-folder c:\projects\pdal\build .
 
-dir
 FOR /F %%I IN ('DIR c:\projects\pdal\build\win-64\*.bz2 /B /O:-D')  DO echo %%I > condaPackage
 set /p CONDA_PACKAGE= < condaPackage
-dir
 DEL condaPackage
-echo %CONDA_PACKAGE%
 copy c:\projects\pdal\build\win-64\%CONDA_PACKAGE% c:\projects\pdal\pdal-%APPVEYOR_REPO_COMMIT%-py37_appveyor.tar.bz2
 
-dir c:\projects\pdal
 conda build purge
 popd
