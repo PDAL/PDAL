3a934ca35bd0f7e8754706eb34f0b2022199bf80
Andrew Bell <andrew.bell.ia@gmail.com>
2019-02-13 13:42:34 -0500

    Update python.cmake (#2369)
    * Test python module.
    
    * Set python min version to 2.4.
    
    * Require version 2.7 of Python.

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,8 +1,15 @@
 #
 # Python
 #
-find_package(PythonInterp QUIET)
-find_package(PythonLibs QUIET 2.4)
+
+# Try to find version 3.  If not, we fall back to version 2.
+find_package(PythonInterp QUIET 3)
+find_package(PythonInterp QUIET 2.7)
+
+# Looking for PythonLibs will use the version of the intpreter found to
+# tell it which version of the libraries to use.
+find_package(PythonLibs QUIET)
+
 set_package_properties(PythonInterp PROPERTIES TYPE REQUIRED)
 if(PYTHONLIBS_FOUND)
     set(CMAKE_REQUIRED_LIBRARIES "${PYTHON_LIBRARY}")
