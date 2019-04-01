261457f2a40e1322ea82562a11fd9699ac222a52
Andrew Bell <andrew.bell.ia@gmail.com>
2019-04-01 13:29:41 -0400

    Better handle mixed python 2 and 3 packaging. Close #2431

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -2,22 +2,33 @@
 # Python
 #
 
-# Try to find version 3.  If not, we fall back to version 2.
-find_package(PythonInterp QUIET 3)
-find_package(PythonInterp QUIET 2.7)
+#
+# Version 3.12 has shiny new FindPython2 and FindPython3 scripts
+#
+if (NOT (CMAKE_VERSION VERSION_LESS "3.12.0"))
+    find_package(Python3 QUIET COMPONENTS Interpreter Development NumPy)
+    if (NOT PYTHON3_FOUND)
+        find_package(Python2 2.7 REQUIRED
+            COMPONENTS Interpreter Development Numpy)
 
-# Looking for PythonLibs will use the version of the intpreter found to
-# tell it which version of the libraries to use.
-find_package(PythonLibs QUIET)
-
-set_package_properties(PythonInterp PROPERTIES TYPE REQUIRED)
-if(PYTHONLIBS_FOUND)
-    set(CMAKE_REQUIRED_LIBRARIES "${PYTHON_LIBRARY}")
-    include_directories(SYSTEM ${PYTHON_INCLUDE_DIR})
-    add_definitions(-DHAVE_PYTHON=1)
+        # Since we've required 2.7, these should all be valid
+        set(PYTHON_LIBRARIES ${Python2_LIBRARIES})
+        set(PYTHON_INCLUDE_DIRS ${Python2_INCLUDE_DIRS})
+        set(PYTHON_NUMPY_INCLUDE_DIRS ${Python2_NumPy_INCLUDE_DIRS})
+    else()
+        set(PYTHON_LIBRARIES ${Python3_LIBRARIES})
+        set(PYTHON_INCLUDE_DIRS ${Python3_INCLUDE_DIRS})
+        set(PYTHON_NUMPY_INCLUDE_DIRS ${Python3_NumPy_INCLUDE_DIRS})
+    endif()
     set(PDAL_HAVE_PYTHON 1)
-    set(PDAL_PYTHON_VERSION_STRING "${PYTHONLIBS_VERSION_STRING}" CACHE STRING "PDAL Python version" FORCE)
-
-    find_package(NumPy QUIET 1.5 REQUIRED)
-    include_directories(SYSTEM ${NUMPY_INCLUDE_DIR})
+else()
+    find_package(PythonInterp 3 QUIET)
+    find_package(PythonLibs 3 QUIET)
+    if ((NOT PYTHONINTERP_FOUND) OR (NOT PYTHONLIBS_FOUND))
+        unset(PYTHON_EXECUTABLE CACHE)
+        find_package(PythonInterp 2.7 EXACT REQUIRED)
+        find_package(PythonLibs 2.7 EXACT REQUIRED)
+    endif()
+    set(PDAL_HAVE_PYTHON 1)
+    find_package(NumPy 1.5 REQUIRED)
 endif()
