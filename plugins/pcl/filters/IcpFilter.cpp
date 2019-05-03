2d0df544d3ead38d6c9208b0bdd0e812ba352e87
Bradley J Chambers <brad.chambers@gmail.com>
2019-05-03 14:24:12 -0400

    Add bounds back to the transformed points

--- plugins/pcl/filters/IcpFilter.cpp
+++ plugins/pcl/filters/IcpFilter.cpp
@@ -113,9 +113,9 @@ PointViewPtr IcpFilter::icp(PointViewPtr fixed, PointViewPtr moving) const
     assert(moving->size() == result.points.size());
     for (PointId i = 0; i < moving->size(); ++i)
     {
-        moving->setField(Dimension::Id::X, i, result.points[i].x);
-        moving->setField(Dimension::Id::Y, i, result.points[i].y);
-        moving->setField(Dimension::Id::Z, i, result.points[i].z);
+        moving->setField(Dimension::Id::X, i, result.points[i].x) + buffer_bounds.minx;
+        moving->setField(Dimension::Id::Y, i, result.points[i].y) + buffer_bounds.miny;
+        moving->setField(Dimension::Id::Z, i, result.points[i].z) + buffer_bounds.minz;
     }
     return moving;
 }
