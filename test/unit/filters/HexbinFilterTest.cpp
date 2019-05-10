16359e04104b4a83376882e70bd9455b22ac83f7
Andrew Bell <andrew.bell.ia@gmail.com>
2019-05-10 16:45:55 -0400

    Add link to photo of data.

--- test/unit/filters/HexbinFilterTest.cpp
+++ test/unit/filters/HexbinFilterTest.cpp
@@ -103,6 +103,9 @@ TEST(HexbinFilterTest, issue_2507)
 
     // This is an arrangement with two holes.  One of the holes has two
     // islands and one of those islands has a hole.
+    // Here's a link to the picture.  the green numbers represent the hexes
+    // represented:
+    // https://photos.app.goo.gl/P3B3mU4Zre6zADEQ6
     std::vector<std::pair<int, int>> hexes {
         {0, 3}, {0, 4}, {0,5}, {0, 6},
         {1, 2}, {1, 6},
