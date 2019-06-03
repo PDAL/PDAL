cdcb96c8dbe7aa31e0fef712abf2b678a26d698c
Andrew Bell <andrew.bell.ia@gmail.com>
2019-06-03 12:14:39 -0400

    Don't run triangulation if we don't have at least three points. Use filter() instead of run() for cleaner interface. Close #2513

--- filters/DelaunayFilter.cpp
+++ filters/DelaunayFilter.cpp
@@ -53,39 +53,44 @@ std::string DelaunayFilter::getName() const
     return s_info.name;
 }
 
-PointViewSet DelaunayFilter::run(PointViewPtr pointView)
+
+DelaunayFilter::DelaunayFilter()
+{}
+
+
+DelaunayFilter::~DelaunayFilter()
+{}
+
+
+void DelaunayFilter::filter(PointView& pointView)
 {
     // Returns NULL if the mesh already exists
-    TriangularMesh *mesh = pointView->createMesh("delaunay2d");
-    
-    if (mesh != NULL)
+    TriangularMesh *mesh = pointView.createMesh("delaunay2d");
+
+    if (!mesh)
+        throwError("Unable to create mesh 'delaunay2d'.");
+    if (pointView.size() < 3)
     {
-        std::vector<double> delaunayPoints;
-        for (PointId i = 0; i < pointView->size(); i++)
-        {
-            PointRef point(*pointView, i);
-            double x(point.getFieldAs<double>(Dimension::Id::X));
-            double y(point.getFieldAs<double>(Dimension::Id::Y));
-            
-            delaunayPoints.push_back(x);
-            delaunayPoints.push_back(y);
-        }
-        
-        // Actually perform the triangulation
-        delaunator::Delaunator triangulation(delaunayPoints);
-        
-        for (std::size_t i = 0; i < triangulation.triangles.size(); i += 3)
-        {
-            mesh->add(triangulation.triangles[i+2],
-                triangulation.triangles[i+1],
-                triangulation.triangles[i]);
-        }
+        log()->get(LogLevel::Warning) << getName() << ": triangulation "
+            "requested for fewer than three points.\n";
+        return;
     }
-    
-    PointViewSet viewSet;
-    viewSet.insert(pointView);
-    
-    return viewSet;
+
+    std::vector<double> delaunayPoints;
+    for (PointId i = 0; i < pointView.size(); i++)
+    {
+        delaunayPoints.push_back(
+            pointView.getFieldAs<double>(Dimension::Id::X, i));
+        delaunayPoints.push_back(
+            pointView.getFieldAs<double>(Dimension::Id::Y, i));
+    }
+
+    // Actually perform the triangulation
+    delaunator::Delaunator triangulation(delaunayPoints);
+
+    for (std::size_t i = 0; i < triangulation.triangles.size(); i += 3)
+        mesh->add(triangulation.triangles[i+2], triangulation.triangles[i+1],
+            triangulation.triangles[i]);
 }
 
 } // namespace pdal
