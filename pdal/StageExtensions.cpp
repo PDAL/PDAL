105a1a24dd371a6feb41c62a3d1d488040c20056
Andrew Bell <andrew.bell.ia@gmail.com>
2019-06-20 10:04:57 -0400

    Add extensions to driver output. (#2573)
    * Add extensions to driver output.
    
    * Export StageExtensions::extensions()

--- pdal/StageExtensions.cpp
+++ pdal/StageExtensions.cpp
@@ -172,4 +172,26 @@ std::string StageExtensions::defaultWriter(const std::string& extension)
     return (m_writers[extension]);
 }
 
+
+StringList StageExtensions::extensions(const std::string& stage)
+{
+    StringList exts;
+
+    std::lock_guard<std::mutex> lock(m_mutex);
+
+    if (Utils::startsWith(stage, "readers."))
+    {
+        for (auto& entry : m_readers)
+            if (entry.second == stage)
+                exts.push_back(entry.first);
+    }
+    else if (Utils::startsWith(stage, "writers."))
+    {
+        for (auto& entry : m_writers)
+            if (entry.second == stage)
+                exts.push_back(entry.first);
+    }
+    return exts;
+}
+
 } // namespace pdal
