ae15172649c100dc9a866b5271fddb05ebe935f5
Nicolas Chaulet <nicolas.chaulet@gmail.com>
2019-06-28 00:33:05 +0200

    Code formatting

--- plugins/e57/io/E57Reader.cpp
+++ plugins/e57/io/E57Reader.cpp
@@ -1,39 +1,39 @@
 /******************************************************************************
-* Copyright (c) 2019, Helix Re Inc.
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following
-* conditions are met:
-*
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in
-*       the documentation and/or other materials provided
-*       with the distribution.
-*     * Neither the name of Helix Re Inc. nor the
-*       names of its contributors may be used to endorse or promote
-*       products derived from this software without specific prior
-*       written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-* OF SUCH DAMAGE.
-****************************************************************************/
+ * Copyright (c) 2019, Helix Re Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of Helix Re Inc. nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
 
-#include <iostream>
 #include <E57Exception.h>
+#include <iostream>
 
 #include "E57Reader.hpp"
 #include "Utils.hpp"
@@ -41,22 +41,24 @@
 namespace pdal
 {
 
-E57Reader::ChunkReader::ChunkReader
-(const pdal::point_count_t &pointOffset,const pdal::point_count_t &maxPointRead,
-                                    const std::shared_ptr<e57::Scan> &scan,
-                                    const std::set<std::string>& e57Dimensions):
-m_startIndex(0), m_maxPointRead(maxPointRead), m_defaultChunkSize(1e6), m_scan(scan)
+E57Reader::ChunkReader::ChunkReader(const pdal::point_count_t& pointOffset,
+                                    const pdal::point_count_t& maxPointRead,
+                                    const std::shared_ptr<e57::Scan>& scan,
+                                    const std::set<std::string>& e57Dimensions)
+    : m_startIndex(0), m_maxPointRead(maxPointRead), m_defaultChunkSize(1e6),
+      m_scan(scan)
 {
     // Initialise the read buffers
-    for (auto& dimension: e57Dimensions)
-        m_doubleBuffers[dimension] = std::vector<double>(m_defaultChunkSize,0);
+    for (auto& dimension : e57Dimensions)
+        m_doubleBuffers[dimension] = std::vector<double>(m_defaultChunkSize, 0);
 
     auto vectorNode = scan->getPoints();
     auto prototype = static_cast<e57::StructureNode>(vectorNode.prototype());
-    for (auto& keyValue: m_doubleBuffers)
-        m_e57buffers.emplace_back(vectorNode.destImageFile(), keyValue.first,
-             keyValue.second.data(), m_defaultChunkSize, true,
-             (prototype.get(keyValue.first).type() == e57::E57_SCALED_INTEGER) );
+    for (auto& keyValue : m_doubleBuffers)
+        m_e57buffers.emplace_back(
+            vectorNode.destImageFile(), keyValue.first, keyValue.second.data(),
+            m_defaultChunkSize, true,
+            (prototype.get(keyValue.first).type() == e57::E57_SCALED_INTEGER));
 
     m_pointOffset = pointOffset;
 
@@ -81,19 +83,21 @@ bool E57Reader::ChunkReader::isInChunk(pdal::point_count_t index) const
 {
     pdal::point_count_t actualIndex = index - m_pointOffset;
     if (actualIndex < m_startIndex)
-       throw std::out_of_range("E57 reader tries to go back in the data. Not allowed.");
+        throw std::out_of_range(
+            "E57 reader tries to go back in the data. Not allowed.");
 
-    return actualIndex < (m_startIndex+m_defaultChunkSize);
+    return actualIndex < (m_startIndex + m_defaultChunkSize);
 }
 
-void E57Reader::ChunkReader::setPoint
-(pdal::point_count_t pointIndex, pdal::PointRef point, const std::set<std::string>& e57Dimensions) const
+void E57Reader::ChunkReader::setPoint(
+    pdal::point_count_t pointIndex, pdal::PointRef point,
+    const std::set<std::string>& e57Dimensions) const
 {
     pdal::point_count_t actualIndex = pointIndex - m_pointOffset;
     pdal::point_count_t index = actualIndex - m_startIndex;
 
     // Sets values from buffer arrays for each dimension in the scan
-    for (auto& keyValue: m_doubleBuffers)
+    for (auto& keyValue : m_doubleBuffers)
     {
         auto pdalDimension = pdal::e57plugin::e57ToPdal(keyValue.first);
         if (pdalDimension != pdal::Dimension::Id::Unknown)
@@ -103,13 +107,14 @@ void E57Reader::ChunkReader::setPoint
             try
             {
                 auto minmax = m_scan->getLimits(pdalDimension);
-                value = pdal::e57plugin::rescaleE57ToPdalValue(keyValue.first,keyValue.second[index],minmax);
+                value = pdal::e57plugin::rescaleE57ToPdalValue(
+                    keyValue.first, keyValue.second[index], minmax);
             }
-            catch (std::out_of_range &e)
+            catch (std::out_of_range& e)
             {
                 value = keyValue.second[index];
             }
-            point.setField(pdalDimension,value);
+            point.setField(pdalDimension, value);
         }
     }
 
@@ -118,21 +123,15 @@ void E57Reader::ChunkReader::setPoint
         m_scan->transformPoint(point);
 }
 
-pdal::point_count_t E57Reader::ChunkReader::read(pdal::point_count_t  index)
+pdal::point_count_t E57Reader::ChunkReader::read(pdal::point_count_t index)
 {
     pdal::point_count_t actualIndex = index - m_pointOffset;
     m_startIndex = actualIndex;
     return m_dataReader->read();
 }
 
-
-
-static PluginInfo const s_info
-{
-    "readers.e57",
-    "E57 Reader",
-    "http://libe57.org/"
-};
+static PluginInfo const s_info{"readers.e57", "E57 Reader",
+                               "http://libe57.org/"};
 
 CREATE_SHARED_STAGE(E57Reader, s_info)
 
@@ -141,7 +140,7 @@ E57Reader::~E57Reader()
     closeFile();
 }
 
-    E57Reader::E57Reader(std::string filename)
+E57Reader::E57Reader(std::string filename)
     : Reader(), Streamable(), m_currentPoint(0), m_pointCount(0)
 {
     m_filenameManual = filename;
@@ -172,7 +171,7 @@ void E57Reader::addDimensions(PointLayoutPtr layout)
         return;
 
     auto commonDimensions = getDimensions();
-    for (auto& dim: commonDimensions)
+    for (auto& dim : commonDimensions)
     {
         auto pdalDimension = pdal::e57plugin::e57ToPdal(dim);
         if (pdalDimension != pdal::Dimension::Id::Unknown)
@@ -188,7 +187,7 @@ point_count_t E57Reader::read(PointViewPtr view, point_count_t count)
     point_count_t toReadCount = std::min(count, remainingInput);
 
     point_count_t remaining = toReadCount;
-    for (point_count_t i=0; i < remaining; i++)
+    for (point_count_t i = 0; i < remaining; i++)
     {
         PointRef point(view->point(nextId));
         m_currentPoint = nextId;
@@ -217,7 +216,7 @@ bool E57Reader::processOne(pdal::PointRef& point)
     if (!m_chunk->isInChunk(m_currentPoint))
         m_chunk->read(m_currentPoint);
 
-    m_chunk->setPoint(m_currentPoint,point, getDimensions());
+    m_chunk->setPoint(m_currentPoint, point, getDimensions());
     m_currentPoint++;
     return true;
 }
@@ -234,7 +233,7 @@ QuickInfo E57Reader::inspect()
         qi.m_dimNames.push_back(layout->dimName(*di));
     qi.m_pointCount = m_pointCount;
 
-    for (auto& scan: m_scans)
+    for (auto& scan : m_scans)
         qi.m_bounds.grow(scan->getBoundingBox());
 
     qi.m_valid = true;
@@ -252,10 +251,10 @@ int E57Reader::getScanIndex(pdal::point_count_t pointIndex) const
         return -1;
 
     pdal::point_count_t counter = 0;
-    for (pdal::point_count_t i=0; i < m_scans.size(); i++)
+    for (pdal::point_count_t i = 0; i < m_scans.size(); i++)
     {
         counter += m_scans[i]->getNumPoints();
-        if (pointIndex <counter)
+        if (pointIndex < counter)
             return i;
     }
     return -1;
@@ -269,16 +268,16 @@ std::set<std::string> E57Reader::getDimensions()
     // Extract smallest common denominator across all scans
     auto commonDimensions = m_scans[0]->getDimensions();
     std::vector<std::string> dimensionsToRemove;
-    for (auto &scan: m_scans)
+    for (auto& scan : m_scans)
     {
         auto newDims = scan->getDimensions();
-        for (auto &dim: commonDimensions)
+        for (auto& dim : commonDimensions)
         {
             if (newDims.find(dim) == newDims.end())
                 dimensionsToRemove.push_back(dim);
         }
     }
-    for (auto &dim: dimensionsToRemove)
+    for (auto& dim : dimensionsToRemove)
         commonDimensions.erase(dim);
 
     m_validDimensions = commonDimensions;
@@ -290,24 +289,29 @@ pdal::point_count_t E57Reader::getNumberPoints() const
     return m_pointCount;
 }
 
-void E57Reader::openFile(const std::string &filename)
+void E57Reader::openFile(const std::string& filename)
 {
     try
     {
-        m_imf = std::unique_ptr<e57::ImageFile>(new e57::ImageFile(filename,"r",e57::CHECKSUM_POLICY_SPARSE));
+        m_imf = std::unique_ptr<e57::ImageFile>(
+            new e57::ImageFile(filename, "r", e57::CHECKSUM_POLICY_SPARSE));
         if (!m_imf->isOpen())
             throwError("Failed opening the file : " + filename);
 
-        const e57::ustring normalsExtension("http://www.libe57.org/E57_NOR_surface_normals.txt");
+        const e57::ustring normalsExtension(
+            "http://www.libe57.org/E57_NOR_surface_normals.txt");
         e57::ustring _normalsExtension;
-        if (!m_imf->extensionsLookupPrefix("nor", _normalsExtension)) //the extension may already be registered
+        if (!m_imf->extensionsLookupPrefix(
+                "nor",
+                _normalsExtension)) // the extension may already be registered
             m_imf->extensionsAdd("nor", normalsExtension);
     }
-    catch(const e57::E57Exception& e)
+    catch (const e57::E57Exception& e)
     {
-        throwError("E57 error with code " + std::to_string(e.errorCode()) + " : " + e.context());
+        throwError("E57 error with code " + std::to_string(e.errorCode()) +
+                   " : " + e.context());
     }
-    catch(...)
+    catch (...)
     {
         std::string msg("Unknown error in E57 plugin");
         throwError(msg);
@@ -328,21 +332,22 @@ void E57Reader::setupReader(pdal::point_count_t pointNumber)
 
     pdal::point_count_t offset = 0;
     pdal::point_count_t maxRead = m_scans[0]->getNumPoints();
-    for (int i=1; i<=currentScan; i++)
+    for (int i = 1; i <= currentScan; i++)
     {
         maxRead = m_scans[i]->getNumPoints();
-        offset += m_scans[i-1]->getNumPoints();
+        offset += m_scans[i - 1]->getNumPoints();
     }
 
     m_chunk.reset();
     auto pointNode = m_scans[currentScan]->getPoints();
-    m_chunk = std::unique_ptr<ChunkReader>(new ChunkReader(offset,maxRead,m_scans[currentScan],getDimensions()));
+    m_chunk = std::unique_ptr<ChunkReader>(new ChunkReader(
+        offset, maxRead, m_scans[currentScan], getDimensions()));
 }
 
 pdal::point_count_t E57Reader::extractNumberPoints() const
 {
     pdal::point_count_t count = 0;
-    for (auto& scan: m_scans)
+    for (auto& scan : m_scans)
         count += scan->getNumPoints();
 
     return count;
@@ -354,18 +359,22 @@ void E57Reader::extractScans()
     if (!root.isDefined("/data3D"))
         throw std::invalid_argument("File does not contain valid 3D data");
 
-    //E57 standard: "data3D is a vector for storing an arbitrary number of 3D data sets "
+    // E57 standard: "data3D is a vector for storing an arbitrary number of 3D
+    // data sets "
     e57::VectorNode n(root.get("/data3D"));
     for (unsigned i = 0; i < n.childCount(); i++)
     {
-        try {
+        try
+        {
             e57::StructureNode scanNode(n.get(i));
             m_scans.emplace_back(new e57::Scan(scanNode));
         }
         catch (const std::exception& e)
         {
-            throwError(std::string("Failed to extract scans from the E57 file: ") + e.what());
+            throwError(
+                std::string("Failed to extract scans from the E57 file: ") +
+                e.what());
         }
     }
 }
-}
+} // namespace pdal
--- plugins/e57/io/Uuid.cpp
+++ plugins/e57/io/Uuid.cpp
@@ -1,41 +1,43 @@
 /******************************************************************************
-* Copyright (c) 2019, Helix Re Inc.
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following
-* conditions are met:
-*
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in
-*       the documentation and/or other materials provided
-*       with the distribution.
-*     * Neither the name of Helix Re Inc. nor the
-*       names of its contributors may be used to endorse or promote
-*       products derived from this software without specific prior
-*       written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-* OF SUCH DAMAGE.
-****************************************************************************/
+ * Copyright (c) 2019, Helix Re Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of Helix Re Inc. nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
 
 #include "Uuid.hpp"
 
-namespace pdal {
-    namespace uuidGenerator {
+namespace pdal
+{
+namespace uuidGenerator
+{
 unsigned char random_char()
 {
     std::random_device rd;
@@ -44,10 +46,11 @@ unsigned char random_char()
     return static_cast<unsigned char>(dis(gen));
 }
 
-        std::string generate_hex(const unsigned int len)
+std::string generate_hex(const unsigned int len)
 {
     std::stringstream ss;
-    for (unsigned int i = 0; i < len; i++) {
+    for (unsigned int i = 0; i < len; i++)
+    {
         auto rc = random_char();
         std::stringstream hexstream;
         hexstream << std::hex << int(rc);
@@ -59,8 +62,8 @@ unsigned char random_char()
 
 std::string generate_uuid()
 {
-    return generate_hex(4) + '-' + generate_hex(2) + "-" + generate_hex(2) + "-" + generate_hex(2) + "-" +
-           generate_hex(6);
+    return generate_hex(4) + '-' + generate_hex(2) + "-" + generate_hex(2) +
+           "-" + generate_hex(2) + "-" + generate_hex(6);
 }
-    }
+} // namespace uuidGenerator
 }
