8e3fa7834bca7d477dd43d4ede4d9a0be55f7921
Nicolas <nicolas.chaulet@gmail.com>
2019-07-31 16:04:44 +0100

    Reverting brken commit

--- cmake/gdal.cmake
+++ cmake/gdal.cmake
@@ -5,7 +5,10 @@ find_package(GDAL 2.2.0)
 set_package_properties(GDAL PROPERTIES TYPE REQUIRED
     PURPOSE "Provides general purpose raster, vector, and reference system support")
 if (GDAL_FOUND)
-    execute_process(COMMAND ${GDAL_CONFIG} "--version" OUTPUT_VARIABLE GDAL_VERSION)
+
+    if (NOT ${GDAL_VERSION})
+        execute_process(COMMAND ${GDAL_CONFIG} "--version" OUTPUT_VARIABLE GDAL_VERSION)
+    endif()
     string(COMPARE GREATER ${GDAL_VERSION} "3.0.0" GDAL_3)
     mark_as_advanced(CLEAR GDAL_INCLUDE_DIR)
     mark_as_advanced(CLEAR GDAL_LIBRARY)
