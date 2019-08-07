aec16d32546c2af14b414cc697e11c1c9f0085c9
Howard Butler <howard@hobu.co>
2019-08-07 11:37:47 -0500

    attempt to run gdal-config if we don't have a GDAL_VERSION

--- cmake/gdal.cmake
+++ cmake/gdal.cmake
@@ -5,6 +5,14 @@ find_package(GDAL 2.2.0)
 set_package_properties(GDAL PROPERTIES TYPE REQUIRED
     PURPOSE "Provides general purpose raster, vector, and reference system support")
 if (GDAL_FOUND)
+    # if we have GDAL_VERSION (as a version type, not a string)
+    # don't run gdal-config
+    if ( ${GDAL_VERSION})
+        # cmake is dumb. if we test NOT ${GDAL_CONFIG} it is true when GDAL_CONFIG
+        # is set to a value. I don't know why.
+    else()
+        execute_process(COMMAND ${GDAL_CONFIG} "--version" OUTPUT_VARIABLE GDAL_VERSION)
+    endif()
     string(COMPARE GREATER ${GDAL_VERSION} "3.0.0" GDAL_3)
     mark_as_advanced(CLEAR GDAL_INCLUDE_DIR)
     mark_as_advanced(CLEAR GDAL_LIBRARY)
