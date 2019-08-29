edf08479d34c990c05f4f87162c77d1e6fb83e37
Howard Butler <howard@hobu.co>
2019-08-29 08:11:57 -0500

    remove PDAL_PYTHON_LIBRARY printing

--- plugins/python/plang/Environment.cpp
+++ plugins/python/plang/Environment.cpp
@@ -68,8 +68,6 @@ static void loadPython()
     std::string libname;
 
     pdal::Utils::getenv("PDAL_PYTHON_LIBRARY", libname);
-    std::cout << "libname:" << libname << std::endl;
-    std::cout << "PDAL_PYTHON_LIBRARY:" << PDAL_PYTHON_LIBRARY << std::endl;
 
 // PDAL_PYTHON_LIBRARY below is the result of the cmake FindPython script's
 // PYTHON_LIBRARY.
