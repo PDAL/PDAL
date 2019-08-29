4147228cb12eab153718e2e2a6dfc46289c58ed7
Howard Butler <howard@hobu.co>
2019-08-29 07:26:38 -0500

    try to print PDAL_PYTHON_LIBRARY

--- plugins/python/plang/Environment.cpp
+++ plugins/python/plang/Environment.cpp
@@ -68,6 +68,7 @@ static void loadPython()
     std::string libname;
 
     pdal::Utils::getenv("PDAL_PYTHON_LIBRARY", libname);
+    std::cout << "PDAL_PYTHON_LIBRARY:" << libname << std::endl;
 
 // PDAL_PYTHON_LIBRARY below is the result of the cmake FindPython script's
 // PYTHON_LIBRARY.
