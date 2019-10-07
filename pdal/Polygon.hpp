79f7fb3a4b264cb1f6ba01e337a6f2ca7fb1a7b9
Andrew Bell <andrew.bell.ia@gmail.com>
2019-10-07 14:16:07 -0400

    Remove OGR code for EPT

--- pdal/Polygon.hpp
+++ pdal/Polygon.hpp
@@ -58,6 +58,7 @@ public:
 
     Polygon(const BOX2D&);
     Polygon(const BOX3D&);
+    Polygon(OGRGeometryH g);
     Polygon(OGRGeometryH g, const SpatialReference& srs);
 
     OGRGeometryH getOGRHandle();
