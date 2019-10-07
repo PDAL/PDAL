de7f6d5fe142c7ea1bd734e758d30db09035d45c
pravinshinde825 <pravin@helix.re>
2019-10-07 14:40:25 +0530

    Added leveldb as a third party anf voxeldownsize as a external plugin.

---
+++ cmake/leveldb.cmake
@@ -0,0 +1,15 @@
+include(FetchContent)
+FetchContent_Declare(
+    leveldb
+    GIT_REPOSITORY 	https://github.com/helix-re/leveldb.git
+	GIT_TAG 		helix-master
+)
+
+FetchContent_GetProperties(leveldb)
+if(NOT leveldb_POPULATED)
+    FetchContent_Populate(leveldb)
+    add_subdirectory(${leveldb_SOURCE_DIR} ${leveldb_BINARY_DIR})
+    set(leveldb_INCLUDE "${leveldb_SOURCE_DIR}/include")
+    set_property(TARGET leveldb PROPERTY FOLDER "Third/leveldb")
+    set_property(TARGET leveldbutil PROPERTY FOLDER "Third/leveldb")
+endif()
