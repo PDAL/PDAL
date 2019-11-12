9cd6476313ea74e41a7bef7406ecaf1893aaeeb1
Nicolas <nicolas.chaulet@gmail.com>
2019-11-12 16:56:12 +0000

    macos compatibility

--- plugins/e57/io/E57Writer.cpp
+++ plugins/e57/io/E57Writer.cpp
@@ -98,7 +98,7 @@ void E57Writer::ChunkWriter::write(pdal::PointRef& pt)
 
             if (pdaldim == DimId::Intensity && val > m_intensityLimit)
             {
-                m_intensityLimit = max(val, m_intensityLimit);
+                m_intensityLimit = fmax(val, m_intensityLimit);
             }
             keyValue.second[m_currentIndex] = val;
         }
