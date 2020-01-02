9e743155d6b809356aa8942e247d60622a0c69da
Norman Barker <norman@tiledb.io>
2020-01-02 15:06:26 -0600

    added json prop check if metadata is an empty array

--- plugins/tiledb/io/TileDBReader.cpp
+++ plugins/tiledb/io/TileDBReader.cpp
@@ -322,7 +322,9 @@ void TileDBReader::localReady()
         }
     }
 
-    if ((meta != nullptr) && (meta["writers.tiledb"].count("spatialreference") > 0))
+    if ((meta != nullptr) &&
+        (meta.count("writers.tiledb") > 0) &&
+        (meta["writers.tiledb"].count("spatialreference") > 0))
     {
         SpatialReference ref(meta["writers.tiledb"]["spatialreference"]);
         setSpatialReference(ref);
