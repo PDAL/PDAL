3b18d33d5f60bcda04272181cce6730e33973801
Howard Butler <howard@hobu.co>
2020-02-08 10:24:48 -0600

    Undo edits to my build script

--- scripts/conda/win64.bat
+++ scripts/conda/win64.bat
@@ -1,29 +1,18 @@
 set CONDA_ENVIRO=pdal-build
 
-REM del /s /q  %CONDA_ENVIRO%
-mkdir %CONDA_ENVIRO%
-pushd %CONDA_ENVIRO%
-
 call conda activate %CONDA_ENVIRO%
 
 cd %CONDA_ENVIRO%
 
 echo "conda prefix" %CONDA_PREFIX%
-set GENERATOR="Visual Studio 14 2015 Win64"
-REM set GENERATOR="NMake Makefiles"
+REM set GENERATOR="Visual Studio 14 2015 Win64"
+set GENERATOR="NMake Makefiles"
 set GENERATOR="Ninja"
 
 set ORACLE_HOME=%CONDA_PREFIX%
 cmake -G %GENERATOR% ^
       -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo ^
-      -DCMAKE_LIBRARY_PATH:FILEPATH="%CONDA_PREFIX%/Library/lib" ^
-      -DCMAKE_INCLUDE_PATH:FILEPATH="%CONDA_PREFIX%/Library/include" ^
-      -DLIBLZMA_LIBRARY:FILEPATH=%CONDA_PREFIX%\Library\lib\liblzma.lib ^
-      -DZSTD_LIBRARY:FILEPATH=%CONDA_PREFIX%\Library\lib\libzstd.lib ^
-      -DCMAKE_INSTALL_PREFIX:FILEPATH="%CONDA_PREFIX%/" ^
-	  -DPython3_ROOT_DIR:FILEPATH="%CONDA_PREFIX%" ^
-      -DPython3_FIND_STRATEGY=LOCATION ^
-      -DPYTHON_EXECUTABLE="%CONDA_PREFIX%\python" ^
+      -DCMAKE_LIBRARY_PATH:FILEPATH="=%CONDA_PREFIX%/Library/lib" ^
       -DBUILD_PLUGIN_PYTHON=ON ^
 	  -DPython3_ROOT_DIR:FILEPATH="%CONDA_PREFIX%" ^
 	  -DPython3_FIND_STRATEGY=LOCATION ^
@@ -32,7 +21,7 @@ cmake -G %GENERATOR% ^
       -DBUILD_PLUGIN_I3S=ON ^
 	  -DBUILD_PLUGIN_E57=ON ^
 	  -DBUILD_PLUGIN_TILEDB=ON ^
-      -DBUILD_PLUGIN_FBX=OFF ^
+      -DBUILD_PLUGIN_FBX=ON ^
       -DBUILD_PLUGIN_ICEBRIDGE=ON ^
       -DBUILD_PLUGIN_NITF=ON ^
       -DBUILD_PLUGIN_TILEDB=ON ^
@@ -50,7 +39,7 @@ cmake -G %GENERATOR% ^
       -DWITH_LZMA=ON ^
       -DLIBLZMA_LIBRARY:FILEPATH=%CONDA_PREFIX%\Library\lib\liblzma.lib ^
       -DZSTD_LIBRARY:FILEPATH=%CONDA_PREFIX%\Library\lib\libzstd.lib ^
-	  -DBUILD_PLUGIN_RDBLIB=OFF ^
+	  -DBUILD_PLUGIN_RDBLIB=ON ^
 	  -DRdb_ROOT=..\..\..\..\rdblib-2.2.1-x86_64-windows\interface\cpp ^
       .. --debug-trycompile
 
