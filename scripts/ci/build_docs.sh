ff3e3a2c7efe30481ca593515e61a0a0a348d024
Howard Butler <howard@hobu.co>
2020-02-11 10:43:21 -0600

    WIP Sphinx RTD theme

--- scripts/ci/build_docs.sh
+++ scripts/ci/build_docs.sh
@@ -2,8 +2,8 @@
 
 TRAVIS_BUILD_DIR=$1
 echo "building docs for $TRAVIS_BUILD_DIR/docs"
-docker run -v $TRAVIS_BUILD_DIR:/data -w /data/doc pdal/docs make doxygen
-docker run -v $TRAVIS_BUILD_DIR:/data -w /data/doc pdal/docs make html
-docker run -v $TRAVIS_BUILD_DIR:/data -w /data/doc pdal/docs make latexpdf
+#docker run -v $TRAVIS_BUILD_DIR:/data -w /data/doc pdal/docs make doxygen
+docker run -v $TRAVIS_BUILD_DIR:/data -w /data/doc osgeo/proj-docs make html
+#docker run -v $TRAVIS_BUILD_DIR:/data -w /data/doc pdal/docs make latexpdf
 
 
