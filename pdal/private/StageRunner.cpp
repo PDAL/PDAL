7286ffe09993d41ac89dd3db8aa6483a4f7aaea9
Andrew Bell <andrew.bell.ia@gmail.com>
2020-07-21 06:45:57 -0400

    Add missed file.

---
+++ pdal/private/StageRunner.cpp
@@ -0,0 +1,85 @@
+/******************************************************************************
+* Copyright (c) 2014, Hobu Inc.
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following
+* conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in
+*       the documentation and/or other materials provided
+*       with the distribution.
+*     * Neither the name of Hobu, Inc. or Flaxen Geo Consulting nor the
+*       names of its contributors may be used to endorse or promote
+*       products derived from this software without specific prior
+*       written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+* OF SUCH DAMAGE.
+****************************************************************************/
+
+#include "StageRunner.hpp"
+
+#include <pdal/Stage.hpp>
+
+namespace pdal
+{
+
+StageRunner::StageRunner(Stage *s, PointViewPtr keeps, PointViewPtr skips) :
+    m_stage(s), m_keep(keeps), m_skip(skips)
+{}
+
+// For now this is all synchronous
+void StageRunner::run()
+{
+    point_count_t keepSize = m_keep->size();
+    int keepId = m_keep->id();
+    m_viewSet = m_stage->run(m_keep);
+    if (m_skip && m_skip->size())
+    {
+        if (m_stage->mergeMode() == Stage::WhereMergeMode::True)
+        {
+            if (m_viewSet.size())
+            {
+                (*m_viewSet.begin())->append(*m_skip);
+                return;
+            }
+
+        }
+        else if (m_stage->mergeMode() == Stage::WhereMergeMode::Auto)
+        {
+            if (m_viewSet.size() == 1)
+            {
+                PointViewPtr keep = *m_viewSet.begin();
+                if (keep->id() == keepId && keepSize == keep->size())
+                {
+                    keep->append(*m_skip);
+                    return;
+                }
+            }
+        }
+        m_viewSet.insert(m_skip);
+    }
+}
+
+PointViewSet StageRunner::wait()
+{
+    return m_viewSet;
+}
+
+} // namespace pdal
+
