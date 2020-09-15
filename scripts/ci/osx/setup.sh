cecb004418de00082464142c29a54d03264bd0c2
Howard Butler <howard@hobu.co>
2020-09-15 16:02:08 -0500

    turn off abseil on win/osx

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -9,7 +9,7 @@ conda install cmake ninja conda-build compilers -y
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
-    conda install --yes --quiet gdal=3.0.4=py38h9f7df5a_9 python=3.8 abseil-cpp  -y
+    conda install --yes --quiet gdal=3.0.4=py38h9f7df5a_9 python=3.8  -y
     conda install --yes --quiet pdal  --only-deps -y
 
 else
