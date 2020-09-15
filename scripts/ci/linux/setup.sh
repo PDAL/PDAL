8b5d8c5bf9578c80ed5dcda79ba3023f9a22468b
Howard Butler <howard@hobu.co>
2020-09-15 10:25:45 -0500

    quit failing at logic

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -10,14 +10,13 @@ echo "Configuring build type '$BUILD_TYPE'"
 
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
-    conda install pdal --only-deps -y
-
-else
-
     conda config --set channel_priority strict
     conda install --yes --quiet -c conda-forge gdal=3.0.2=py37hbb6b9fb_8 python=3.7 abseil-cpp  -y
     conda install --yes --quiet -c conda-forge pdal  --only-deps -y
 
+else
+
+    conda install pdal --only-deps -y
 
 fi
 
