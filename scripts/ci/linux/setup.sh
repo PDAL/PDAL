35ea69a1a395efcba3b200d34b4414f5124b73ca
Howard Butler <howard@hobu.co>
2020-09-15 09:57:38 -0500

    gdal-config lint

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 mkdir build
+
+conda config --set channel_priority strict
 conda update -n base -c defaults conda
+conda install cmake ninja conda-build compilers -y
+
+echo "Configuring build type '$BUILD_TYPE'"
 
 if [ "$BUILD_TYPE" == "fixed" ]; then
-    echo "Configuring build type '$BUILD_TYPE'"
 
     conda install pdal --only-deps -y
-    conda install cmake ninja compilers -y
+
 else
-    conda install --yes gdal=3.0.2=py37hbb6b9fb_8 python=3.7 abseil-cpp conda-build ninja
-    echo "Configuring build type '$BUILD_TYPE'"
+
+    conda install conda-forge gdal=3.0.2=py37hbb6b9fb_8 python=3.7 abseil-cpp -y
+    conda install pdal --only-deps -y
+
 fi
+
+gdal-config --version
 
