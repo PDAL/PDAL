c62767b41ee574ab9e68f511ffe563e01b0fdb5e
Howard Butler <howard@hobu.co>
2020-09-15 11:43:03 -0500

    cleanups

--- scripts/ci/linux/cmake.sh
+++ scripts/ci/linux/cmake.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cmake .. \
+LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib" cmake .. \
       -G Ninja \
+      -DCMAKE_LIBRARY_PATH:FILEPATH="$CONDA_PREFIX/lib" \
+      -DCMAKE_INCLUDE_PATH:FILEPATH="$CONDA_PREFIX/include" \
       -DCMAKE_BUILD_TYPE=Debug \
-      -DCMAKE_INSTALL_PREFIX=`pwd`/../install \
+      -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
       -DBUILD_PLUGIN_I3S=ON \
       -DBUILD_PLUGIN_NITF=ON \
       -DBUILD_PLUGIN_TILEDB=ON \
--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-#conda config --set channel_priority strict
 conda update -n base -c defaults conda
 conda install cmake ninja conda-build compilers -y
-
-echo "Configuring build type '$BUILD_TYPE'"
 
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
-    conda install --yes --quiet -c conda-forge gdal=3.0.2=py37hbb6b9fb_8 python=3.7 abseil-cpp  -y
-    conda install --yes --quiet -c conda-forge pdal  --only-deps -y
+    conda install --yes --quiet gdal=3.0.2=py37hbb6b9fb_8 python=3.7 abseil-cpp  -y
+    conda install --yes --quiet pdal  --only-deps -y
 
 else
 
