ec1fcab5f395df4b323a580a670480bc39403ee3
Howard Butler <howard@hobu.co>
2020-09-15 14:54:00 -0500

    fixed/float build matrix for OSX and Windows

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-conda update -n base -c defaults conda
-conda install pdal --only-deps -y
-conda install cmake ninja compilers -y
+echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
+
+conda update -n base -c defaults conda
+conda install cmake ninja conda-build compilers -y
+
+if [ "$BUILD_TYPE" == "fixed" ]; then
+
+    conda config --set channel_priority strict
+    conda install --yes --quiet gdal=3.0.4=py38h9f7df5a_9 python=3.8 abseil-cpp  -y
+    conda install --yes --quiet pdal  --only-deps -y
+
+else
+
+    conda install pdal --only-deps -y
+
+fi
+
+gdal-config --version
+
