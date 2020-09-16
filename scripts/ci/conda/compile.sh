38120196ceaf8e40e1f5314abf666fc0990b4183
Howard Butler <howard@hobu.co>
2020-09-16 14:17:52 -0500

    use local packages directory for install

--- scripts/ci/conda/compile.sh
+++ scripts/ci/conda/compile.sh
@@ -3,6 +3,6 @@
 mkdir packages
 
 conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages
-conda install pdal -c packages
+conda install -c ./packages pdal
 
 pdal --version
