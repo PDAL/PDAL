cfa67458d6baf88eb6e472bd4e9bb71c5d79f573
Norman Barker <norman@tiledb.io>
2020-10-15 18:23:38 -0500

    Default to unordered reads with TileDB plugin

--- plugins/tiledb/io/TileDBReader.cpp
+++ plugins/tiledb/io/TileDBReader.cpp
@@ -252,6 +252,7 @@ void TileDBReader::localReady()
     int numDims = m_array->schema().domain().dimensions().size();
 
     m_query.reset(new tiledb::Query(*m_ctx, *m_array));
+    m_query->set_layout( TILEDB_UNORDERED );
 
     // Build the buffer for the dimensions.
     auto it = std::find_if(m_dims.begin(), m_dims.end(),
