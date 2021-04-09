ea0dc4b09391faf85b2b64b054ef9b4691d07c2c
Andrew Bell <andrew.bell.ia@gmail.com>
2021-04-09 19:28:44 -0400

    If there are no extended VLRs, set the offset to 0 (#3407)
    * Don't try to test ext VLR position for zipped file.
    Close #3348
    
    * Set the VLR offsets to 0 if there are no offsets.
    
    * VLR Offset is really header size.

--- io/LasWriter.cpp
+++ io/LasWriter.cpp
@@ -1168,7 +1168,7 @@ void LasWriter::finishOutput()
     OLeStream out(m_ostream);
 
     // addVlr prevents any eVlrs from being added before version 1.4.
-    m_lasHeader.setEVlrOffset((uint32_t)m_ostream->tellp());
+    m_lasHeader.setEVlrOffset(m_eVlrs.size() ? (uint32_t)m_ostream->tellp() : 0);
     for (auto vi = m_eVlrs.begin(); vi != m_eVlrs.end(); ++vi)
     {
         ExtLasVLR evlr = *vi;
