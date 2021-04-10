035dfd8c4b5ce505008eeea35aac17638cea7e3d
Howard Butler <howard@hobu.co>
2021-04-10 10:10:23 -0500

    remove WITH_DRACO

--- cmake/draco.cmake
+++ cmake/draco.cmake
@@ -1,13 +1,6 @@
 #
-option(WITH_DRACO "Choose if Draco support should be built" FALSE)
-if (WITH_DRACO)
-    # the `pkg_check_modules` function is created with this call
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(DRACO REQUIRED draco>1.4.0)
+# the `pkg_check_modules` function is created with this call
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(DRACO REQUIRED draco>1.4.0)
 
-    set_package_properties(Draco PROPERTIES TYPE RECOMMENDED
-        PURPOSE "Provides Draco compression")
-    set(PDAL_HAVE_DRACO 1)
-else()
-    set(WITH_DRACO FALSE)
-endif()
+set(PDAL_HAVE_DRACO 1)
