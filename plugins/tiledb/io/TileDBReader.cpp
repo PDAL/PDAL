0b4e20c1635754d3b858d01aabcac4ab2f89e113
normanb <norman@tiledb.com>
2021-07-24 09:40:45 -0500

    checked for tiledb >= 2.3

--- plugins/tiledb/io/TileDBReader.cpp
+++ plugins/tiledb/io/TileDBReader.cpp
@@ -108,7 +108,7 @@ void TileDBReader::addArgs(ProgramArgs& args)
     args.add("end_timestamp", "TileDB array timestamp", m_endTimeStamp,
         point_count_t(0));
     args.addSynonym("end_timestamp", "timestamp");
-#if TILEDB_VERSION_MAJOR > 1 || TILEDB_VERSION_MINOR >= 3
+#if TILEDB_VERSION_MAJOR > 2 || (TILEDB_VERSION_MAJOR == 2 && TILEDB_VERSION_MINOR >= 3)
     args.add("start_timestamp", "TileDB array timestamp", m_startTimeStamp,
         point_count_t(0));
 #endif
@@ -139,7 +139,7 @@ void TileDBReader::initialize()
 
         if (m_endTimeStamp)
         {
-#if TILEDB_VERSION_MAJOR > 1 && TILEDB_VERSION_MINOR >= 3
+#if TILEDB_VERSION_MAJOR > 2 || (TILEDB_VERSION_MAJOR == 2 && TILEDB_VERSION_MINOR >= 3)
             if (m_startTimeStamp)
             {
                 m_array.reset(new tiledb::Array(*m_ctx, m_filename, TILEDB_READ));
