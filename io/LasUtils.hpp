c09a968ebf8c870c9086fc7c33d3f6bb1dd7a355
Andrew Bell <andrew.bell.ia@gmail.com>
2021-10-12 14:54:35 -0400

    Use IdList typedef.

--- io/LasUtils.hpp
+++ io/LasUtils.hpp
@@ -185,7 +185,7 @@ struct IgnoreVLR
     uint16_t m_recordId;
 };
 std::vector<IgnoreVLR> parseIgnoreVLRs(const StringList& ignored);
-const std::vector<Dimension::Id>& pdrfDims(int pdrf);
+const Dimension::IdList& pdrfDims(int pdrf);
 
 } // namespace LasUtils
 
