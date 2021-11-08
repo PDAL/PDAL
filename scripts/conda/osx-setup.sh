7d1be56e603354644265d016408d8778425fb034
Andrew Bell <andrew.bell.ia@gmail.com>
2021-11-08 13:15:09 -0500

    Ci churn (#3583)
    * do not install draco anymore
    
    * force python 3.9 for now
    
    * add back draco
    
    * fix pgpointcloud target
    
    * update my build config
    
    * nuke any patches
    
    Co-authored-by: Howard Butler <hobu.inc@gmail.com>

--- scripts/conda/osx-setup.sh
+++ scripts/conda/osx-setup.sh
@@ -1,31 +1,6 @@
-export CONDA_EXE=/Users/hobu/miniconda3/bin/conda
 
-CONDA_ENV="pdal-build"
-
-$CONDA_EXE env remove -n $CONDA_ENV -y
-$CONDA_EXE config --add channels conda-forge
-$CONDA_EXE create --name $CONDA_ENV -y
-source /Users/hobu/miniconda3/bin/activate $CONDA_ENV
-$CONDA_EXE install  -y laz-perf \
-                laszip \
-                libunwind \
-                geotiff \
-                sqlite \
-                draco \
-                libxml2 \
-                nitro \
-                curl \
-                gdal \
-                postgresql \
-                hdf5 \
-                cmake \
-                compilers \
-                libspatialite \
-                eigen \
-                ninja \
-                libgdal \
-                zstd \
-                numpy \
-                tiledb
-
-activate $CONDA_ENV
+conda install -c conda-forge pdal --only-deps
+conda install -c conda-forge compilers \
+    pkg-config \
+    ninja \
+    cmake
