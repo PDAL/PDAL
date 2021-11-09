76ca423953b0aa6fe2b1694ece614eebf5b9e3a1
Howard Butler <hobu.inc@gmail.com>
2021-11-09 14:36:28 -0600

    temporary install of laz-perf so CI works

--- scripts/ci/win/cmake.sh
+++ scripts/ci/win/cmake.sh
@@ -4,6 +4,13 @@ pwd
 where cl.exe
 export CC=cl.exe
 export CXX=cl.exe
+
+git clone https://github.com/hobu/laz-perf.git laz-perf && cd laz-perf && \
+    mkdir build && cd build && \
+    cmake -G Ninja .. -DWITH_TESTS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo  -Dgtest_force_shared_crt=ON -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX/Library && \
+    ninja install && \
+    cd ../..
+
 cmake .. -G "Ninja" \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" \
