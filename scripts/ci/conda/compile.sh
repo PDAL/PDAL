e0ecebeb63dcb7b6506dff02b16f01508dae846e
Howard Butler <howard@hobu.co>
2021-12-10 12:59:03 -0600

    conda builds need different conda-build support (#3630)

--- scripts/ci/conda/compile.sh
+++ scripts/ci/conda/compile.sh
@@ -15,7 +15,7 @@ if grep -q "macos" <<< "$PDAL_PLATFORM"; then
     CI_PLAT="osx"
 fi
 
-conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_.yaml"
+conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_openssl1.1.1.yaml"
 conda create -y -n test -c ./packages python=3.8 pdal
 conda deactivate
 
