b3d2c8954b16ba61995e2b1dba456ff74e82268a
Howard Butler <hobu.inc@gmail.com>
2022-01-03 13:36:28 -0600

    remove non-standard <filesystem> stuff

--- pdal/util/FileUtils.cpp
+++ pdal/util/FileUtils.cpp
@@ -46,20 +46,8 @@
 #include <codecvt>
 #endif
 
-#ifndef __has_include
-  static_assert(false, "__has_include not supported");
-#else
-#  if __has_include(<filesystem>)
-#    include <filesystem>
-     namespace fs = std::filesystem;
-#  elif __has_include(<experimental/filesystem>)
-#    include <experimental/filesystem>
-     namespace fs = std::experimental::filesystem;
-#  elif __has_include(<boost/filesystem.hpp>)
-#    include <boost/filesystem.hpp>
-     namespace fs = boost::filesystem;
-#  endif
-#endif
+#include <filesystem>
+namespace fs = std::filesystem;
 
 
 
