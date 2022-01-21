fdc9f4c9e20fe855f9a821406b532d64244b6336
Howard Butler <hobu.inc@gmail.com>
2022-01-21 11:19:56 -0600

    Fix #3621. PDAL does not support building as a static library at this time and should not advertise that it does

--- scripts/conda/osx.sh
+++ scripts/conda/osx.sh
@@ -1,8 +1,7 @@
 
 BUILDDIR=conda-build
 
-#CONFIG="Unix Makefiles"
-CONFIG="Ninja"
+CONFIG="Debug"
 
 if ! [ -z "$1" ]; then
     CONFIG="$1"
@@ -12,11 +11,11 @@ fi
 rm -rf $BUILDDIR
 mkdir -p $BUILDDIR
 cd $BUILDDIR
-CFLAGS= CXXFLAGS= CC=/usr/bin/cc CXX=/usr/bin/c++ cmake   -G "$CONFIG"  \
+CFLAGS= CXXFLAGS= CC=/usr/bin/cc CXX=/usr/bin/c++ cmake   -G "Ninja"  \
         -DCMAKE_LIBRARY_PATH:FILEPATH="$CONDA_PREFIX/lib" \
         -DCMAKE_INCLUDE_PATH:FILEPATH="$CONDA_PREFIX/include" \
         -DCMAKE_FIND_FRAMEWORK="NEVER" \
-        -DCMAKE_BUILD_TYPE=Debug \
+        -DCMAKE_BUILD_TYPE=$CONFIG \
         -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
         -DBUILD_PLUGIN_PGPOINTCLOUD=ON \
         -DBUILD_PLUGIN_NITF=ON \
