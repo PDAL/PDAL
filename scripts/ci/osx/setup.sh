b812d8713fbecde043b637a4d5cd679eb1380333
Howard Butler <hobu.inc@gmail.com>
2022-03-16 15:39:22 -0500

    update 'fixed' versions

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -3,13 +3,13 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-conda install -c conda-forge python=3.9 pkg-config cmake ninja pkgconfig -y
+conda install -c conda-forge pkg-config cmake ninja pkgconfig -y
 
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
     conda install --yes draco -y
-    conda install --yes --quiet gdal=3.2.2=py38h140b4a6_2 python=3.8  -y
+    conda install --yes --quiet gdal=3.4.1=py310h888c299_5 python=3.10  -y
     conda install --yes --quiet pdal  --only-deps -y
 
 else
