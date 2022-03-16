45acf03185fd36e8c7e7ccbb102d12c3616383b6
Howard Butler <hobu.inc@gmail.com>
2022-03-16 10:33:08 -0500

    need draco for fixed builder still

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -8,6 +8,7 @@ conda install -c conda-forge python=3.9 pkg-config cmake ninja pkgconfig -y
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
     conda config --set channel_priority strict
+    conda install --yes draco -y
     conda install --yes --quiet gdal=3.2.2=py38h140b4a6_2 python=3.8  -y
     conda install --yes --quiet pdal  --only-deps -y
 
