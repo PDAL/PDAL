f4c7dfb130676420ac01fc866176b7f7b3880580
Howard Butler <hobu.inc@gmail.com>
2022-06-24 12:50:42 -0500

    GDAL 64bit integer support (#3802)
    * support GDAL's 64bit integer types if they are defined
    
    * a better fix for the new GDAL int64 types
    
    * condition based on GDAL version

--- pdal/private/gdal/Raster.cpp
+++ pdal/private/gdal/Raster.cpp
@@ -94,11 +94,9 @@ Dimension::Type toPdalType(GDALDataType t)
         return Dimension::Type::Float;
     case GDT_Float64:
         return Dimension::Type::Double;
-#ifdef GDT_UInt64
+#if GDAL_VERSION_NUM >= GDAL_COMPUTE_VERSION(3,5,0)
     case GDT_UInt64:
         return Dimension::Type::Unsigned64;
-#endif
-#ifdef GDT_Int64
     case GDT_Int64:
         return Dimension::Type::Signed64;
 #endif
@@ -111,6 +109,9 @@ Dimension::Type toPdalType(GDALDataType t)
         throw pdal_error("GDAL unknown type unsupported.");
     case GDT_TypeCount:
         throw pdal_error("Detected bad GDAL data type.");
+    default:
+        throw pdal_error("Detected bad GDAL data type.");
+
     }
     return Dimension::Type::None;
 }
