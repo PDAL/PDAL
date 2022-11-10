80b97bb2d247ae0a1aa8766cec54da7e47af5b94
Howard Butler <hobu.inc@gmail.com>
2022-11-10 08:06:43 -0600

    pin python 3.10 on FIXED build for osx

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -3,18 +3,18 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-conda install -c conda-forge pkg-config cmake ninja pkgconfig -y
+mamba install -c conda-forge pkg-config cmake ninja pkgconfig -y
 
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
-    conda config --set channel_priority strict
-    conda install --yes draco -y
-    conda install --yes --quiet gdal=3.4.1=py310h2eb646b_6
-    conda install --yes --quiet pdal  --only-deps -y
+    mamba config --set channel_priority strict
+    mamba install --yes draco -y
+    mamba install --yes --quiet gdal=3.4.1=py310h2eb646b_6 python=3.10
+    mamba install --yes --quiet pdal  --only-deps -y
 
 else
-    conda install --yes --quiet gdal=3.4.1=py310h2eb646b_6
-    conda install pdal  --only-deps -y
+    mamba install --yes --quiet
+    mamba install pdal  --only-deps -y
 
 fi
 
