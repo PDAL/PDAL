cce4f0f8d9bd1abde1054a6c3e23e4fc84886eeb
Kyle Mann <39525199+kylemann16@users.noreply.github.com>
2023-01-22 11:40:19 -0600

    Fixed bounds comparisons.  (#3953)
    * Fixed bounds comparisons. If SRS is passed with bounds to args, reproject to 4326 for comparisons with geometry from STAC item. Adjusted test to reflect changes.
    
    * Adjusted tests. Moved item colletion to local and pared down item collection test to have fewer items.

--- test/unit/io/StacReaderTest.cpp
+++ test/unit/io/StacReaderTest.cpp
@@ -56,7 +56,7 @@ TEST(StacReaderTest, item_collection_test)
 
     options.add("filename", Support::datapath("stac/test_item_collection.json"));
     options.add("asset_names", "ept.json");
-    options.add("item_ids", "AK_NorthSlope_\\w{0,}");
+    options.add("item_ids", "AK_GlacierBay_\\w{0,}");
 
     StageFactory f;
     Stage& reader = *f.createStage("readers.stac");
@@ -68,22 +68,11 @@ TEST(StacReaderTest, item_collection_test)
     EXPECT_TRUE(jsonMetadata.contains("stac_ids"));
     std::vector<std::string> idList = jsonMetadata["stac_ids"].get<std::vector<std::string>>();
 
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B1_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B2_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B3_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B4_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B5_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B6_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B7_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B8_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B9_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B10_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B11_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B12_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B13_2018") != idList.end());
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_NorthSlope_B14_2018") != idList.end());
+    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_GlacierBay_B3_2019") != idList.end());
+    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "AK_GlacierBay_4_2019") != idList.end());
+    EXPECT_EQ(idList.size(), 2);
 
-    EXPECT_EQ(qi.m_pointCount, 9050847242);
+    EXPECT_EQ(qi.m_pointCount, 33904747117);
 }
 
 TEST(StacReaderTest, remote_item_test)
@@ -261,9 +250,9 @@ TEST(StacReaderTest, date_prune_reject_test)
 TEST(StacReaderTest, bounds_prune_accept_test)
 {
     Options options;
-    std::string bounds = "([-79.0,-74.0],[38.0,39.0])";
+    std::string bounds = "([-10190065.06156413, -10189065.06156413], [5109498.61041016, 5110498.61041016]) / EPSG:3857";
 
-    options.add("filename", Support::datapath("stac/MD_GoldenBeach_2012.json"));
+    options.add("filename", Support::datapath("stac/test_item_collection.json"));
     options.add("asset_names", "ept.json");
     options.add("bounds", bounds);
 
@@ -276,8 +265,10 @@ TEST(StacReaderTest, bounds_prune_accept_test)
     NL::json jsonMetadata = NL::json::parse(Utils::toJSON(qi.m_metadata));
     EXPECT_TRUE(jsonMetadata.contains("stac_ids"));
     std::vector<std::string> idList = jsonMetadata["stac_ids"].get<std::vector<std::string>>();
-    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "MD_GoldenBeach_2012") != idList.end());
-    EXPECT_EQ(qi.m_pointCount, 4860658);
+    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "IA_Eastern_1_2019") != idList.end());
+    EXPECT_TRUE(std::find(idList.begin(), idList.end(), "IA_FullState") != idList.end());
+    EXPECT_EQ(idList.size(), 2);
+    EXPECT_EQ(qi.m_pointCount, 322323007151);
 
 }
 
@@ -293,7 +284,6 @@ TEST(StacReaderTest, bounds_prune_reject_test)
     StageFactory f;
     Stage& reader = *f.createStage("readers.stac");
     reader.setOptions(options);
-
     EXPECT_THROW(QuickInfo qi = reader.preview(), pdal_error);
 }
 
