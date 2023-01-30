25796cb718db7f644c807c57bf728c052183f1a7
Howard Butler <hobu.inc@gmail.com>
2023-01-30 11:38:47 -0600

    openssl selector is now gone

--- scripts/ci/conda/compile.sh
+++ scripts/ci/conda/compile.sh
@@ -15,7 +15,7 @@ if grep -q "macos" <<< "$PDAL_PLATFORM"; then
     CI_PLAT="osx"
 fi
 
-conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_openssl1.1.1.yaml"
+conda build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_.yaml"
 conda create -y -n test -c ./packages/$CI_PLAT-64 python pdal
 conda deactivate
 
