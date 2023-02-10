ac1b35994224d2471feb4d8065833d192431350c
Howard Butler <hobu.inc@gmail.com>
2023-02-10 16:01:15 -0600

    add ceres and enable PLUGIN_TRAJECTORY

--- scripts/ci/win/setup.sh
+++ scripts/ci/win/setup.sh
@@ -3,21 +3,23 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-conda update -n base -c defaults conda
-conda install -c conda-forge pkg-config cmake ninja pkgconfig -y
+mamba update -n base -c defaults conda
+mamba install -c conda-forge pkg-config cmake ninja pkgconfig -y
 
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
-    conda config --set channel_priority strict
-    conda install -c conda-forge draco -y
-    conda install --yes --quiet gdal=3.5.3  python=3.11 -y
-    conda install --yes --quiet pdal  --only-deps -y
+    mamba config --set channel_priority strict
+    mamba install -c conda-forge draco -y
+    mamba install --yes --quiet gdal=3.5.3  python=3.11 -y
+    mamba install --yes --quiet pdal  --only-deps -y
 
 else
 
-    conda install pdal --only-deps -y
+    mamba install pdal --only-deps -y
 
 fi
+
+mamba install --yes ceres-solver mkl
 
 gdalinfo --version
 
