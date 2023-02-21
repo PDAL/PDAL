241f662e96df59b9661befb363145f2e7f659dbe
Howard Butler <hobu.inc@gmail.com>
2023-02-21 15:36:54 -0600

    CI Simplification (#3982)
    * disable draco builds
    
    * require 1.5.6 draco version, which has proper cmake config
    
    * not getting correct packages
    
    * use-only-tar-bz2: false
    
    * use environment file for configuring conda setup
    
    * environment.yaml syntax
    
    * update osx and win
    
    * need activate-environment
    
    * nits
    
    * try enabling ccache for linux
    
    * syntax

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -3,20 +3,8 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-mamba update -n base -c defaults conda
-mamba install -c conda-forge abseil-cpp re2 pkg-config cmake ninja compilers -y
-
 if [ "$BUILD_TYPE" == "fixed" ]; then
-
-    mamba config --set channel_priority strict
-    mamba install --yes draco
     mamba install --yes --quiet gdal=3.5.3 python=3.11 abseil-cpp  -y
-    mamba install --yes --quiet pdal  --only-deps -y
-
-else
-
-    mamba install pdal --only-deps -y
-
 fi
 
 gdal-config --version
