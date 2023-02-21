fca5d49577875c96cde7d312bfeeb730ac79ffd6
Howard Butler <hobu.inc@gmail.com>
2023-02-21 13:50:55 -0600

    nits

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -3,19 +3,8 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-mamba update -n base -c defaults conda
-mamba install -c conda-forge abseil-cpp re2  cmake ninja compilers -y
-
 if [ "$BUILD_TYPE" == "fixed" ]; then
-
-    mamba config --set channel_priority strict
     mamba install --yes --quiet gdal=3.5.3 python=3.11 abseil-cpp  -y
-    mamba install --yes --quiet pdal  --only-deps -y
-
-else
-
-    mamba install pdal --only-deps -y
-
 fi
 
 gdal-config --version
