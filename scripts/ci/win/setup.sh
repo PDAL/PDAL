cc9eeb603656f1ebf933523a2f34aff1766025df
Howard Butler <hobu.inc@gmail.com>
2023-02-21 11:51:06 -0600

    update osx and win

--- scripts/ci/win/setup.sh
+++ scripts/ci/win/setup.sh
@@ -3,22 +3,11 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-mamba update -n base -c defaults conda
-mamba install -c conda-forge config cmake ninja -y
-
 if [ "$BUILD_TYPE" == "fixed" ]; then
 
-    mamba config --set channel_priority strict
     mamba install --yes --quiet gdal=3.5.3  python=3.11 -y
-    mamba install --yes --quiet pdal  --only-deps -y
-
-else
-
-    mamba install pdal --only-deps -y
 
 fi
-
-mamba install --yes ceres-solver mkl blas
 
 gdalinfo --version
 
