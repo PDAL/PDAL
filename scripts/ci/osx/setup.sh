470c74926f9e148800606acb1ca7d1fcc53e9cf4
Howard Butler <hobu.inc@gmail.com>
2023-02-27 21:42:41 -0600

    force libcxx=14

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -3,6 +3,8 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
+mamba install --yes libcxx=14
+
 if [ "$BUILD_TYPE" == "fixed" ]; then
     mamba install --yes --quiet gdal=3.5.3  python=3.11
 fi
