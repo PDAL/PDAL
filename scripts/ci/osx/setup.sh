5b240185e00fd9157bcf48562f805c6ab8c807cc
Howard Butler <hobu.inc@gmail.com>
2023-05-13 12:26:26 -0500

    update osx-fixed builder for nitro

--- scripts/ci/osx/setup.sh
+++ scripts/ci/osx/setup.sh
@@ -5,8 +5,9 @@ mkdir build
 
 mamba install --yes libcxx=14
 
+# nitro now broken due to OSX update so we need latest version
 if [ "$BUILD_TYPE" == "fixed" ]; then
-    mamba install --yes --quiet gdal=3.5.3  python=3.11
+    mamba install --yes --quiet gdal=3.5.3  python=3.11 nitro=2.7.dev7
 fi
 
 gdal-config --version
