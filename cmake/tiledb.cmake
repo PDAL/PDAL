88bdc3153a7f9e9e131880de24da8968a6b0ef9e
Julia Dark <24235303+jp-dark@users.noreply.github.com>
2023-06-13 09:22:25 -0400

    Fix FindTileDB.cmake to check version (#4077)
    Find version was not correctly checking version due to mixed case name
    differences. This change updates the TileDB find package module to
    consistently use snakecase.

--- cmake/tiledb.cmake
+++ cmake/tiledb.cmake
@@ -1,14 +1,12 @@
 #
 # TileDB support
 #
-set(FPHSA_NAME_MISMATCHED 1) # Suppress warnings, see https://cmake.org/cmake/help/v3.17/module/FindPackageHandleStandardArgs.html
 find_package(TileDB QUIET 2.3.0 REQUIRED)
-set(FPHSA_NAME_MISMATCHED 0)
 
 set_package_properties(TileDB PROPERTIES
         TYPE OPTIONAL
         URL "https://www.tiledb.com"
         PURPOSE "TileDB support")
 
-mark_as_advanced(CLEAR TILEDB_INCLUDE_DIRS)
-mark_as_advanced(CLEAR TILEDB_LIBRARIES)
+mark_as_advanced(CLEAR TileDB_INCLUDE_DIRS)
+mark_as_advanced(CLEAR TileDB_LIBRARIES)
