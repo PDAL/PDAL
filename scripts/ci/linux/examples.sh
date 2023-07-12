9cc59c61e0e95b3eacec1b6b015588a6a936d242
Howard Butler <hobu.inc@gmail.com>
2023-07-12 16:41:13 -0500

    debug-find

--- scripts/ci/linux/examples.sh
+++ scripts/ci/linux/examples.sh
@@ -7,5 +7,4 @@ do
     cd $BASE/examples/$EXAMPLE
     mkdir -p _build || exit 1
     cd _build || exit 1
-    cmake -G "Ninja" .. -DPDAL_DIR=$CONDA_PREFIX/lib/cmake/PDAL -DCMAKE_VERBOSE_MAKEFILE=ON && cmake --build . -v --debug-find-pkg=PDAL
-done
+    cmake -G "Ninja" .. -DPDAL_DIR=$CONDA_PREFIX/lib/cmake/PDAL -DCMAKE_VERBOSE_MAKEFILE=ON --debug-find && cmake --build . -v -done
