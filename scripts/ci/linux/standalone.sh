83c6dd6caf95e120dcf13d55deb372a32ecbbec9
Howard Butler <hobu.inc@gmail.com>
2023-07-14 09:35:46 -0500

    implement STANDALONE plugin building for common plugins

---
+++ scripts/ci/linux/standalone.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -a arr=("i3s" "pgpointcloud" "draco" "nitf" "tiledb" "trajectory" "hdf" "icebridge" )
+
+BASE=`pwd`
+
+for i in "${arr[@]}"
+do
+    cd $BASE
+    cd ./plugins/$i && \
+    rm -rf build ; \
+    mkdir build && cd build && \
+    cmake .. \
+        -DSTANDALONE=ON \
+        -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" -DCMAKE_BUILD_TYPE=Debug -G Ninja && \
+    ninja && \
+    ninja install
+
+done
