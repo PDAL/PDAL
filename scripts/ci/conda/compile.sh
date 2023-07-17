b3d6dfa02cedaf8c8cb6330e29028cd7cd8835eb
Howard Butler <hobu.inc@gmail.com>
2023-07-17 12:15:16 -0500

    shell not initted for mamba

--- scripts/ci/conda/compile.sh
+++ scripts/ci/conda/compile.sh
@@ -16,8 +16,8 @@ if grep -q "macos" <<< "$PDAL_PLATFORM"; then
 fi
 
 mamba build recipe --clobber-file recipe/recipe_clobber.yaml --output-folder packages -m ".ci_support/${CI_PLAT}_64_.yaml"
-mamba create -y -n test -c ./packages/$CI_PLAT-64 python pdal
-mamba deactivate
+conda create -y -n test -c ./packages/$CI_PLAT-64 python pdal
+conda deactivate
 
 conda activate test
 pdal --version
