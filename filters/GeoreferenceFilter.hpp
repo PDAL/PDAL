27cbc4d80a2ce5c022d9656c8a4a583232369278
Guilhem <guilhem.villemin@altametris.com>
2023-07-18 17:10:34 +0200

    namespace depollution + LocalCartesian reverse fix + remove ScanAngleRank

--- filters/GeoreferenceFilter.hpp
+++ filters/GeoreferenceFilter.hpp
@@ -1,36 +1,36 @@
 /******************************************************************************
-* Copyright (c) 2023, Guilhem Villemin (guilhem.villemin@altametris.com)
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following
-* conditions are met:
-*
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in
-*       the documentation and/or other materials provided
-*       with the distribution.
-*     * Neither the name of Hobu, Inc. or Flaxen Consulting LLC nor the
-*       names of its contributors may be used to endorse or promote
-*       products derived from this software without specific prior
-*       written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-* COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-* OF SUCH DAMAGE.
-****************************************************************************/
+ * Copyright (c) 2023, Guilhem Villemin (guilhem.villemin@altametris.com)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of Hobu, Inc. or Flaxen Consulting LLC nor the
+ *       names of its contributors may be used to endorse or promote
+ *       products derived from this software without specific prior
+ *       written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
 
 #pragma once
 #include <pdal/Filter.hpp>
@@ -38,40 +38,39 @@
 #include <pdal/pdal_internal.hpp>
 #include <pdal/util/ProgramArgs.hpp>
 
-
-
 namespace pdal
+{
+namespace georeference
 {
 class TransformationFilter;
 class LocalCartesian;
-
+} // namespace georeference
 class PDAL_DLL GeoreferenceFilter : public Filter, public Streamable
 {
 public:
-  GeoreferenceFilter();
-  ~GeoreferenceFilter();
+    GeoreferenceFilter();
+    ~GeoreferenceFilter();
 
-  GeoreferenceFilter& operator=(const GeoreferenceFilter&) = delete;
-  GeoreferenceFilter(const GeoreferenceFilter&) = delete;
+    GeoreferenceFilter& operator=(const GeoreferenceFilter&) = delete;
+    GeoreferenceFilter(const GeoreferenceFilter&) = delete;
 
-  std::string getName() const;
+    std::string getName() const;
 
 private:
-  virtual void addArgs(ProgramArgs& args) override;
-  virtual void initialize() override;
-  virtual bool processOne(PointRef& point) override;
-  virtual void filter(PointView& view) override;
-  virtual void addDimensions(PointLayoutPtr layout);
-  virtual void prepared(PointTableRef table);
+    virtual void addArgs(ProgramArgs& args) override;
+    virtual void initialize() override;
+    virtual bool processOne(PointRef& point) override;
+    virtual void filter(PointView& view) override;
+    virtual void prepared(PointTableRef table);
 
-  struct Config;
-  std::unique_ptr<TransformationFilter::Transform> m_matrix;
-  std::unique_ptr<Config> m_config;
-  std::unique_ptr<LocalCartesian> m_localCartesian;
-  std::string m_trajectory;
-  std::string m_scan2imu;
-  double m_timeOffset;
-  bool m_reverse;
+    struct Config;
+    std::unique_ptr<TransformationFilter::Transform> m_matrix;
+    std::unique_ptr<Config> m_config;
+    std::unique_ptr<georeference::LocalCartesian> m_localCartesian;
+    std::string m_trajectory;
+    std::string m_scan2imu;
+    double m_timeOffset;
+    bool m_reverse;
 };
 
-}  // namespace pdal
+} // namespace pdal
