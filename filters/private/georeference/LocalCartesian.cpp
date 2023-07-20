a05158a3864dd1753bfcb6ff39aa34800ae9f172
Guilhem <guilhem.villemin@altametris.com>
2023-07-20 18:26:15 +0200

    proj implementation

--- filters/private/georeference/LocalCartesian.cpp
+++ filters/private/georeference/LocalCartesian.cpp
@@ -35,66 +35,103 @@
 #include "LocalCartesian.hpp"
 #include <ogr_spatialref.h>
 #include <ostream>
+#include <proj/io.hpp>
 
 namespace pdal
 {
 namespace georeference
 {
-LocalCartesian::LocalCartesian(double lat0, double lon0, double h0)
-{
-    reset(lat0, lon0, h0);
-}
 
-LocalCartesian::~LocalCartesian() {}
-
-void LocalCartesian::reset(double lat0, double lon0, double h0)
+std::string enuProj(const double lat0, const double lon0, const double h0 = 0.0)
 {
-    OGRCoordinateTransformationOptions coordTransfoOptions;
     std::stringstream ss;
-    // By default OGRCoordinateTransformation class output angular unit are in
-    // radians. To change to degrees we need to apply a unit conversion step.
     ss << std::fixed << std::setprecision(12)
-       << "+proj=pipeline +step +proj=unitconvert +xy_in=deg +xy_out=rad +step "
-          "+proj=cart +ellps=WGS84 +step "
-          "+proj=topocentric +ellps=WGS84 +lon_0="
-       << lon0 << " +lat_0=" << lat0 << " +h_0=" << h0;
-    coordTransfoOptions.SetCoordinateOperation(ss.str().c_str(), false);
-    OGRSpatialReference nullSrs("");
-    m_forwardTransfo.reset(OGRCreateCoordinateTransformation(
-        &nullSrs, &nullSrs, coordTransfoOptions));
-    m_reverseTransfo.reset(m_forwardTransfo->GetInverse());
+       << "+proj=topocentric +ellps=WGS84"
+       << " +lon_0=" << lon0 << " +lat_0=" << lat0 << " +h_0=" << h0;
+    return ss.str();
 }
 
-bool LocalCartesian::forward(PointRef& point)
+// NNPtr has no default ctor so we must init all that here
+LocalCartesian::LocalCartesian(const double lat0, const double lon0,
+                               const double h0)
+    : m_ctx(proj_context_create()),
+      m_source2ecef(NN_CHECK_THROW(nn_dynamic_pointer_cast<CoordinateOperation>(
+                                       createFromUserInput(
+                                           "+proj=cart +ellps=WGS84", m_ctx)))
+                        ->coordinateTransformer(m_ctx)),
+      m_ecef2source(
+          NN_CHECK_THROW(
+              nn_dynamic_pointer_cast<CoordinateOperation>(
+                  createFromUserInput("+proj=cart +ellps=WGS84 +inv", m_ctx)))
+              ->coordinateTransformer(m_ctx)),
+      m_deg2rad(
+          NN_CHECK_THROW(
+              nn_dynamic_pointer_cast<CoordinateOperation>(createFromUserInput(
+                  "+proj=unitconvert +xy_in=deg +xy_out=rad", m_ctx)))
+              ->coordinateTransformer(m_ctx)),
+      m_rad2deg(
+          NN_CHECK_THROW(
+              nn_dynamic_pointer_cast<CoordinateOperation>(createFromUserInput(
+                  "+proj=unitconvert +xy_in=rad +xy_out=deg", m_ctx)))
+              ->coordinateTransformer(m_ctx)),
+      m_ecef2enu(NN_CHECK_THROW(
+                     nn_dynamic_pointer_cast<CoordinateOperation>(
+                         createFromUserInput(enuProj(lat0, lon0, h0), m_ctx)))
+                     ->coordinateTransformer(m_ctx)),
+      m_enu2ecef(NN_CHECK_THROW(
+                     nn_dynamic_pointer_cast<CoordinateOperation>(
+                         createFromUserInput(enuProj(lat0, lon0, h0), m_ctx)))
+                     ->inverse()
+                     ->coordinateTransformer(m_ctx))
+
 {
-    double x(point.getFieldAs<double>(Dimension::Id::X));
-    double y(point.getFieldAs<double>(Dimension::Id::Y));
-    double z(point.getFieldAs<double>(Dimension::Id::Z));
-
-    bool ok = m_forwardTransfo && m_forwardTransfo->Transform(1, &x, &y, &z);
-    if (ok)
-    {
-        point.setField(Dimension::Id::X, x);
-        point.setField(Dimension::Id::Y, y);
-        point.setField(Dimension::Id::Z, z);
-    }
-    return ok;
 }
 
-bool LocalCartesian::reverse(PointRef& point)
+LocalCartesian::~LocalCartesian()
 {
-    double x(point.getFieldAs<double>(Dimension::Id::X));
-    double y(point.getFieldAs<double>(Dimension::Id::Y));
-    double z(point.getFieldAs<double>(Dimension::Id::Z));
-
-    bool ok = m_reverseTransfo && m_reverseTransfo->Transform(1, &x, &y, &z);
-    if (ok)
-    {
-        point.setField(Dimension::Id::X, x);
-        point.setField(Dimension::Id::Y, y);
-        point.setField(Dimension::Id::Z, z);
-    }
-    return ok;
+    proj_context_destroy(m_ctx);
 }
+
+void LocalCartesian::reset(const double lat0, const double lon0,
+                           const double h0)
+{
+    auto ecef2enuOperation =
+        NN_CHECK_THROW(nn_dynamic_pointer_cast<CoordinateOperation>(
+            createFromUserInput(enuProj(lat0, lon0, h0), m_ctx)));
+    m_ecef2enu = ecef2enuOperation->coordinateTransformer(m_ctx);
+    m_enu2ecef = ecef2enuOperation->inverse()->coordinateTransformer(m_ctx);
+};
+
+void LocalCartesian::forward(PointRef& point)
+{
+    PJ_COORD c = {{point.getFieldAs<double>(Dimension::Id::X),
+                   point.getFieldAs<double>(Dimension::Id::Y),
+                   point.getFieldAs<double>(Dimension::Id::Z), HUGE_VAL}};
+
+    std::cout << __PRETTY_FUNCTION__ << " " << c.v[0] << " " << c.v[1] << " "
+              << c.v[2] << std::endl;
+    c = m_ecef2enu->transform(
+        m_source2ecef->transform(m_deg2rad->transform(c)));
+    std::cout << __PRETTY_FUNCTION__ << " " << c.v[0] << " " << c.v[1] << " "
+              << c.v[2] << std::endl;
+    point.setField(Dimension::Id::X, c.v[0]);
+    point.setField(Dimension::Id::Y, c.v[1]);
+    point.setField(Dimension::Id::Z, c.v[2]);
+};
+
+void LocalCartesian::reverse(PointRef& point)
+{
+    PJ_COORD c = {{point.getFieldAs<double>(Dimension::Id::X),
+                   point.getFieldAs<double>(Dimension::Id::Y),
+                   point.getFieldAs<double>(Dimension::Id::Z), HUGE_VAL}};
+
+    c = m_rad2deg->transform(
+        m_ecef2source->transform(m_enu2ecef->transform(c)));
+
+    point.setField(Dimension::Id::X, c.v[0]);
+    point.setField(Dimension::Id::Y, c.v[1]);
+    point.setField(Dimension::Id::Z, c.v[2]);
+}
+
 } // namespace georeference
 } // namespace pdal
