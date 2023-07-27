5eacf96857cd54c3fc9a09ac42dcd47ae911d605
Andrew Bell <andrew.bell.ia@gmail.com>
2023-07-27 12:59:52 -0400

    Fix OB1 error with pdal tile.  Add test. Close #4076

--- kernels/TileKernel.cpp
+++ kernels/TileKernel.cpp
@@ -234,15 +234,14 @@ void TileKernel::process(const Readers& readers)
         while (!finished)
         {
             // Read subsequent points.
-            while (true)
+            for (; idx < m_table.capacity(); idx++)
             {
                 point.setPointId(idx);
                 finished = !StreamableWrapper::processOne(r, point);
-                idx++;
-                if (idx == m_table.capacity() || finished)
+                if (finished)
                     break;
             }
-            PointId last = idx - 1;
+            PointId end = idx;
 
             SpatialReference srs = r.getSpatialReference();
             if (!srs.empty())
@@ -250,7 +249,7 @@ void TileKernel::process(const Readers& readers)
             // Reproject if necessary.
             if (m_repro)
             {
-                for (idx = 0; idx < last; ++idx)
+                for (idx = 0; idx < end; ++idx)
                 {
                     point.setPointId(idx);
                     if (!StreamableWrapper::processOne(*m_repro, point))
@@ -262,14 +261,13 @@ void TileKernel::process(const Readers& readers)
             }
 
             // Split and write.
-            for (idx = 0; idx < last; ++idx)
+            for (idx = 0; idx < end; ++idx)
             {
                 if (skips[idx])
                     continue;
 
                 point.setPointId(idx);
                 m_splitter.processPoint(point, adder);
-
             }
             for (size_t i = 0; i < skips.size(); ++i)
                 skips[i] = false;
