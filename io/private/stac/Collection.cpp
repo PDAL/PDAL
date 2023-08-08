64d9a471088dd169cf4788275ca8f656b611273f
Kyle Mann <39525199+kylemann16@users.noreply.github.com>
2023-08-08 15:33:10 -0500

    STAC Reader overhaul (#4057)
    * Relative paths to asset data now available. Associated tests added.
    
    * Change path method name.
    
    * splitting up STAC classes WIP
    
    * WIP, questions
    
    * Items working.
    
    * WIP debugging
    
    * Fixed Reader bug, debugging catalog
    
    * Adding catalog.cpp
    
    * Items and Catalogs working again.
    
    * Schema validation split into catalog and item
    
    * Filtering add WIP
    
    * Threading and filtering now working.
    
    * ItemCollection split into its own class.
    
    * ItemCollections working
    
    * Add next links to item collection test
    
    * WIP adding Collections. Failing threading.
    
    * Collections working. Updated readme to reflect new ID syntax
    
    * make sure to check result of dynamic_cast
    
    * Fixing out of scope errors
    
    * Revert alpine changes
    
    * Split up item filtering to be easier to read/follow
    
    * fixing filter logic
    
    * Fixing bad insertion
    
    * Add test for pdal_wrench virtual point cloud
    
    * Adding synonyms, adding stricter date validation/support
    
    * Change test to have better interaction
    
    * initialize tm struct
    
    * Json schema changes, date validation, json crackdown
    
    * Json fetching util scripts added and replace all json fetching
    
    * Cleaning up json fetching and error messaging
    
    * Debugging bound checking problems:w
    
    * Fixed polygon/bbox filtering
    
    * add back tests
    
    * Move head request further up logic flow to test of remote files exist.
    
    * linting changes, using namespace stac to clear up space, minor changes to clarity
    
    * Changing StacUtils class to namespace
    
    * Break lambda into smaller chunks
    
    * Fixing reference/copy usage in lambda
    
    * Add failure if catalogs/collections have collected errors
    
    * Added more metadata output and general object bookkeeping, included docs section to complement
    
    * Reflect previous commit in tests
    
    * Fix nested catalog logic and error handling. Hoist all catalogs up the chain.
    
    * Test changes, readme fixes
    
    * Change docs and reduce duplicate code
    
    * Forward declare rather than include.
    
    * satisfy windows linking
    
    ---------
    
    Co-authored-by: Howard Butler <hobu.inc@gmail.com>

---
+++ io/private/stac/Collection.cpp
@@ -0,0 +1,100 @@
+/******************************************************************************
+ * Copyright (c) 2022, Kyle Mann (kyle@hobu.co)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of the Martin Isenburg or Iowa Department
+ *       of Natural Resources nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
+#include "Collection.hpp"
+
+#include "Utils.hpp"
+#include <nlohmann/json.hpp>
+#include <schema-validator/json-schema.hpp>
+
+namespace pdal
+{
+
+namespace stac
+{
+
+using namespace StacUtils;
+
+Collection::~Collection() {}
+
+void Collection::validate() {
+    nlohmann::json_schema::json_validator val(
+        [this](const nlohmann::json_uri& json_uri, nlohmann::json& json) {
+            json = m_connector.getJson(json_uri.url());
+        },
+        [](const std::string &, const std::string &) {}
+    );
+
+    // Validate against base Collection schema first
+    NL::json schemaJson = m_connector.getJson(m_schemaUrls.collection);
+    val.set_root_schema(schemaJson);
+    try {
+        val.validate(m_json);
+    }
+    catch (std::exception &e)
+    {
+        throw stac_error(m_id, "collection",
+            "STAC schema validation Error in root schema: " +
+            m_schemaUrls.collection + ". \n\n" + e.what());
+    }
+
+    // Validate against stac extensions if present
+    if (m_json.contains("stac_extensions"))
+    {
+        NL::json extensions = stacValue(m_json, "stac_extensions");
+        for (auto& extSchemaUrl: extensions)
+        {
+            std::string url = stacValue<std::string>(extSchemaUrl,
+                "", m_json);
+
+            try {
+                NL::json schemaJson = m_connector.getJson(url);
+                val.set_root_schema(schemaJson);
+                val.validate(m_json);
+            }
+            catch (std::exception& e) {
+                std::string msg  =
+                    "STAC Validation Error in extension: " + url +
+                    ". Errors found: \n" + e.what();
+                throw stac_error(m_id, "collection", msg);
+
+            }
+        }
+
+    }
+}
+
+
+}//stac
+
+}//pdal
---
+++ io/private/stac/Utils.cpp
@@ -0,0 +1,147 @@
+/******************************************************************************
+ * Copyright (c) 2022, Kyle Mann (kyle@hobu.co)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of the Martin Isenburg or Iowa Department
+ *       of Natural Resources nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ ****************************************************************************/
+
+#include "Utils.hpp"
+
+namespace pdal
+{
+
+namespace stac
+{
+
+pdal_error stac_error(std::string id, std::string stacType,
+    std::string const& msg)
+{
+    return pdal_error("STACError (" + stacType + ": " + id + "): " + msg);
+}
+
+pdal_error stac_error(std::string const& msg)
+{
+    return pdal_error("STACError: " + msg);
+}
+
+namespace StacUtils
+{
+
+// StacUtils::StacUtils() {}
+// StacUtils::~StacUtils() {}
+
+std::string handleRelativePath(std::string srcPath, std::string linkPath)
+{
+    //Make absolute path of current item's directory, then create relative path from that
+
+    //Get driectory of src item
+    const std::string baseDir = FileUtils::getDirectory(srcPath);
+    if (FileUtils::isAbsolutePath(linkPath))
+        return linkPath;
+    //Create absolute path from src item filepath, if it's not already
+    // and join relative path to src item's dir path
+    return FileUtils::toAbsolutePath(linkPath, baseDir);
+
+}
+
+std::time_t getStacTime(std::string in)
+{
+    std::istringstream dateStr(in);
+    std::tm date {};
+    dateStr >> std::get_time(&date, "%Y-%m-%dT%H:%M:%S");
+    if (dateStr.fail())
+        throw stac_error("Specified date (" + dateStr.str() +
+            ") cannot be parsed. Dates must fit RFC 3339 specs.");
+    return std::mktime(&date);
+}
+
+std::string stacId(const NL::json& stac)
+{
+    std::stringstream msg;
+    try
+    {
+        return stac.at("id").get<std::string>();
+    }
+    catch (NL::detail::out_of_range e)
+    {
+        msg << "Missing required key 'id'. " << e.what();
+        throw pdal_error(msg.str());
+    }
+    catch (NL::detail::type_error e)
+    {
+        msg << "Required key 'id' is not of type 'string'. " << e.what();
+        throw pdal_error(msg.str());
+    }
+}
+
+std::string stacType(const NL::json& stac)
+{
+    try
+    {
+        return stac.at("type").get<std::string>();
+    }
+    catch (NL::detail::out_of_range e)
+    {
+        std::stringstream msg;
+        msg << "Missing required key 'type'. " << e.what();
+        throw pdal_error(msg.str());
+    }
+    catch (NL::detail::type_error e)
+    {
+        std::stringstream msg;
+        msg << "Invalid key value 'type'. " << e.what();
+        throw pdal_error(msg.str());
+    }
+}
+
+std::string icSelfPath(const NL::json& json)
+{
+    try
+    {
+        NL::json links = jsonValue(json, "links");
+        for (const NL::json& link: links)
+        {
+            std::string target = jsonValue<std::string>(link, "rel");
+            if (target == "self")
+                return jsonValue<std::string>(link, "href");
+        }
+    }
+    catch(std::runtime_error e)
+    {
+        return "";
+    }
+
+    return "";
+}
+
+
+}//StacUtils
+}//stac
+}//pdal
