611f2c59c1bdaf1cfdbb22e33b57011465475e65
Connor Manning <connor@hobu.co>
2023-11-21 13:28:31 -0600

    Correct the metadata point counts when we've created a zero-point LAS file for 'info' purposes.

--- kernels/private/PointlessLas.cpp
+++ kernels/private/PointlessLas.cpp
@@ -57,7 +57,7 @@ arbiter::http::Headers getRangeHeader(int start, int end = 0)
 
 } // namespace
 
-std::unique_ptr<arbiter::LocalHandle> getPointlessLasFile(const std::string& path)
+PointlessLas getPointlessLasFile(const std::string& path)
 {
     assert(Utils::isRemote(path));
 
@@ -77,6 +77,8 @@ std::unique_ptr<arbiter::LocalHandle> getPointlessLasFile(const std::string& pat
     uint32_t pointOffset(0);
     uint64_t evlrOffset(0);
     uint32_t evlrNumber(0);
+    uint32_t legacyPointCount(0);
+    uint64_t pointCount(0);
 
     arbiter::Arbiter a;
 
@@ -107,6 +109,10 @@ std::unique_ptr<arbiter::LocalHandle> getPointlessLasFile(const std::string& pat
     is.seek(pointOffsetPos);
     is >> pointOffset;
 
+    is.seek(legacyPointCountPos);
+    is >> legacyPointCount;
+    pointCount = legacyPointCount;
+
     // Set the legacy point count to 0 since we are removing the point data.
     os.seek(legacyPointCountPos);
     os << (uint32_t)0;
@@ -118,6 +124,9 @@ std::unique_ptr<arbiter::LocalHandle> getPointlessLasFile(const std::string& pat
 
         is.seek(evlrNumberPos);
         is >> evlrNumber;
+
+        is.seek(pointCountPos);
+        is >> pointCount;
 
         // Modify the header such that the EVLRs come directly after the VLRs -
         // removing the point data itself.
@@ -156,7 +165,10 @@ std::unique_ptr<arbiter::LocalHandle> getPointlessLasFile(const std::string& pat
 
     const std::string localPath = arbiter::join(arbiter::getTempPath(), basename);
     a.put(localPath, data);
-    return std::make_unique<arbiter::LocalHandle>(localPath, /*isRemote=*/ false);
+    return PointlessLas {
+        pointCount,
+        std::make_unique<arbiter::LocalHandle>(localPath, /*isRemote=*/ false)
+    };
 }
 
 } // namespace pdal
