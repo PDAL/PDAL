b65adfc36a126d16e94fc5e6f86b27d0e96ae3e1
Andrew Bell <andrew.bell.ia@gmail.com>
2023-12-05 04:27:59 +0100

    Remove remaining pdal_util library references (#4267)
    * Remove remaining PDAL_UTIL references.
    
    * Remove unnecessary cmake files.
    
    * Add missed file.
    
    * Fix lasdump build.

--- cmake/unwind.cmake
+++
@@ -1,8 +0,0 @@
-#
-# Unwind
-#
-
-set(FPHSA_NAME_MISMATCHED 1) # Suppress warnings, see https://cmake.org/cmake/help/v3.17/module/FindPackageHandleStandardArgs.html
-find_package(Libunwind QUIET)
-set(FPHSA_NAME_MISMATCHED 0)
-
