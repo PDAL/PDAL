057bc0bfd8895685bd0e62af3ef6ab6e01c73dea
Howard Butler <hobu.inc@gmail.com>
2023-12-26 14:48:03 -0600

    shut off cmake debug

--- scripts/ci/linux/examples.sh
+++ scripts/ci/linux/examples.sh
@@ -7,5 +7,5 @@ do
     cd $BASE/examples/$EXAMPLE
     mkdir -p _build || exit 1
     cd _build || exit 1
-    cmake -G "Ninja" .. -DPDAL_DIR=$CONDA_PREFIX/lib/cmake/PDAL -DCMAKE_VERBOSE_MAKEFILE=ON --debug-find && cmake --build . -v
+    cmake -G "Ninja" .. -DPDAL_DIR=$CONDA_PREFIX/lib/cmake/PDAL && cmake --build . -v
 done
