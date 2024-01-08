a395a36fa12ec794e7aea387100553d9c2c43354
Howard Butler <hobu.inc@gmail.com>
2024-01-07 20:34:03 -0600

    Use standard CMake installation locations (#4291)
    * use standard CMake variables for install and build locations to attempt to address #4280
    
    * PDAL_DRIVER_PATH subs
    
    * shut off cmake debug

--- scripts/conda/osx.sh
+++ scripts/conda/osx.sh
@@ -2,9 +2,14 @@
 BUILDDIR=conda-build
 
 CONFIG="Debug"
+INSTALL_PREFIX="$CONDA_PREFIX"
 
 if ! [ -z "$1" ]; then
     CONFIG="$1"
+fi
+
+if ! [ -z "$2" ]; then
+    INSTALL_PREFIX="$2"
 fi
 
 
@@ -17,7 +22,16 @@ CFLAGS= CXXFLAGS="-Werror=strict-aliasing" CC=/usr/bin/cc CXX=/usr/bin/c++ cmake
         -DCMAKE_FIND_FRAMEWORK="NEVER" \
         -DCMAKE_BUILD_TYPE=$CONFIG \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-        -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
+        -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
+        -DCMAKE_INSTALL_BINDIR="$INSTALL_PREFIX/bin" \
+        -DCMAKE_INSTALL_LIBEXECDIR="$INSTALL_PREFIX/libexec" \
+        -DCMAKE_INSTALL_LOCALEDIR="$INSTALL_PREFIX/locale" \
+        -DCMAKE_INSTALL_LIBDIR="$INSTALL_PREFIX/lib" \
+        -DCMAKE_INSTALL_DOCDIR="$INSTALL_PREFIX/man" \
+        -DCMAKE_INSTALL_INCLUDEDIR="$INSTALL_PREFIX/include" \
+        -DCMAKE_INSTALL_NAME_DIR="$INSTALL_PREFIX/lib" \
+        -DCMAKE_INSTALL_SBINDIR="$INSTALL_PREFIX/sbin" \
+        -DCMAKE_INSTALL_OLDINCLUDEDIR="$INSTALL_PREFIX/include" \
         -DBUILD_PLUGIN_PGPOINTCLOUD=ON \
         -DBUILD_PLUGIN_NITF=ON \
         -DBUILD_PLUGIN_HDF=ON \
@@ -29,6 +43,7 @@ CFLAGS= CXXFLAGS="-Werror=strict-aliasing" CC=/usr/bin/cc CXX=/usr/bin/c++ cmake
         -DCMAKE_CXX_FLAGS="-fsanitize=address" \
         -DBUILD_PLUGIN_TILEDB=OFF \
         -DWITH_ZSTD=ON \
+        -DWITH_TESTS=ON \
         ..
 
 
