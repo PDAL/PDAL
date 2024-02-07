7c8aea5aabb841b41e070e1e4516315ac3bf72de
Alexander Nehrbass <alex.bass@mailbox.org>
2024-02-07 17:03:33 +0100

    CMake: Add WITH_OPENSSL CMake-option
    Linking against OpenSSL is optional and not necessary if
    Google storage IO support is not required (or desired.
    However, until now there was now way of disabling linking
    against OpenSSL in CMake.
    
    The added WITH_OPENSSL CMake-option is set to TRUE to keep
    the current default behavior.

--- cmake/openssl.cmake
+++ cmake/openssl.cmake
@@ -1,7 +1,14 @@
-find_package(OpenSSL 1.1)
-if (OPENSSL_FOUND)
-    set(ARBITER_OPENSSL TRUE)
-else ()
-    message("OpenSSL NOT found - `export OPENSSL_ROOT_DIR=___`")
-    message("Google storage IO will not be available")
+#
+# OpenSSL support
+#
+option(WITH_OPENSSL
+    "Build with OpenSSL for Google storage IO support" TRUE)
+if (WITH_OPENSSL)
+    find_package(OpenSSL 1.1)
+    if (OPENSSL_FOUND)
+        set(ARBITER_OPENSSL TRUE)
+    else ()
+        message("OpenSSL NOT found - `export OPENSSL_ROOT_DIR=___`")
+        message("Google storage IO will not be available")
+    endif ()
 endif ()
