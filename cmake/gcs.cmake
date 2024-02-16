d4d9af85e905506ab87297350edb7ef81aeebcdc
Howard Butler <hobu.inc@gmail.com>
2024-02-16 13:25:30 -0600

    rename this to be WITH_GCS

--- cmake/gcs.cmake
+++ cmake/gcs.cmake
@@ -1,14 +1,17 @@
 #
-# OpenSSL support
+# Google Cloud Storage support
 #
-option(WITH_OPENSSL
-    "Build with OpenSSL for Google storage IO support" TRUE)
-if (WITH_OPENSSL)
+
+
+option(WITH_GCS
+    "Build with OpenSSL and others for Google storage IO support" TRUE)
+if (WITH_GCS)
     find_package(OpenSSL 1.1)
     if (OPENSSL_FOUND)
         set(ARBITER_OPENSSL TRUE)
     else ()
         message("OpenSSL NOT found - `export OPENSSL_ROOT_DIR=___`")
         message("Google storage IO will not be available")
+        set(ARBITER_OPENSSL FALSE)
     endif ()
 endif ()
