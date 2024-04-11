c4ddf96d96a2d500c3983894b475ad3b7be1ee2a
Andrew Bell <andrew.bell.ia@gmail.com>
2024-04-11 14:43:12 -0400

    Remove dead comments.

--- pdal/PointRef.hpp
+++ pdal/PointRef.hpp
@@ -51,19 +51,6 @@ private:
     }
 
 public:
-    // TODO: These three special methods:
-    //   PointRef()
-    //   PointRef(const PointRef&)
-    //   operator=(const PointRef&)
-    // which are not really intended for downstream usage but rather for
-    // allowing std::sort and friends to work, are not necessarily working in
-    // all compilers and should be avoided for mutating operations like
-    // std::sort (use PointView::stableSort instead).  Read-only operations like
-    // std::accumulate should be fine.  For now, PointView::stableSort is a
-    // stopgap since PDAL sorts PointViews internally in several places.
-
-    // This ctor is normally used by some std::algorithm that needs a temporary.
-    // See the copy ctor below for more info.
     PointRef() : m_table(nullptr), m_idx(0), m_view(nullptr), m_viewIdx(0)
     {}
 
