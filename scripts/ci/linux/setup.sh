c5f409e7e55d878c66c644c27f716a34a5754185
Howard Butler <hobu.inc@gmail.com>
2024-07-22 16:56:19 -0500

    parameterize 'current' and 'fixed' build variants (#4454)

--- scripts/ci/linux/setup.sh
+++ scripts/ci/linux/setup.sh
@@ -3,9 +3,5 @@
 echo "Configuring build type '$BUILD_TYPE'"
 mkdir build
 
-if [ "$BUILD_TYPE" == "fixed" ]; then
-    mamba install --yes --quiet gdal=3.7.1 python=3.11 abseil-cpp  -y
-fi
-
 gdal-config --version
 
