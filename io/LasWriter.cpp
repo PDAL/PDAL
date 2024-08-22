07e43d6f2f9a0e9489875e984f43dfff914366d6
Howard Butler <hobu.inc@gmail.com>
2024-08-22 13:56:22 -0500

    Default writers.las.dataformat_id to 6

--- io/LasWriter.cpp
+++ io/LasWriter.cpp
@@ -150,9 +150,9 @@ void LasWriter::addArgs(ProgramArgs& args)
     args.add("minor_version", "LAS minor version", d->opts.minorVersion,
         decltype(d->opts.minorVersion)(4));
     args.add("dataformat_id", "Point format", d->opts.dataformatId,
-        decltype(d->opts.dataformatId)(3));
+        decltype(d->opts.dataformatId)(6));
     args.add("format", "Point format", d->opts.dataformatId,
-        decltype(d->opts.dataformatId)(3));
+        decltype(d->opts.dataformatId)(6));
     args.add("global_encoding", "Global encoding byte", d->opts.globalEncoding,
         decltype(d->opts.globalEncoding)(0));
     args.add("project_id", "Project ID", d->opts.projectId);
--- test/unit/io/LasWriterTest.cpp
+++ test/unit/io/LasWriterTest.cpp
@@ -479,6 +479,7 @@ TEST(LasWriterTest, extra_dims)
     Options writerOps;
     writerOps.add("extra_dims", "Red=int32, Blue = int16, Green = int32_t");
     writerOps.add("minor_version", "2");
+    writerOps.add("dataformat_id", "3");
     writerOps.add("filename", Support::temppath("simple.las"));
     LasWriter writer;
     writer.setInput(reader);
@@ -617,6 +618,7 @@ TEST(LasWriterTest, forward)
     Options writerOps;
     writerOps.add("forward", "header");
     writerOps.add("minor_version", 3);
+    writerOps.add("dataformat_id", 3);
     writerOps.add("filename", testfile);
 
     LasWriter w;
@@ -833,6 +835,7 @@ TEST(LasWriterTest, lazperf)
     Options writerOps;
     writerOps.add("filename", testfile);
     writerOps.add("compression", "lazperf");
+    writerOps.add("forward", "dataformat_id");
 
     LasWriter lazWriter;
     lazWriter.setOptions(writerOps);
@@ -1768,6 +1771,7 @@ TEST(LasWriterTest, oversize_vlr)
 
     o.add("filename", "out.las");
     o.add("minor_version", "2");
+    o.add("dataformat_id", "3");
     w.addOptions(o);
 
     PointTable t;
