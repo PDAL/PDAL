fe23535775f8847ef3ef0ca66bc1abc2c8c1402b
ibell13 <mightybell@gmail.com>
2024-09-25 16:49:38 -0500

    windows max fix

--- filters/private/hexer/H3grid.hpp
+++ filters/private/hexer/H3grid.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include <h3api.h>
 
 #include "BaseGrid.hpp"
--- filters/private/hexer/HexGrid.hpp
+++ filters/private/hexer/HexGrid.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include "BaseGrid.hpp"
 
 namespace hexer
