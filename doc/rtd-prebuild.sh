33fe17c06b49c4d13e570589dcc4bf601ee7a21e
ibell13 <isaac@hobu.co>
2025-04-10 13:08:30 -0500

    update jupyter/sphinx build directory

--- doc/rtd-prebuild.sh
+++ doc/rtd-prebuild.sh
@@ -3,9 +3,9 @@
 # Assuming readthedocs is running this from pdal root directory
 
 # Making a build directory for cmake - if we name it _build it will also be
-# the default output location for jupyterbook. This can be customizable if we want
-mkdir -p _build
-cd _build
+# the default output location for jupyterbook (I think?). This can be customizable if we want
+mkdir -p doc/_build
+cd doc/_build
 
 # JB_CONF_APPEND variable replaces the sed command to append conf.py.
 # JB_CONF_PREPEND replaces the sed command to prepend conf.py, set to
@@ -13,7 +13,7 @@ cd _build
 cmake \
 	 -DBUILD_API_DOCS=ON \
 	 -DJB_CONF_APPEND="html_baseurl = os.environ.get(\\\"READTHEDOCS_CANONICAL_URL\\\", \\\"\\\")" \
-	 ../doc/
+	 ..
 # Target makes dimension-table.csv, runs doxygen, runs jupyter-book config
 cmake --build . --target generate_docs
 
