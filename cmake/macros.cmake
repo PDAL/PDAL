6eb217387993c7d7b401861d6056f984eb35c963
Howard Butler <hobu.inc@gmail.com>
2025-04-14 14:53:18 -0500

    de-vendor gtest (#4679)
    * de-vendor gtest
    
    * pull in GTest cmake config if we need it before adding a test

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -184,6 +184,11 @@ endmacro(PDAL_ADD_PLUGIN)
 #    LINK_WITH link test executable with libraries
 #    INCLUDES header file directories
 #
+# If we don't have GTest go activate it first
+if(NOT TARGET GTest::gtest)
+    include (${PDAL_CMAKE_DIR}/gtest.cmake)
+endif()
+
 macro(PDAL_ADD_TEST _name)
 
     if (NOT WITH_TESTS)
@@ -216,6 +221,7 @@ macro(PDAL_ADD_TEST _name)
         PRIVATE
             ${PDAL_LIB_NAME}
             GTest::gtest
+            GTest::gtest_main
             ${PDAL_ADD_TEST_LINK_WITH}
             ${WINSOCK_LIBRARY}
     )
