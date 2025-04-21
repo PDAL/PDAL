763d80399f9f6e86fdca4a97ae7090d76bef74d7
ibell13 <isaac@hobu.co>
2025-04-21 15:04:03 -0500

    Update rtd_deploy.py

--- doc/_ext/rtd_deploy.py
+++ doc/_ext/rtd_deploy.py
@@ -1,6 +1,8 @@
+# script to add readthedocs canonical url
 import os
 
 def setup(app):
     if 'READTHEDOCS_CANONICAL_URL' in os.environ:
+        del app.config.html_baseurl
         print('\n\n\nrtd env set: ' + os.getenv("READTHEDOCS_CANONICAL_URL") + '\n\n\n')
         app.add_config_value('html_baseurl', os.environ.get('READTHEDOCS_CANONICAL_URL', ''), 'env')
