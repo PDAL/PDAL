98acf262c0eb80b8921220e2cc8d7fc5c6d9a6cc
ibell13 <isaac@hobu.co>
2025-04-22 15:42:07 -0500

    add all build steps for rtd

--- doc/_ext/rtd_deploy.py
+++ doc/_ext/rtd_deploy.py
@@ -1,6 +1,5 @@
-# script to add readthedocs canonical url
 import os
 
 def setup(app):
-    if 'READTHEDOCS_CANONICAL_URL' in os.environ:
-        app.config["html_baseurl"] = os.environ.get('READTHEDOCS_CANONICAL_URL', '')
+    if os.environ.get('READTHEDOCS') == 'True':
+        app.config["html_baseurl"] = os.environ['READTHEDOCS_CANONICAL_URL']
