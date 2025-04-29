edf459bbf9b68274d4b2b97a137fa55fc2372dc5
Isaac Bell <122644517+ibell13@users.noreply.github.com>
2025-04-28 22:32:48 -0500

    Remove `jupyter-book build` in ReadTheDocs (#4702)

--- doc/rtd-prebuild.sh
+++ doc/rtd-prebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Assuming readthedocs is running this from pdal root directory
-
 # Making a build directory for cmake - if we name it _build it will also be
 # the output location for jupyterbook. This can be customizable if we want
 mkdir -p doc/_build
@@ -10,5 +8,8 @@ cd doc/_build
 cmake \
 	-DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
 	..
+
 # Target makes dimension-table.csv, runs doxygen
 cmake --build . --target doxygen
+# Target creates conf.py
+cmake --build . --target jupyterbook_config
