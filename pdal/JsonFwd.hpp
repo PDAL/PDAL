6ec58a4aa641327a6eab99db1456d12840cc476c
Andrew Bell <andrew.bell.ia@gmail.com>
2025-05-13 14:36:55 -0400

    Move some stuff around.

--- pdal/JsonFwd.hpp
+++ pdal/JsonFwd.hpp
@@ -2,7 +2,7 @@
 
 #if !defined(INCLUDE_NLOHMANN_JSON_FWD_HPP_) && !defined(INCLUDE_NLOHMANN_JSON_HPP_)
 
-#include "../vendor/nlohmann/nlohmann/json_fwd.hpp"
+#include "./json/json_fwd.hpp"
 
 #else
 
