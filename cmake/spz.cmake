760d573a64b3d7369495b37ae4d5eb31fba2f1fc
Andrew Bell <andrew.bell.ia@gmail.com>
2025-08-12 08:41:42 -0400

    Use namespace-qualified name for spz library. (#4783)
    * Use namespace-qualified name for spz library.
    
    * remove lint
    
    ---------
    
    Co-authored-by: Howard Butler <hobu.inc@gmail.com>

--- cmake/spz.cmake
+++ cmake/spz.cmake
@@ -4,7 +4,7 @@
 
 # The oldest spz release that's on conda-forge is 2.0.0,
 # but they have it marked as 1.1 in their cmake file.
-find_package(spz 1.1.0 REQUIRED CONFIG)
+find_package(spz 1.1.0 REQUIRED)
 set_package_properties(spz PROPERTIES TYPE REQUIRED
     PURPOSE "SPZ gaussian splat format support")
 if(spz_FOUND)
