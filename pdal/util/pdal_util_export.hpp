cc0e8c2e6f3941a2c5943ca01ab4b0cc4da05045
Howard Butler <hobu.inc@gmail.com>
2025-09-02 08:47:41 -0500

    alias PDAL_DLL => PDAL_EXPORT for now

--- pdal/util/pdal_util_export.hpp
+++ pdal/util/pdal_util_export.hpp
@@ -37,13 +37,10 @@
 
 #pragma once
 
-#ifdef PDAL_DLL
-#warning "PDAL_DLL has been deprecated in favor of PDAL_EXPORT. Please update your code"
-#   define PDAL_DLL     PDAL_EXPORT
-#endif
 
 #ifdef _WIN32
 #   define PDAL_EXPORT   __declspec(dllexport)
+#   define PDAL_DLL     PDAL_EXPORT
 #   define PDAL_LOCAL
 #   define PDAL_EXPORT_UNIX
 #ifdef __GNUC__
@@ -55,6 +52,7 @@
 #   define PDAL_EXPORT     __attribute__ ((visibility("default")))
 // Keep the PDAL_DLL name around so any external plugins that still might have that defined
 // can still compile and be used
+#   define PDAL_DLL     PDAL_EXPORT
 #   define PDAL_LOCAL   __attribute__((visibility("hidden")))
 #   define PDAL_EXPORT_UNIX   __attribute__ ((visibility("default")))
 #   define PDAL_EXPORT_DEPRECATED   __attribute__((deprecated))
