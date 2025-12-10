de6ca7629b566685ea40102f2290f955f8566354
Peter Chen <kevincwq@gmail.com>
2025-12-09 17:16:16 -0700

    Update make list and inlcude missing files

--- plugins/e57/libE57Format/cmake/ClangFormat.cmake
+++ plugins/e57/libE57Format/cmake/ClangFormat.cmake
@@ -12,17 +12,6 @@ if ( E57_CLANG_FORMAT_PROGRAM )
     list( FILTER e57_sources EXCLUDE REGEX ".*/E57Export.h" )
     list( FILTER e57_sources EXCLUDE REGEX ".*/extern/.*" )
 
-    # Get list of test files. We cannot use get_target_property here
-    # since we will not have a target if E57_BUILD_TEST is off.
-    file( GLOB e57_test_sources
-        LIST_DIRECTORIES false
-        CONFIGURE_DEPENDS
-        ${PROJECT_SOURCE_DIR}/test/include/*.h
-        ${PROJECT_SOURCE_DIR}/test/src/*.cpp
-    )
-
-    list( APPEND e57_sources ${e57_test_sources} )
-
     add_custom_target( e57-clang-format
         COMMAND ${E57_CLANG_FORMAT_PROGRAM} --style=file -i ${e57_sources}
         COMMENT "Running clang-format..."
