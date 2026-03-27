2c83e1ec3006760b1446d306c7167cbfa0cbe0de
Mathieu Westphal <mathieu.westphal@kitware.com>
2026-03-27 20:45:26 +0100

    Support static build (plugins not working)

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -1,6 +1,6 @@
 # Build shared libraries by default.
 
-set(PDAL_LIB_TYPE "SHARED")
+set(PDAL_LIB_TYPE "STATIC")
 if (WIN32)
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_IMPORT_LIBRARY_SUFFIX})
 endif()
