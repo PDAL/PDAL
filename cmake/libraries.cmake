1ed658e711a89563220e21b5781e7a1334510ebb
Mathieu Westphal <mathieu.westphal@kitware.com>
2026-03-27 20:45:26 +0100

    CMake: Add BUILD_SHARED_LIBS option and support static build

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -1,6 +1,10 @@
 # Build shared libraries by default.
 
-set(PDAL_LIB_TYPE "SHARED")
+if (BUILD_SHARED_LIBS)
+  set(PDAL_LIB_TYPE "SHARED")
+else ()
+  set(PDAL_LIB_TYPE "STATIC")
+endif ()
 if (WIN32)
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_IMPORT_LIBRARY_SUFFIX})
 endif()
