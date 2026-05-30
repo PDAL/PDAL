723d1b2ea03bc10deb1369a851c5046edee0a81e
Andrew Bell <andrew.bell.ia@gmail.com>
2026-05-30 13:18:07 -0400

    Encapsulate dimrange (#5033)
    * Encapsulate behavior of DimRange by creating DimRangeList.
    Remove out-of-class sorting of DimRange.
    
    * Small fixes.

--- filters/RadiusAssignFilter.cpp
+++ filters/RadiusAssignFilter.cpp
@@ -1,15 +1,31 @@
 #include "RadiusAssignFilter.hpp"
 
+#include <vector>
+
+#include <pdal/KDIndex.hpp>
 #include <pdal/PipelineManager.hpp>
 #include <pdal/StageFactory.hpp>
 #include <pdal/util/ProgramArgs.hpp>
 
 #include "private/DimRange.hpp"
+#include "private/expr/AssignStatement.hpp"
 
-#include <iostream>
 #include <utility>
 namespace pdal
 {
+
+struct RadiusAssignFilter::Private
+{
+    DimRangeList referenceDomain;
+    DimRangeList srcDomain;
+    double radius;
+    std::vector<expr::AssignStatement> updateExpr;
+    bool search3d;
+    double max2dAbove;
+    double max2dBelow;
+    PointViewPtr refView;
+    PointIdList ptsToUpdate;
+};
 
 static PluginInfo const s_info = PluginInfo(
     "filters.radiusassign",
@@ -18,7 +34,7 @@ static PluginInfo const s_info = PluginInfo(
 
 CREATE_STATIC_STAGE(RadiusAssignFilter, s_info)
 
-RadiusAssignFilter::RadiusAssignFilter()
+RadiusAssignFilter::RadiusAssignFilter() : m_p(new Private)
 {}
 
 
@@ -29,128 +45,100 @@ RadiusAssignFilter::~RadiusAssignFilter()
 void RadiusAssignFilter::addArgs(ProgramArgs& args)
 {
     args.add("src_domain", "Selects which points will be subject to "
-        "radius-based neighbors search", m_srcDomainSpec);
+        "radius-based neighbors search", m_p->srcDomain);
     args.add("reference_domain", "Selects which points will be considered as "
-        "potential neighbors", m_referenceDomainSpec);
-    args.add("radius", "Distance of neighbors to consult",
-        m_radius);
+        "potential neighbors", m_p->referenceDomain);
+    args.add("radius", "Distance of neighbors to consult", m_p->radius);
     args.add("update_expression", "Value to assign to dimension of points of src_domain "
             "that have at least one neighbor in reference domain based on expression.",
-        m_updateExpr);
-    args.add("is3d", "Search in 3d", m_search3d, false );
-    args.add("max2d_above", "if search in 2d : upward maximum distance in Z for potential neighbors "
-        "(corresponds to a search in a cylinder with a height = max2d_above above the source point). "
-        "Values < 0 mean infinite height", m_max2dAbove, -1.);
-    args.add("max2d_below", "if search in 2d : downward maximum distance in Z for potential neighbors ("
-        "corresponds to a search in a cylinder with a height = max2d_below below the source point). "
-        "Values < 0 mean infinite height", m_max2dBelow, -1.);
-
-}
-
-
-void RadiusAssignFilter::initializeDomain(StringList domainSpec, std::vector<DimRange> &domain)
-{
-    for (auto const& r : domainSpec)
-    {
-        try
-        {
-            DimRange range;
-            range.parse(r);
-            domain.push_back(range);
-        }
-        catch (const DimRange::error& err)
-        {
-            throwError("Invalid 'domain' option: '" + r + "': " + err.what());
-        }
-    }
+        m_p->updateExpr);
+    args.add("is3d", "Search in 3d", m_p->search3d, false);
+    args.add("max2d_above", "if search in 2d : upward maximum distance in Z for "
+        "potential neighbors (corresponds to a search in a cylinder with a height = "
+        "max2d_above above the source point). Values < 0 mean infinite height",
+        m_p->max2dAbove, -1.);
+    args.add("max2d_below", "if search in 2d : downward maximum distance in Z "
+        "for potential neighbors (corresponds to a search in a cylinder with a height = "
+        "max2d_below below the source point). Values < 0 mean infinite height",
+        m_p->max2dBelow, -1.);
 }
 
 void RadiusAssignFilter::initialize()
 {
-    this->initializeDomain(m_referenceDomainSpec, m_referenceDomain);
-    this->initializeDomain(m_srcDomainSpec, m_srcDomain);
-
-    if (m_radius <= 0)
-        throwError("Invalid 'radius' option: " + std::to_string(m_radius) +
-            ", must be > 0");
-    if (m_updateExpr.size() == 0)
+    if (m_p->radius <= 0)
+        throwError("Invalid 'radius' option: " + std::to_string(m_p->radius) + ", must be > 0");
+    if (m_p->updateExpr.size() == 0)
         throwError("Empty 'update_epxression' option, must be set to apply any change on the data");
-
 }
 
-void RadiusAssignFilter::preparedDomain(std::vector<DimRange> &domain, PointLayoutPtr layout)
-{
-    for (auto& r : domain)
-    {
-        r.m_id = layout->findDim(r.m_name);
-        if (r.m_id == Dimension::Id::Unknown)
-            throwError("Invalid dimension name in 'srcDomain' option: '" +
-                r.m_name + "'.");
-    }
-    std::sort(domain.begin(), domain.end());
-}
 
 void RadiusAssignFilter::prepared(PointTableRef table)
 {
+    Utils::StatusWithReason status;
+
     PointLayoutPtr layout(table.layout());
-    this->preparedDomain(m_srcDomain, layout);
-    this->preparedDomain(m_referenceDomain, layout);
+    status = m_p->srcDomain.prepare(layout);
+    if (!status)
+        throwError("Invalid dimension name in 'src_domain': " + status.what());
+    status = m_p->referenceDomain.prepare(layout);
+    if (!status)
+        throwError("Invalid dimension name in 'reference_domain': " + status.what());
 
-
-    for (expr::AssignStatement& expr : m_updateExpr)
+    for (expr::AssignStatement& expr : m_p->updateExpr)
     {
-        auto status = expr.prepare(layout);
+        status = expr.prepare(layout);
         if (!status)
             throwError("Invalid assignment expression in 'update_expression' option: " +
-            status.what());
+                status.what());
     }
 }
 
+
 void RadiusAssignFilter::ready(PointTableRef)
 {
-    m_ptsToUpdate.clear();
+    m_p->ptsToUpdate.clear();
 }
 
 
 void RadiusAssignFilter::doOneNoDomain(PointRef &pointSrc)
 {
     PointIdList iNeighbors;
-    if (m_search3d) iNeighbors = refView->build3dIndex().radius(pointSrc, m_radius);
-    else iNeighbors = refView->build2dIndex().radius(pointSrc, m_radius);
-
+    if (m_p->search3d)
+        iNeighbors = m_p->refView->build3dIndex().radius(pointSrc, m_p->radius);
+    else
+        iNeighbors = m_p->refView->build2dIndex().radius(pointSrc, m_p->radius);
 
     if (iNeighbors.size() == 0)
         return;
 
-    if (!m_search3d && (m_max2dBelow>=0 || m_max2dAbove>=0))
+    if (!m_p->search3d && (m_p->max2dBelow>=0 || m_p->max2dAbove>=0))
     {
         double Zsrc = pointSrc.getFieldAs<double>(Dimension::Id::Z);
 
         bool take (false);
         for (PointId ptId : iNeighbors)
         {
-            double Zref = refView->point(ptId).getFieldAs<double>(Dimension::Id::Z);
-            if (m_max2dAbove>=0 && Zref>Zsrc && (Zref-Zsrc)>m_max2dAbove) continue;
-            if (m_max2dBelow>=0 && Zsrc>Zref && (Zsrc-Zref)>m_max2dBelow) continue;
+            double Zref = m_p->refView->point(ptId).getFieldAs<double>(Dimension::Id::Z);
+            if (m_p->max2dAbove>=0 && Zref>Zsrc && (Zref-Zsrc)>m_p->max2dAbove) continue;
+            if (m_p->max2dBelow>=0 && Zsrc>Zref && (Zsrc-Zref)>m_p->max2dBelow) continue;
             take = true;
             break;
         }
 
-        if (!take) return;
+        if (!take)
+            return;
     }
 
-
-    m_ptsToUpdate.push_back(pointSrc.pointId());
-
+    m_p->ptsToUpdate.push_back(pointSrc.pointId());
 }
 
 // update point.  kdi and temp both reference the NN point cloud
 bool RadiusAssignFilter::doOne(PointRef& pointSrc)
 {
-    if (m_srcDomain.empty())  // No domain, process all points
+    if (m_p->srcDomain.empty())  // No domain, process all points
         doOneNoDomain(pointSrc);
 
-    for (DimRange& r : m_srcDomain)
+    for (const DimRange& r : m_p->srcDomain.ranges())
     {   // process only points that satisfy a domain condition
         if (r.valuePasses(pointSrc.getFieldAs<double>(r.m_id)))
         {
@@ -166,20 +154,20 @@ void RadiusAssignFilter::filter(PointView& view)
     PointRef pointTemp(view, 0);
 
     // Create reference domain view
-    refView = view.makeNew();
-    if (m_referenceDomain.empty())
+    m_p->refView = view.makeNew();
+    if (m_p->referenceDomain.empty())
         for (PointId id = 0; id < view.size(); ++id)
-            refView->appendPoint(view, id);
+            m_p->refView->appendPoint(view, id);
     else
     {
         for (PointId id = 0; id < view.size(); ++id)
         {
-            for (DimRange& r : m_referenceDomain)
+            for (const DimRange& r : m_p->referenceDomain.ranges())
             {
                 pointTemp.setPointId(id);
                 if (r.valuePasses(pointTemp.getFieldAs<double>(r.m_id)))
                 {
-                    refView->appendPoint(view, id);
+                    m_p->refView->appendPoint(view, id);
                     break;
                 }
             }
@@ -193,16 +181,14 @@ void RadiusAssignFilter::filter(PointView& view)
         doOne(pointTemp);
     }
 
-
-    for (auto id: m_ptsToUpdate)
+    for (auto id: m_p->ptsToUpdate)
     {
         pointTemp.setPointId(id);
-        for (expr::AssignStatement& expr : m_updateExpr)
+        for (expr::AssignStatement& expr : m_p->updateExpr)
             if (expr.conditionalExpr().eval(pointTemp))
                 pointTemp.setField(expr.identExpr().eval(), expr.valueExpr().eval(pointTemp));
     }
 }
-
 
 } // namespace pdal
 
