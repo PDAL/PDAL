1ed9bd24d9982421b14f3fd95f0d52e6d6d81d57
Howard Butler <hobu.inc@gmail.com>
2026-06-15 16:35:47 -0500

    switch to using rattler-build conda CI (#5049)
    * switch to using rattler-build conda CI
    
    * typo/copypasta
    
    * GHA_CI_PLATFORM
    
    * churn
    
    * in-tree build support for rattler-build
    
    * fix macosx designation
    
    * install correct package at the end of this saga
    
    * one more GHA_CI_PLATFORM

--- scripts/ci/conda/setup.sh
+++ scripts/ci/conda/setup.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-conda install -c conda-forge conda-build -y
-pwd
-ls
-git clone https://github.com/conda-forge/libpdal-feedstock.git
+conda info
+conda list
+conda config --show-sources
+
+rm -f ~/.condarc
+
+# For Python 3.13
+conda config --add channels conda-forge/label/python_rc
+
+conda config --show-sources
+
+conda config --show
+
+conda install -c conda-forge rattler-build yq -y
+
+git clone  https://github.com/conda-forge/libpdal-feedstock.git
 
 cd libpdal-feedstock
 
-cat > recipe/recipe_clobber.yaml <<EOL
-source:
-  path: ../../
-  url:
-  sha256:
-  patches:
+# Patch version: "X.Y.Z" to "X.Y.99"
+sed -E 's/version: "([0-9]+)\.([0-9]+)\.([0-9]+)"/version: "\1.\2.99"/' < recipe/recipe.yaml > recipe.yaml
+mv recipe.yaml recipe/recipe.yaml
 
-build:
-  number: 2112
-EOL
+yq -y -i 'del(.source.url)' recipe/recipe.yaml
+yq -y -i 'del(.source.sha256)' recipe/recipe.yaml
+yq -y -i '.source.path = "../../"' recipe/recipe.yaml
+yq -y -i '.build.number = 2112' recipe/recipe.yaml
+
+# Update installation messages of plugins to reflect the appropriate channel
+sed "s/-c conda-forge/-c pdal-master/" < recipe/build.sh >  recipe/build.sh.new
+mv recipe/build.sh.new  recipe/build.sh
+sed "s/-c conda-forge/-c pdal-master/" < recipe/build.bat > recipe/build.bat.new
+mv recipe/build.bat.new  recipe/build.bat
+
 
 ls recipe
