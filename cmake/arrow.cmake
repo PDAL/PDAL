8ffadd5dfa66102d72fb5c56abed5c660984714e
ibell13 <isaac@hobu.co>
2026-07-30 11:10:23 -0500

    fix typo

--- cmake/arrow.cmake
+++ cmake/arrow.cmake
@@ -9,5 +9,5 @@ endif()
 
 find_package(Parquet REQUIRED CONFIG)
 if (Parquet_VERSION VERSION_LESS "21.0")
-    message("Required at least Arrow version 21.0, but found ${Arrow_VERSION}")
+    message("Required at least Parquet version 21.0, but found ${Parquet_VERSION}")
 endif()
