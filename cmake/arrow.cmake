d8b08c94d9f02b9d4b057f92b95f5fa8709d776c
ibell13 <isaac@hobu.co>
2026-08-06 10:24:37 -0500

    cmake: make fatal

--- cmake/arrow.cmake
+++ cmake/arrow.cmake
@@ -4,10 +4,10 @@
 
 find_package(Arrow REQUIRED CONFIG)
 if (Arrow_VERSION VERSION_LESS "21.0")
-    message("Required at least Arrow version 21.0, but found ${Arrow_VERSION}")
+    message(FATAL_ERROR "Required at least Arrow version 21.0, but found ${Arrow_VERSION}")
 endif()
 
 find_package(Parquet REQUIRED CONFIG)
 if (Parquet_VERSION VERSION_LESS "21.0")
-    message("Required at least Parquet version 21.0, but found ${Parquet_VERSION}")
+    message(FATAL_ERROR "Required at least Parquet version 21.0, but found ${Parquet_VERSION}")
 endif()
